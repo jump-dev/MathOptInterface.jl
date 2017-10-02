21aad40b53f87a1e6d8cba413dd7ca27bf44d40c
odow <odow003@aucklanduni.ac.nz>
2017-10-03 10:55:42 +1300

    First example

--- src/MathOptFormat.jl
+++ src/MathOptFormat.jl
@@ -1,19 +1,20 @@
 module MathOptFormat
 
-using JSON
+using JSON, DataStructures
 
 using MathOptInterface
 const MOI = MathOptInterface
 
-const Object = Dict{String, Any}
+# we use an ordered dict to make the JSON printing nicer
+const Object = OrderedDict{String, Any}
 
 immutable MOFFile
     d::Object
     variables::Dict{MOI.VariableReference, String}
 end
 MOFFile() = MOFFile(
-    Dict(
-        "version" => v"0.1",
+    OrderedDict(
+        "version" => "0.0",
         "sense"   => "min",
         "variables" => String[],
         "objective" => Object(),
@@ -24,24 +25,31 @@ MOFFile() = MOFFile(
 Base.getindex(m, key) = getindex(m.d, key)
 Base.setindex!(m, key, value) = setindex!(m.d, key, value)
 
-save(f::IO, m::MOFFile) = write(io, JSON.json(m.d))
+save(io::IO, m::MOFFile) = write(io, JSON.json(m.d, 1))
 function save(f::String, m::MOFFile)
     open(f, "w") do io
-        write(io, m
+        write(io, m)
     end
 end
 
-Object(::MOI.MaxSense) = "max"
-Object(::MOI.MinSense) = "min"
+function Object(sense::MOI.OptimizationSense)
+    if sense == MOI.MaxSense
+        return "max"
+    elseif sense == MOI.MinSense
+        return "min"
+    end
+    error("Sense $(sense) not recognised.")
+end
+
 function MOI.setobjective!(m::MOFFile, sense, func)
     m["sense"] = Object(sense)
-    m["objective"] = Object(m, func)
+    m["objective"] = Object!(m, func)
 end
 
 function MOI.addconstraint!(m::MOFFile, func, set)
     push!(
         m["constraints"],
-        Dict(
+        Object(
             "set" => Object(set),
             "function" => Object!(m, func)
         )
@@ -52,7 +60,7 @@ end
     FUNCTIONS
 =#
 
-function getvariable!(m::MOFFile, v::VariableReference)
+function getvariable!(m::MOFFile, v::MOI.VariableReference)
     if !haskey(m.variables, v)
         m.variables[v] = "x$(v.value)"
         push!(m.d["variables"], m.variables[v])
@@ -60,16 +68,76 @@ function getvariable!(m::MOFFile, v::VariableReference)
     m.variables[v]
 end
 
+"""
+    {
+        "head": "variable",
+        "value": "x1"
+    }
+"""
 Object!(m::MOFFile, func::MOI.SingleVariable) = Object("head"=>"variable", "name"=>getvariable!(m, func.variable))
 
-Object!(m::MOFFile, func::MOI.VectorOfVariables) = Object("head"=>"variable", "names"=>getvariable!.(m, func.variables))
+"""
+    {
+        "head": "variableset",
+        "value": ["x1", "x2"]
+    }
+"""
+Object!(m::MOFFile, func::MOI.VectorOfVariables) = Object("head"=>"variableset", "names"=>getvariable!.(m, func.variables))
+
+"""
+    MathOptInterface.ScalarAffineFunction
+
+A special case to express an affine expression <variables, coefficients> +
+constant. Other fields are:
+
+ - `variables`: an array of variable names. Every element must be a string that
+ corresponds to the name of a variable. Objects and numeric values are illegal.
+ - `coefficients`: an array of numeric values. Every element must be a numeric
+ value. Objects and strings are illegal.
+ - `constant`: This must be a numeric value. Objects and strings are illegal.
+
+### Example: x + y + 3
+
+    {
+      head: "linear",
+      variables: ["x", "y"],
+      coefficients: [1.0, 2.0],
+      constant: 3.0
+    }
+"""
+Object!(m::MOFFile, func::MOI.ScalarAffineFunction) = Object(
+    "head"         => "linear",
+    "variables"    => getvariable!.(m, func.variables),
+    "coefficients" => func.coefficients,
+    "constant"     => func.constant
+)
 
 #=
     SETS
 =#
 
+"""
+    {
+        "head": "EqualTo",
+        "value": 3.0
+    }
+"""
 Object(set::MOI.EqualTo) = Object("head" => "EqualTo", "value"=>set.value)
+
+"""
+    {
+        "head": "LessThan",
+        "value": 3.0
+    }
+"""
 Object(set::MOI.LessThan) = Object("head" => "LessThan", "value"=>set.upper)
+
+"""
+    {
+        "head": "GreaterThan",
+        "value": 3.0
+    }
+"""
 Object(set::MOI.GreaterThan) = Object("head" => "GreaterThan", "value"=>set.lower)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,74 @@
-using MathOptFormat
-using Base.Test
+using MathOptFormat, Base.Test
+const MOF = MathOptFormat
+const MOI = MathOptFormat.MathOptInterface
+const JSON = MOF.JSON
 
-# write your own tests here
-@test 1 == 2
+function problem(file::String)
+    replace(readstring(joinpath(@__DIR__, "problems", file)), "\r\n", "\n")
+end
+
+@testset "MOFFile" begin
+    @testset "getvariable!" begin
+        v = MOI.VariableReference(1)
+        m = MOF.MOFFile()
+        @test MOF.getvariable!(m, v) == "x1"
+        @test length(m.d["variables"]) == 1
+        @test length(keys(m.variables)) == 1
+        @test MOF.getvariable!(m, v) == "x1"
+        @test length(m.d["variables"]) == 1
+        @test length(keys(m.variables)) == 1
+    end
+    @testset "JSON.json(::MOFFile)" begin
+        m = MOF.MOFFile()
+        @test JSON.json(m.d) == "{\"version\":\"0.0\",\"sense\":\"min\",\"variables\":[],\"objective\":{},\"constraints\":[]}"
+    end
+
+    @testset "OptimizationSense" begin
+        @test MOF.Object(MOI.MinSense) == "min"
+        @test MOF.Object(MOI.MaxSense) == "max"
+    end
+end
+
+@testset "Sets" begin
+    @test JSON.json(MOF.Object(MOI.EqualTo(3.0)))     == "{\"head\":\"EqualTo\",\"value\":3.0}"
+    @test JSON.json(MOF.Object(MOI.LessThan(3.0)))    == "{\"head\":\"LessThan\",\"value\":3.0}"
+    @test JSON.json(MOF.Object(MOI.GreaterThan(3.0))) == "{\"head\":\"GreaterThan\",\"value\":3.0}"
+end
+
+@testset "Functions" begin
+
+
+    @testset "SingleVariable" begin
+        v = MOI.VariableReference(1)
+        m = MOF.MOFFile()
+        @test JSON.json(MOF.Object!(m, MOI.SingleVariable(v))) == "{\"head\":\"variable\",\"name\":\"x1\"}"
+    end
+
+    @testset "VectorOfVariables" begin
+        v = MOI.VariableReference(1)
+        m = MOF.MOFFile()
+        @test JSON.json(MOF.Object!(m, MOI.VectorOfVariables([v,v]))) == "{\"head\":\"variableset\",\"names\":[\"x1\",\"x1\"]}"
+    end
+
+    @testset "ScalarAffineFunction" begin
+        v = MOI.VariableReference(1)
+        m = MOF.MOFFile()
+        f = MOI.ScalarAffineFunction([v, v], [1.0, 2.0], 3.0)
+
+        @test JSON.json(MOF.Object!(m, f)) == "{\"head\":\"linear\",\"variables\":[\"x1\",\"x1\"],\"coefficients\":[1.0,2.0],\"constant\":3.0}"
+    end
+end
+
+@testset "Examples" begin
+    @testset "1.mof.json" begin
+        v = MOI.VariableReference(1)
+        m = MOF.MOFFile()
+        f = MOI.ScalarAffineFunction([v, v], [1.0, 2.0], 3.0)
+        MOI.setobjective!(m, MOI.MinSense, f)
+        f2 = MOI.ScalarAffineFunction([v], [1.0], 0.0)
+        MOI.addconstraint!(m, f2, MOI.LessThan(3.0))
+        io = IOBuffer()
+        MOF.save(io, m)
+        @test String(take!(io)) == problem("1.mof.json")
+    end
+end
