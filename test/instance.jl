6bea4fcaa05798cb3ebb3b20d5f5d8d38b92755a
Benoît Legat <benoit.legat@gmail.com>
2017-10-28 11:50:07 +0200

    Add test for variable and constraint names when deleted

--- test/instance.jl
+++ test/instance.jl
@@ -23,6 +23,12 @@ function nametest(instance::MOI.AbstractInstance)
         @test MOI.get(instance, MOI.VariableReference, "Var2") == v[2]
         @test_throws KeyError MOI.get(instance, MOI.VariableReference, "Var3")
 
+        if MOI.candelete(instance, v[2])
+            MOI.delete!(instance, v[2])
+            @test !MOI.canget(instance, MOI.VariableReference, "Var2")
+            @test_throws KeyError MOI.get(instance, MOI.VariableReference, "Var2")
+        end
+
         c = MOI.addconstraint!(instance, MOI.ScalarAffineFunction(v, [1.0,1.0], 0.0), MOI.LessThan(1.0))
         @test MOI.canset(instance, MOI.ConstraintName(), c)
         @test MOI.canget(instance, MOI.ConstraintName(), c)
@@ -39,6 +45,14 @@ function nametest(instance::MOI.AbstractInstance)
         @test_throws KeyError MOI.get(instance, MOI.ConstraintReference{MOI.ScalarAffineFunction{Float64},MOI.LessThan{Float64}}, "Con2")
         @test MOI.get(instance, MOI.ConstraintReference, "Con1") == c
         @test_throws KeyError MOI.get(instance, MOI.ConstraintReference, "Con2")
+
+        if MOI.candelete(instance, c)
+            MOI.delete!(instance, c)
+            @test !MOI.canget(instance, MOI.ConstraintReference{MOI.ScalarAffineFunction{Float64},MOI.LessThan{Float64}}, "Con1")
+            @test !MOI.canget(instance, MOI.ConstraintReference, "Con1")
+            @test_throws KeyError MOI.get(instance, MOI.ConstraintReference{MOI.ScalarAffineFunction{Float64},MOI.LessThan{Float64}}, "Con1")
+            @test_throws KeyError MOI.get(instance, MOI.ConstraintReference, "Con1")
+        end
 
         # TODO: Test for error when duplicate names are assigned
     end
