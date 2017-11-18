24b0513f7e48057a686c28dd77cfdc51ececacd6
Miles Lubin <mlubin@google.com>
2017-11-18 18:01:52 -0500

    Remove tests; relocated to MOIT

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
 using MathOptInterface, Base.Test
 
-const MOI = MathOptInterface
-
-# TODO: Any tests here should be self-contained. We should have a toy reference implementation of a solver that we can test. Until then, the interface is tested when solvers run the separate test files.
+# There is no standalone code in this package to test.
+# Tests for solvers are located in MathOptInterfaceTests.
