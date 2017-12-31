6244a54e1c994972b8b813a886f587f736f6b445
Miles Lubin <mlubin@google.com>
2017-12-31 16:44:54 -0500

    wording fix

--- src/MathOptInterface.jl
+++ src/MathOptInterface.jl
@@ -65,7 +65,7 @@ function read! end
 """
     isempty(instance::AbstractInstance)
 
-Returns whether the `instance` has any attribute set or has any variables or constraints its model.
+Returns `false` if the `instance` has any attribute set or has any variables, constraints, or objective function.
 """
 function isempty end
 
