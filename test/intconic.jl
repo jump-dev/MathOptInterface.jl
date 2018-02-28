fdc3d5ce66898929f574f18ac8385cfa34d2156f
Benoît Legat <benoit.legat@gmail.com>
2018-02-28 15:19:20 +0100

    optimizer -> mock

--- test/intconic.jl
+++ test/intconic.jl
@@ -1,10 +1,10 @@
 @testset "Integer Conic" begin
-    optimizer = MOIU.MockOptimizer(Model{Float64}())
+    mock = MOIU.MockOptimizer(Model{Float64}())
     config = MOIT.TestConfig()
-    optimizer.evalobjective = true
+    mock.evalobjective = true
 
     @testset "SOC" begin
-        optimizer.optimize! = (optimizer::MOIU.MockOptimizer) -> MOIU.mock_optimize!(optimizer, [1.0, 1.0, 0.0])
-        MOIT.intsoc1test(optimizer, config)
+        mock.optimize! = (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock, [1.0, 1.0, 0.0])
+        MOIT.intsoc1test(mock, config)
     end
 end
--- test/intlinear.jl
+++ test/intlinear.jl
@@ -1,13 +1,13 @@
 @testset "Integer Linear" begin
-    optimizer = MOIU.MockOptimizer(Model{Float64}())
+    mock = MOIU.MockOptimizer(Model{Float64}())
     config = MOIT.TestConfig()
-    optimizer.evalobjective = true
+    mock.evalobjective = true
 
-    optimizer.optimize! = (optimizer::MOIU.MockOptimizer) -> MOIU.mock_optimize!(optimizer, [4, 5, 1])
-    MOIT.int1test(optimizer, config)
+    mock.optimize! = (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock, [4, 5, 1])
+    MOIT.int1test(mock, config)
     # FIXME [1, 0...] is not the correct optimal solution but it passes the test
-    optimizer.optimize! = (optimizer::MOIU.MockOptimizer) -> MOIU.mock_optimize!(optimizer, [1.0; zeros(10)])
-    MOIT.int3test(optimizer, config)
-    optimizer.optimize! = (optimizer::MOIU.MockOptimizer) -> MOIU.mock_optimize!(optimizer, [1, 0, 0, 1, 1])
-    MOIT.knapsacktest(optimizer, config)
+    mock.optimize! = (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock, [1.0; zeros(10)])
+    MOIT.int3test(mock, config)
+    mock.optimize! = (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock, [1, 0, 0, 1, 1])
+    MOIT.knapsacktest(mock, config)
 end
