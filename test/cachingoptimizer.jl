92187c114372a078c5ac5a51e3efd541935507e2
Benoît Legat <benoit.legat@gmail.com>
2018-03-20 16:41:48 +0100

    Do not share state between tests

--- test/cachingoptimizer.jl
+++ test/cachingoptimizer.jl
@@ -163,22 +163,25 @@ end
 end
 
 @testset "CachingOptimizer constructor with optimizer" begin
-    s = MOIU.MockOptimizer(ModelForMock{Float64}())
-    model = ModelForCachingOptimizer{Float64}()
     @testset "Empty model and optimizer" begin
+        s = MOIU.MockOptimizer(ModelForMock{Float64}())
+        model = ModelForCachingOptimizer{Float64}()
         m = MOIU.CachingOptimizer(model, s)
         @test MOI.isempty(m)
         @test MOIU.state(m) == MOIU.AttachedOptimizer
         @test MOIU.mode(m) == MOIU.Automatic
     end
     @testset "Non-empty optimizer" begin
+        s = MOIU.MockOptimizer(ModelForMock{Float64}())
         MOI.addvariable!(s)
+        model = ModelForCachingOptimizer{Float64}()
         @test MOI.isempty(model)
         @test !MOI.isempty(s)
         @test_throws AssertionError MOIU.CachingOptimizer(model, s)
-        MOI.empty!(s)
     end
     @testset "Non-empty model" begin
+        s = MOIU.MockOptimizer(ModelForMock{Float64}())
+        model = ModelForCachingOptimizer{Float64}()
         MOI.addvariable!(model)
         @test !MOI.isempty(model)
         @test MOI.isempty(s)
