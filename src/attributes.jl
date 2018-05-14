4c3a6d96986d291f1f4039c2bf8fdbd6b051a5a9
Oscar Dowson <odow@users.noreply.github.com>
2018-05-14 15:27:49 +1200

    Update attributes.jl

--- src/attributes.jl
+++ src/attributes.jl
@@ -491,8 +491,9 @@ If `N` is omitted, it is 1 by default.
 
 Given a constraint `function-in-set`, the `ConstraintPrimal` is the value of the
 function evaluated at the primal solution of the variables. For example, given
-the constraint `2x + y <= 1`, and a primal solution of `(x,y) = (1,2)`, the
-`ConstraintPrimal` solution of the constraint is `2 * 1 + 2=4`.
+the constraint `ScalarAffineFunction([x,y], [1, 2], 3)`-in-`LessThan(0)` and 
+a primal solution of `(x,y) = (4,5)`, the `ConstraintPrimal` solution of the 
+constraint is `1 * 4 + 2 * 5 + 3 = 17`.
 """
 struct ConstraintPrimal <: AbstractConstraintAttribute
     N::Int
