de4c0552e59b7a4b3f14562609fc5139099d511f
Benoît Legat <benoit.legat@gmail.com>
2018-05-24 10:36:58 +0200

    Add warning comment

--- src/Bridges/lazybridgeoptimizer.jl
+++ src/Bridges/lazybridgeoptimizer.jl
@@ -1,3 +1,4 @@
+# /!\ LazyBridgeOptimizer is still experimental and is meant to be used by JuMP. Use the SingleBridgeOptimizer in the solver tests as explained in the MOI documentation.
 """
     LazyBridgeOptimizer{OT<:MOI.ModelLike, MT<:MOI.ModelLike} <: AbstractBridgeOptimizer
 
