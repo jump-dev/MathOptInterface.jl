6cfdb4542fe000ce05ca299513411c22cabf05af
Benoît Legat <benoit.legat@gmail.com>
2018-08-03 21:30:06 +0200

    Document get_fallback

--- src/Utilities/results.jl
+++ src/Utilities/results.jl
@@ -1,14 +1,56 @@
+"""
+    get_fallback(model::MOI.ModelLike, ::MOI.ObjectiveValue)
+
+Compute the objective function value using the `VariablePrimal` results and
+the `ObjectiveFunction` value.
+"""
 function get_fallback(model::MOI.ModelLike, ::MOI.ObjectiveValue)
     F = MOI.get(model, MOI.ObjectiveFunctionType())
     f = MOI.get(model, MOI.ObjectiveFunction{F}())
+    # TODO do not include constant if primal solution is a ray
     return evalvariables(vi -> MOI.get(model, MOI.VariablePrimal(), vi), f)
 end
 
+"""
+    get_fallback(model::MOI.ModelLike, ::MOI.ConstraintPrimal,
+                 constraint_index::MOI.ConstraintIndex)
+
+Compute the value of the function of the constraint of index `constraint_index`
+using the `VariablePrimal` results and the `ConstraintFunction` values.
+"""
 function get_fallback(model::MOI.ModelLike, ::MOI.ConstraintPrimal,
                       idx::MOI.ConstraintIndex)
     f = MOI.get(model, MOI.ConstraintFunction(), idx)
+    # TODO do not include constant if primal solution is a ray
     return evalvariables(vi -> MOI.get(model, MOI.VariablePrimal(), vi), f)
 end
+
+################ Constraint Dual for Variable-wise constraints #################
+#
+# In the primal we have
+#   min a_0' x + b_0
+#       A_i  x + b_i in C_i for all i
+# In the dual we have
+#   max b_0 - sum b_i' y
+#       a_0 - sum A_i* y_i = 0
+#                      y_i in C_i* for all i
+# where A_i* is the adjoint operator of the linear operator A_i. That is, A*
+# is the linear operator such that
+# ⟨A x, y⟩_{C_i} = ⟨x, A* y⟩_Rn
+# where
+# * ⟨., .⟩_Rn is the standard scalar product over Rn: ⟨., .⟩_Rn and
+# * ⟨., .⟩_{C_i} is the scalar product `set_dot` defined for the set C_i
+#
+# Suppose we want to get the constraint variable of a variable-wise constraint:
+#   A_j x in C_j
+# where A_j is zero except on a submatrix which is the identity. We have
+# A_j* y_j = a_0 - sum_(i != j) A_i* y_i
+# Thus to get the dual y_j, we simply have to compute the right-hand side and
+# then invert A_j*. To get the kth element of A_i* y_i we need to compute
+# ⟨e_k, A_i* y_i⟩_Rn = ⟨A_i e_k, y_i⟩_{C_i}. A_i e_k is computed using
+# `variable_coefficient` and then it is combined with the dual y_i with
+# `MOI.set_dot`.
+# Once A_j* y_j is obtained, we invert A_j* with `MOI.dot_coefficients`.
 
 function variable_coefficient(func::MOI.ScalarAffineFunction{T},
                               vi::MOI.VariableIndex) where T
@@ -124,39 +166,31 @@ function variable_dual(model::MOI.ModelLike,
     return dual
 end
 
-# In the primal we have
-#   min a_0' x + b_0
-#       A_i  x + b_i in C_i for all i
-# In the dual we have
-#   max b_0 - sum b_i' y
-#       a_0 - sum A_i* y_i = 0
-#                      y_i in C_i* for all i
-# where A_i* is the adjoint operator of the linear operator A_i. That is, A*
-# is the linear operator such that
-# ⟨A x, y⟩_{C_i} = ⟨x, A* y⟩_Rn
-# where
-# * ⟨., .⟩_Rn is the standard scalar product over Rn: ⟨., .⟩_Rn and
-# * ⟨., .⟩_{C_i} is the scalar product `set_dot` defined for the set C_i
-#
-# Suppose we want to get the constraint variable of a variable-wise constraint:
-#   A_j x in C_j
-# where A_j is zero except on a submatrix which is the identity. We have
-# A_j* y_j = a_0 - sum_(i != j) A_i* y_i
-# Thus to get the dual y_j, we simply have to compute the right-hand side and
-# then invert A_j*. To get the kth element of A_i* y_i we need to compute
-# ⟨e_k, A_i* y_i⟩_Rn = ⟨A_i e_k, y_i⟩_{C_i}. A_i e_k is computed using
-# `variable_coefficient` and then it is combined with the dual y_i with
-# `MOI.set_dot`.
-# Once A_j* y_j is obtained, we invert A_j* with `MOI.dot_coefficients`.
-function get_fallback(model::MOI.ModelLike, attr::MOI.ConstraintDual,
-                      ci::MOI.ConstraintIndex{MOI.SingleVariable})
-    func = MOI.get(model, MOI.ConstraintFunction(), ci)
+function variable_dual(model::MOI.ModelLike, attr::MOI.ConstraintDual,
+                       ci::MOI.ConstraintIndex, func::MOI.SingleVariable)
     return variable_dual(model, attr, ci, func.variable)
 end
-function get_fallback(model::MOI.ModelLike, attr::MOI.ConstraintDual,
-                      ci::MOI.ConstraintIndex{MOI.VectorOfVariables})
-    func = MOI.get(model, MOI.ConstraintFunction(), ci)
+function variable_dual(model::MOI.ModelLike, attr::MOI.ConstraintDual,
+                       ci::MOI.ConstraintIndex, func::MOI.VectorOfVariables)
     dual = map(vi -> variable_dual(model, attr, ci, vi), func.variables)
     set = MOI.get(model, MOI.ConstraintSet(), ci)
     return MOI.dot_coefficients(dual, set)
+end
+
+"""
+    get_fallback(model::MOI.ModelLike, attr::MOI.ConstraintDual,
+                 ci::MOI.ConstraintIndex{Union{MOI.SingleVariable,
+                                               MOI.VectorOfVariables}})
+
+Compute the dual of the constraint of index `ci` using the `ConstraintDual` of
+other constraints and the `ConstraintFunction` values. Throws an error if some
+constraints are quadratic or if there is one another `MOI.SingleVariable`-in-`S`
+or `MOI.VectorOfVariables`-in-`S` constraint with one of the variables in the
+function of the constraint `ci`.
+"""
+function get_fallback(model::MOI.ModelLike, attr::MOI.ConstraintDual,
+                      ci::MOI.ConstraintIndex{Union{MOI.SingleVariable,
+                                                    MOI.VectorOfVariables}})
+    func = MOI.get(model, MOI.ConstraintFunction(), ci)
+    return variable_dual(model, attr, ci, func)
 end
