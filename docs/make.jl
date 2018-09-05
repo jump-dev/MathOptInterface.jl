a827a87924a252769f808e67c4ce14e3ccd641b2
Oscar Dowson <odow@users.noreply.github.com>
2018-09-06 11:41:26 +1200

    Build docs from travis (#24)

--- docs/make.jl
+++ docs/make.jl
@@ -8,3 +8,12 @@ makedocs(
         "MathOptFormat" => "index.md"
     ]
 )
+
+deploydocs(
+    repo   = "github.com/odow/MathOptFormat.jl.git",
+    target = "build",
+    osname = "linux",
+    julia  = "1.0",
+    deps   = nothing,
+    make   = nothing
+)
