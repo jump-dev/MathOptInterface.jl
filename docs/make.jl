9c51fb2782949791de5342a25ce166e5a0805f99
Benoît Legat <benoit.legat@gmail.com>
2018-10-28 18:56:12 +0100

    Use a Travis project to build doc

--- docs/make.jl
+++ docs/make.jl
@@ -14,7 +14,7 @@ deploydocs(
     repo   = "github.com/JuliaOpt/MathOptInterface.jl.git",
     target = "build",
     osname = "linux",
-    julia  = "0.6",
+    julia  = "1.0",
     deps   = nothing,
     make   = nothing,
 )
