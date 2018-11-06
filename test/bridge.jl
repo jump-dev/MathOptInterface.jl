95f67187dee7f3ca50d09d9d2880740d40d9e7d9
Benoît Legat <benoit.legat@gmail.com>
2018-11-06 14:45:23 +0100

    Move inside testsets

--- test/bridge.jl
+++ test/bridge.jl
@@ -274,45 +274,51 @@ end
                                    MOI.LessThan(0.0))
             end
         end
-        MOIU.set_mock_optimize!(mock,
-            (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock,
-                MOI.Success,
-                (MOI.FeasiblePoint, [0.5, 0.5])
-            ),
-            (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock,
-                MOI.Success,
-                (MOI.FeasiblePoint, [0.5, (√13 - 1)/4])
+        @testset "Quadratic constraints with 2 variables" begin
+            MOIU.set_mock_optimize!(mock,
+                (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock,
+                    MOI.Success,
+                    (MOI.FeasiblePoint, [0.5, 0.5])
+                ),
+                (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock,
+                    MOI.Success,
+                    (MOI.FeasiblePoint, [0.5, (√13 - 1)/4])
+                )
             )
-        )
-        MOIT.solve_qcp_edge_cases(bridgedmock, config)
-        ci = first(MOI.get(mock,
-                           MOI.ListOfConstraintIndices{MOI.VectorAffineFunction{Float64},
-                                                       MOI.RotatedSecondOrderCone}()))
-        x, y = MOI.get(mock, MOI.ListOfVariableIndices())
-        # The matrix is
-        # 2 1
-        # 1 2
-        # for which the cholesky factorization is U' * U with U =
-        # √2 √2/2
-        #  . √3/√2
-        expected = MOI.VectorAffineFunction{Float64}(MOI.VectorAffineTerm.([3, 3, 4],
-                                                                           MOI.ScalarAffineTerm.([√2, √2/2, √3/√2],
-                                                                                                 [x, y, y])),
-                                                     [1.0, 1.0, 0.0, 0.0])
-        @test MOI.get(mock, MOI.ConstraintFunction(), ci) ≈ expected
-        MOIU.set_mock_optimize!(mock,
-            (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock, [1/2, 7/4], MOI.FeasiblePoint))
-        MOIT.qcp1test(bridgedmock, config)
-        MOIU.set_mock_optimize!(mock,
-            (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock, [√2], MOI.FeasiblePoint))
-        MOIT.qcp2test(bridgedmock, config)
-        MOIU.set_mock_optimize!(mock,
-            (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock, [√2], MOI.FeasiblePoint))
-        MOIT.qcp3test(bridgedmock, config)
-        ci = first(MOI.get(bridgedmock,
-                           MOI.ListOfConstraintIndices{MOI.ScalarQuadraticFunction{Float64},
-                                                       MOI.LessThan{Float64}}()))
-        test_delete_bridge(bridgedmock, ci, 1, ((MOI.VectorAffineFunction{Float64}, MOI.RotatedSecondOrderCone, 0),))
+            MOIT.solve_qcp_edge_cases(bridgedmock, config)
+            ci = first(MOI.get(mock,
+                               MOI.ListOfConstraintIndices{MOI.VectorAffineFunction{Float64},
+                                                           MOI.RotatedSecondOrderCone}()))
+            x, y = MOI.get(mock, MOI.ListOfVariableIndices())
+            # The matrix is
+            # 2 1
+            # 1 2
+            # for which the cholesky factorization is U' * U with U =
+            # √2 √2/2
+            #  . √3/√2
+            expected = MOI.VectorAffineFunction{Float64}(MOI.VectorAffineTerm.([3, 3, 4],
+                                                                               MOI.ScalarAffineTerm.([√2, √2/2, √3/√2],
+                                                                                                     [x, y, y])),
+                                                         [1.0, 1.0, 0.0, 0.0])
+            @test MOI.get(mock, MOI.ConstraintFunction(), ci) ≈ expected
+        end
+        @testset "QCP tests" begin
+            MOIU.set_mock_optimize!(mock,
+                (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock, [1/2, 7/4], MOI.FeasiblePoint))
+            MOIT.qcp1test(bridgedmock, config)
+            MOIU.set_mock_optimize!(mock,
+                (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock, [√2], MOI.FeasiblePoint))
+            MOIT.qcp2test(bridgedmock, config)
+            MOIU.set_mock_optimize!(mock,
+                (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock, [√2], MOI.FeasiblePoint))
+            MOIT.qcp3test(bridgedmock, config)
+            @testset "Bridge deletion" begin
+                ci = first(MOI.get(bridgedmock,
+                                   MOI.ListOfConstraintIndices{MOI.ScalarQuadraticFunction{Float64},
+                                                               MOI.LessThan{Float64}}()))
+                test_delete_bridge(bridgedmock, ci, 1, ((MOI.VectorAffineFunction{Float64}, MOI.RotatedSecondOrderCone, 0),))
+            end
+        end
     end
 
     @testset "SquarePSD" begin
