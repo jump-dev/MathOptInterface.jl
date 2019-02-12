d33e7fae68f898b8e206d404f5503554dc52e3f5
odow <o.dowson@gmail.com>
2019-02-11 20:05:10 -0600

    Tidy docstrings

--- src/callbacks.jl
+++ src/callbacks.jl
@@ -5,16 +5,17 @@ Set MIP-based callbacks. Two basic types are offered: `lazy` and `heuristic`.
 
 ### The lazy callback
 
-The `lazy` callback can be used to add *lazy* constraints which are added on
-demand at integer nodes in the branch and bound tree.
+The `lazy` callback can be used to add *lazy* constraints which are added
+on-demand at integer nodes in the branch and bound tree.
+
+Points to consider are:
 
 - The lazy constraint callback *may* be called when the solver has an integer
-  primal solution
-- You can only access the primal solution through `VariablePrimal`. That is,
-  `ConstraintDual` etc will not work
-- When the solver terminates, it will provide a solution that satisfies all
-  lazy constraints
-- We make no claims about what happens to solutions during the B&B tree
+  primal solution.
+- You can only access the primal solution through `VariablePrimal`. For example,
+  `ConstraintDual` etc will not work.
+- The optimal solution will satisfy all lazy constraints that could *possibly*
+  have been added.
 
 See also [`add_lazy_constraint`](@ref).
 
@@ -24,9 +25,11 @@ The `heuristic` callback can be used to provide the solver with heuristically
 obtained integer-feasible solutions at fractional nodes in the branch and bound
 tree.
 
+Points to consider are:
+
 - The heuristic callback *may* be called when the solver has a fractional (i.e.,
-  non-integer) solution
-- The solver may silently reject the provided solution
+  non-integer) solution.
+- The solver may silently reject the provided solution.
 - Some solvers require a complete solution, others only partial solutions. It's
   up to you to provide the appropriate one. If in doubt, give a complete
   solution.
@@ -47,12 +50,13 @@ end
 
 Add a lazy constraint `func`-in-`set` to `model`.
 
-`cb_data` is a solver-specific callback type.
-
 This can be called only from a lazy callback set by the attribute `Callbacks()`.
+
+`cb_data` is a solver-specific callback type that is passed as the argument to
+the lazy callback.
 """
 function add_lazy_constraint(model, cb_data, func, set)
-    error("add_lazy_constraint not supported by this solver.")
+    error("add_lazy_constraint is not supported by this solver.")
 end
 
 """
@@ -64,7 +68,10 @@ Provide the heuristic solution given by the variable-value mapping of `sol` to
 
 This can be called only from a heuristic callback set by the attribute
 `Callbacks()`.
+
+`cb_data` is a solver-specific callback type that is passed as the argument to
+the heuristic callback.
 """
 function add_heuristic_solution(model, cb_data, sol)
-    error("add_heuristic_solution not supported by this solver.")
+    error("add_heuristic_solution is not supported by this solver.")
 end
