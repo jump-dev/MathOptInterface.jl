c7f926ef44f57e8b1b97caeec9b1ee767784de2c
Mathieu Besançon <mathieu.besancon@gmail.com>
2019-04-25 12:17:11 +0200

    fix comments, added test for ACTIVE_ON_ZERO

--- test/Test/intlinear.jl
+++ test/Test/intlinear.jl
@@ -29,4 +29,8 @@
         (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock, [2.0, 8.0, 1., 0.])
     )
     MOIT.indicator2_test(mock, config)
+    MOIU.set_mock_optimize!(mock,
+        (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock, [1.25, 8.75, 1., 1.])
+    )
+    MOIT.indicator3_test(mock, config)
 end
