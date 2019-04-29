caeb4c497627df02175d7a3dc7cea04c2d04a8fe
Mathieu Besançon <mathieu.besancon@gmail.com>
2019-04-29 10:06:52 +0200

    add mutable test

--- test/sets.jl
+++ test/sets.jl
@@ -1,3 +1,15 @@
+"""
+    MutLessThan{T<:Real} <: MOI.AbstractScalarSet
+
+A mutable `LessThan`-like set to test `copy` of indicator set
+"""
+mutable struct MutLessThan{T<:Real} <: MOI.AbstractScalarSet
+    upper::T
+    MutLessThan(v::T) where {T<:Real} = new{T}(v)
+end
+
+Base.copy(mlt::MutLessThan) = MutLessThan(Base.copy(mlt.upper))
+
 @testset "Sets" begin
     @testset "Copy" begin
         @testset "for $S" for S in [MOI.SOS1, MOI.SOS2]
@@ -15,6 +27,12 @@
             @test s1 == s1_copy
             @test s2_copy isa MOI.IndicatorSet{MOI.ACTIVATE_ON_ZERO}
             @test s2 == s2_copy
+            s3 = MOI.IndicatorSet{MOI.ACTIVATE_ON_ZERO}(MutLessThan(4.0))
+            s3_copy = copy(s3)
+            @test s3.set.upper ≈ 4.0
+            s3_copy.set.upper = 5.0
+            @test s3.set.upper ≈ 4.0
+            @test s3_copy.set.upper ≈ 5.0    
         end
     end
 end
