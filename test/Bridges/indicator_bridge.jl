3a943484f4ca7a1fba3e981fd5c2230c87d30091
Mathieu Besançon <mathieu.besancon@gmail.com>
2019-05-20 14:42:53 +0200

    tests

--- test/Bridges/indicator_bridge.jl
+++ test/Bridges/indicator_bridge.jl
@@ -40,41 +40,12 @@ include("../model.jl")
 
     bridge = MOIB.bridge_constraint(MOIB.IndicatorActiveOnFalseBridge, model, f1, iset1)
 
-    # MOI.add_constraint(model, f1, iset1)
-    #
-    # f2 = MOI.VectorAffineFunction(
-    #   [MOI.VectorAffineTerm(1, MOI.ScalarAffineTerm(1.0, z2)),
-    #    MOI.VectorAffineTerm(2, MOI.ScalarAffineTerm(0.2, x1)),
-    #    MOI.VectorAffineTerm(2, MOI.ScalarAffineTerm(1.0, x2)),
-    #   ],
-    #   [0.0, 0.0],
-    # )
-    # iset2 = MOI.IndicatorSet{MOI.ACTIVATE_ON_ONE}(MOI.LessThan(9.0))
-    #
-    # MOI.add_constraint(model, f2, iset2)
-    #
-    # # Additional regular constraint.
-    # MOI.add_constraint(model,
-    #   MOI.ScalarAffineFunction([MOI.ScalarAffineTerm(1.0, x1), MOI.ScalarAffineTerm(1.0, x2)], 0.0),
-    #   MOI.LessThan(10.0),
-    # )
-    #
-    # # Disjunction (1-z1) ⋁ z2
-    # MOI.add_constraint(model,
-    #   MOI.ScalarAffineFunction([MOI.ScalarAffineTerm(-1.0, z1), MOI.ScalarAffineTerm(1.0, z2)], 0.0),
-    #   MOI.GreaterThan(0.0),
-    # )
-    #
-    # MOI.set(model, MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(),
-    #   MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.([2.0, 3.0], [x1, x2]), 0.0)
-    # )
-    # MOI.set(model, MOI.ObjectiveSense(), MOI.MAX_SENSE)
-    #
-    #
-    #
-    # test_delete_bridge(bridged_mock, ci, 4,
-    #                    ((MOI.VectorAffineFunction{Float64},
-    #                      MOI.PositiveSemidefiniteConeTriangle, 0),
-    #                     (MOI.ScalarAffineFunction{Float64},
-    #                      MOI.EqualTo{Float64}, 1)))
+    z1comp = bridge.variable_index
+    @test MOI.get(model, MOI.ConstraintFunction(), bridge.zero_one_cons) == MOI.SingleVariable(z1comp)
+    @test MOI.get(model, MOI.ConstraintSet(), bridge.disjunction_cons) == MOI.EqualTo(1.0)
+    disjunction_cons = MOI.get(model, MOI.ConstraintFunction(), bridge.disjunction_cons)
+    for t in disjunction_cons.terms
+      @test t.variable_index == z1 || t.variable_index == z1comp
+      @test t.coefficient ≈ 1.0
+    end
 end
