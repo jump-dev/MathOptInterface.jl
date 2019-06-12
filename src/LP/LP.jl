d2a3c9d6bf2c413f034876e84a177861bf290fed
Thibaut Cuvelier <dourouc05@gmail.com>
2019-06-12 20:05:05 +0200

    LP sanitisation (#71)
    * Update LP format to sanitise variable names.
    
    Code ported from LPWriter.jl, changed regex API so it runs in Julia 1.1.
    Other changes: constraints must have a "=" sign, say CPLEX and lp_solve; add an End keyword, says CPLEX.
    
    * Add a test case for the name sanitisation.
    
    * Adding more test cases, ported from LPWriter.
    
    * @odow's nit-picking :).
    
    * Implement a name cache.
    
    Based on https://stackoverflow.com/a/33783534
    
    * Rework the cache mechanism to pass it as an argument.
    
    * Handle created duplicate names in sanitisation.
    
    * Ensure the generated names are never too long.

--- src/LP/LP.jl
+++ src/LP/LP.jl
@@ -30,13 +30,47 @@ end
 #
 # ==============================================================================
 
-function write_function(io::IO, model::Model, func::MOI.SingleVariable)
-    name = MOI.get(model, MOI.VariableName(), func.variable)
-    print(io, name)
+
+const MAX_LENGTH = 255
+# 16 for lp_solve 5.0: http://lpsolve.sourceforge.net/5.0/CPLEX-format.htm
+# 255 for CPLEX 12.5: https://www.ibm.com/support/knowledgecenter/SS9UKU_12.5.0/com.ibm.cplex.zos.help/FileFormats/topics/LP.html
+const START_REG = r"^([\.0-9eE])"
+const NAME_REG = r"([^a-zA-Z0-9\!\"\#\$\%\&\(\)\/\,\.\;\?\@\_\`\'\{\}\|\~])"
+
+function sanitized_name(name::String)
+    m = match(START_REG, name)
+    if m !== nothing
+        plural = length(m.match) > 1
+        @warn("Name $(name) cannot start with a period, a number, e, or E. " *
+              "Prepending an underscore to name.")
+        return sanitized_name("_" * name)
+    end
+
+    m = match(NAME_REG, name)
+    if m !== nothing
+        plural = length(m.match) > 1
+        @warn("Name $(name) contains $(ifelse(plural, "", "an "))" *
+              "illegal character$(ifelse(plural, "s", "")): " *
+              "\"$(m.match)\". Removing the offending " *
+              "character$(ifelse(plural, "s", "")) from name.")
+        return sanitized_name(replace(name, NAME_REG => s"_"))
+    end
+
+    # Truncate at the end to fit as many characters as possible.
+    if length(name) > MAX_LENGTH
+        @warn("Name $(name) too long (length: $(length(name))). Truncating.")
+        return sanitized_name(String(name[1:MAX_LENGTH]))
+    end
+
+    return name
+end
+
+function write_function(io::IO, model::Model, func::MOI.SingleVariable, sanitized_names::Dict{MOI.VariableIndex, String})
+    print(io, sanitized_names[func.variable])
     return
 end
 
-function write_function(io::IO, model::Model, func::MOI.ScalarAffineFunction{Float64})
+function write_function(io::IO, model::Model, func::MOI.ScalarAffineFunction{Float64}, sanitized_names::Dict{MOI.VariableIndex, String})
     is_first_item = true
     if !(func.constant ≈ 0.0)
         Base.Grisu.print_shortest(io, func.constant)
@@ -51,7 +85,8 @@ function write_function(io::IO, model::Model, func::MOI.ScalarAffineFunction{Flo
                 print(io, term.coefficient < 0 ? " - " : " + ")
                 Base.Grisu.print_shortest(io, abs(term.coefficient))
             end
-            print(io, " ", MOI.get(model, MOI.VariableName(), term.variable_index))
+
+            print(io, " ", sanitized_names[term.variable_index])
         end
     end
     return
@@ -72,7 +107,7 @@ function write_constraint_suffix(io::IO, set::MOI.GreaterThan)
 end
 
 function write_constraint_suffix(io::IO, set::MOI.EqualTo)
-    print(io, " == ", )
+    print(io, " = ", )
     Base.Grisu.print_shortest(io, set.value)
     println(io)
     return
@@ -93,14 +128,14 @@ end
 
 write_constraint_prefix(io::IO, set) = nothing
 
-function write_constraint(io::IO, model::Model, index; write_name::Bool = true)
+function write_constraint(io::IO, model::Model, index, sanitized_names::Dict{MOI.VariableIndex, String}; write_name::Bool = true)
     func = MOI.get(model, MOI.ConstraintFunction(), index)
     set = MOI.get(model, MOI.ConstraintSet(), index)
     if write_name
         print(io, MOI.get(model, MOI.ConstraintName(), index), ": ")
     end
     write_constraint_prefix(io, set)
-    write_function(io, model, func)
+    write_function(io, model, func, sanitized_names)
     write_constraint_suffix(io, set)
 end
 
@@ -118,30 +153,59 @@ function write_sense(io::IO, model::Model)
     return
 end
 
-function write_objective(io::IO, model::Model)
+function write_objective(io::IO, model::Model, sanitized_names::Dict{MOI.VariableIndex, String})
     print(io, "obj: ")
     obj_func_type = MOI.get(model, MOI.ObjectiveFunctionType())
     obj_func = MOI.get(model, MOI.ObjectiveFunction{obj_func_type}())
-    write_function(io, model, obj_func)
+    write_function(io, model, obj_func, sanitized_names)
     println(io)
     return
 end
 
 function MOI.write_to_file(model::Model, io::IO)
     MathOptFormat.create_unique_names(model)
+    sanitized_names = Dict{MOI.VariableIndex, String}()
+    sanitized_names_set = Set{String}()
+    for v in MOI.get(model, MOI.ListOfVariableIndices())
+        proposed_sanitized_name = sanitized_name(MOI.get(model, MOI.VariableName(), v))
+
+        # In case of duplicate names after sanitization, add a number at the end.
+        if proposed_sanitized_name in sanitized_names_set
+            # If the name is already too long, make some space for the suffix.
+            if length(proposed_sanitized_name) >= MAX_LENGTH
+                proposed_sanitized_name = String(proposed_sanitized_name[1:MAX_LENGTH - 2])
+            end
+
+            i = 1
+            while proposed_sanitized_name * '_' * string(i) in sanitized_names_set
+                i += 1
+
+                # If the maximum length constraint would be broken with the *next* i,
+                # truncate a bit more the proposed_sanitized_name.
+                if length(proposed_sanitized_name * '_' * string(i)) > MAX_LENGTH
+                    proposed_sanitized_name = String(proposed_sanitized_name[1:length(proposed_sanitized_name) - 1])
+                end
+            end
+            proposed_sanitized_name *= '_' * string(i)
+        end
+
+        push!(sanitized_names_set, proposed_sanitized_name)
+        sanitized_names[v] = proposed_sanitized_name
+    end
+
     write_sense(io, model)
-    write_objective(io, model)
+    write_objective(io, model, sanitized_names)
     println(io, "subject to")
     for S in SCALAR_SETS
         for index in MOI.get(model, MOI.ListOfConstraintIndices{MOI.ScalarAffineFunction{Float64}, S}())
-            write_constraint(io, model, index; write_name = true)
+            write_constraint(io, model, index, sanitized_names; write_name = true)
         end
     end
 
     println(io, "Bounds")
     for S in SCALAR_SETS
         for index in MOI.get(model, MOI.ListOfConstraintIndices{MOI.SingleVariable, S}())
-            write_constraint(io, model, index; write_name = false)
+            write_constraint(io, model, index, sanitized_names; write_name = false)
         end
     end
 
@@ -150,11 +214,13 @@ function MOI.write_to_file(model::Model, io::IO)
         if length(indices) > 0
             println(io, str_S)
             for index in indices
-                write_function(io, model, MOI.get(model, MOI.ConstraintFunction(), index))
+                write_function(io, model, MOI.get(model, MOI.ConstraintFunction(), index), sanitized_names)
                 println(io)
             end
         end
     end
+
+    println(io, "End")
 
     return
 end
--- test/LP/LP.jl
+++ test/LP/LP.jl
@@ -28,18 +28,103 @@ const LP_TEST_FILE = "test.lp"
             "subject to\n" *
             "c5: 1.1 x <= 5.1\n" *
             "c6: -1.4 + 1.3 x >= -0.1\n" *
-            "c7: 1.6 + 1.5 a == 0.2\n" *
+            "c7: 1.6 + 1.5 a = 0.2\n" *
             "c8: 0.3 <= 1.8 + 1.7 a <= 0.4\n" *
             "Bounds\n" *
             "x <= 2\n" *
             "x >= -1\n" *
-            "y == 3\n" *
+            "y = 3\n" *
             "4 <= z <= 5\n" *
             "General\n" *
             "y\n" *
             "Binary\n" *
-            "x\n"
+            "x\n" *
+            "End\n"
     end
+
+    @testset "Name sanitisation" begin
+        @testset "sanitized_name" begin
+            @test LP.sanitized_name("x") == "x"
+            @test LP.sanitized_name(repeat("x", LP.MAX_LENGTH)) == repeat("x", LP.MAX_LENGTH)
+
+            too_long = repeat("x", LP.MAX_LENGTH + 1)
+            @test (@test_logs (:warn, "Name $(too_long) too long (length: $(length(too_long))). Truncating.") LP.sanitized_name(too_long)) == repeat("x", LP.MAX_LENGTH)
+
+            @test (@test_logs (:warn, "Name .x cannot start with a period, a number, e, or E. Prepending an underscore to name.") LP.sanitized_name(".x")) == "_.x"
+            @test (@test_logs (:warn, "Name 0x cannot start with a period, a number, e, or E. Prepending an underscore to name.") LP.sanitized_name("0x")) == "_0x"
+            @test (@test_logs (:warn, "Name Ex cannot start with a period, a number, e, or E. Prepending an underscore to name.") LP.sanitized_name("Ex")) == "_Ex"
+            @test (@test_logs (:warn, "Name ex cannot start with a period, a number, e, or E. Prepending an underscore to name.") LP.sanitized_name("ex")) == "_ex"
+
+            @test (@test_logs (:warn, "Name x*ds contains an illegal character: \"*\". Removing the offending character from name.") LP.sanitized_name("x*ds")) == "x_ds"
+            @test (@test_logs (:warn, "Name x^ contains an illegal character: \"^\". Removing the offending character from name.") LP.sanitized_name("x^")) == "x_"
+            @test (@test_logs (:warn, "Name x*ds[1] contains an illegal character: \"*\". Removing the offending character from name.") LP.sanitized_name("x*ds[1]")) == "x_ds_1_"
+            @test (@test_logs (:warn, "Name Ex*ds[1] cannot start with a period, a number, e, or E. Prepending an underscore to name.") (:warn, "Name _Ex*ds[1] contains an illegal character: \"*\". Removing the offending character from name.") LP.sanitized_name("Ex*ds[1]")) == "_Ex_ds_1_"
+        end
+
+        @testset "Whole chain" begin
+            model = LP.Model()
+            MOIU.loadfromstring!(model, """
+            variables: a
+            minobjective: a
+            c1: a >= -1.0
+            """)
+            MOI.set(model, MOI.VariableName(), MOI.get(model, MOI.ListOfVariableIndices())[1], "a[]")
+
+            @test_logs (:warn, "Name a[] contains an illegal character: \"[\". Removing the offending character from name.") MOI.write_to_file(model, LP_TEST_FILE)
+        end
+
+        @testset "Duplicate names after sanitization" begin
+            model = LP.Model()
+            MOIU.loadfromstring!(model, """
+            variables: a, b, c, d
+            minobjective: a + b + c + d
+            c1: a + b + c + d >= -1.0
+            """)
+            MOI.set(model, MOI.VariableName(), MOI.get(model, MOI.ListOfVariableIndices())[1], "a[")
+            MOI.set(model, MOI.VariableName(), MOI.get(model, MOI.ListOfVariableIndices())[2], "a]")
+            MOI.set(model, MOI.VariableName(), MOI.get(model, MOI.ListOfVariableIndices())[3], "a*")
+            MOI.set(model, MOI.VariableName(), MOI.get(model, MOI.ListOfVariableIndices())[4], "a^")
+
+            @test_logs (:warn, "Name a[ contains an illegal character: \"[\". Removing the offending character from name.") (:warn, "Name a] contains an illegal character: \"]\". Removing the offending character from name.") (:warn, "Name a* contains an illegal character: \"*\". Removing the offending character from name.") (:warn, "Name a^ contains an illegal character: \"^\". Removing the offending character from name.") MOI.write_to_file(model, LP_TEST_FILE)
+            @test read(LP_TEST_FILE, String) ==
+                "minimize\n" *
+                "obj: 1 a_ + 1 a__1 + 1 a__2 + 1 a__3\n" *
+                "subject to\n" *
+                "c1: 1 a_ + 1 a__1 + 1 a__2 + 1 a__3 >= -1\n" *
+                "Bounds\n" *
+                "End\n"
+        end
+
+        @testset "Too long duplicate names after sanitization" begin
+            model = LP.Model()
+            MOIU.loadfromstring!(model, """
+            variables: a, b, c, d
+            minobjective: a + b + c + d
+            c1: a + b + c + d >= -1.0
+            """)
+            MOI.set(model, MOI.VariableName(), MOI.get(model, MOI.ListOfVariableIndices())[1], "a[")
+            MOI.set(model, MOI.VariableName(), MOI.get(model, MOI.ListOfVariableIndices())[2], "a]")
+            MOI.set(model, MOI.VariableName(), MOI.get(model, MOI.ListOfVariableIndices())[3], "a*" * repeat("x", LP.MAX_LENGTH + 1))
+            MOI.set(model, MOI.VariableName(), MOI.get(model, MOI.ListOfVariableIndices())[4], "a^" * repeat("x", LP.MAX_LENGTH + 1))
+
+            @test_logs(
+                (:warn, "Name a[ contains an illegal character: \"[\". Removing the offending character from name."),
+                (:warn, "Name a] contains an illegal character: \"]\". Removing the offending character from name."),
+                (:warn, "Name a*$(repeat("x", LP.MAX_LENGTH + 1)) contains an illegal character: \"*\". Removing the offending character from name."),
+                (:warn, "Name a_$(repeat("x", LP.MAX_LENGTH + 1)) too long (length: $(2 + LP.MAX_LENGTH + 1)). Truncating."),
+                (:warn, "Name a^$(repeat("x", LP.MAX_LENGTH + 1)) contains an illegal character: \"^\". Removing the offending character from name."),
+                (:warn, "Name a_$(repeat("x", LP.MAX_LENGTH + 1)) too long (length: $(2 + LP.MAX_LENGTH + 1)). Truncating."),
+                MOI.write_to_file(model, LP_TEST_FILE))
+            @test read(LP_TEST_FILE, String) ==
+                "minimize\n" *
+                "obj: 1 a_ + 1 a__1 + 1 a_$(repeat("x", LP.MAX_LENGTH - 2)) + 1 a_$(repeat("x", LP.MAX_LENGTH - 4))_1\n" *
+                "subject to\n" *
+                "c1: 1 a_ + 1 a__1 + 1 a_$(repeat("x", LP.MAX_LENGTH - 2)) + 1 a_$(repeat("x", LP.MAX_LENGTH - 4))_1 >= -1\n" *
+                "Bounds\n" *
+                "End\n"
+        end
+    end
+
     @testset "other features" begin
         model = LP.Model()
         MOIU.loadfromstring!(model, """
@@ -51,7 +136,8 @@ const LP_TEST_FILE = "test.lp"
             "maximize\n" *
             "obj: -1 + 2 x\n" *
             "subject to\n" *
-            "Bounds\n"
+            "Bounds\n" *
+            "End\n"
     end
 end
 
