d3b5a3ef1fb47624d16866fbafedad8f8b3a7ea6
Benoît Legat <benoit.legat@gmail.com>
2019-07-21 21:43:09 -0400

    Add normalize_constant

--- src/Utilities/constraints.jl
+++ src/Utilities/constraints.jl
@@ -10,20 +10,26 @@ the set in `model`. If `allow_modify_function` is `true` then the function
 """
 function add_scalar_constraint end
 
-function add_scalar_constraint(model::MOI.ModelLike, func::MOI.SingleVariable,
-                               set::MOI.AbstractScalarSet;
-                               allow_modify_function::Bool=false)
-    return MOI.add_constraint(model, func, set)
-end
 function add_scalar_constraint(model::MOI.ModelLike,
-                               func::Union{MOI.ScalarAffineFunction{T},
-                                           MOI.ScalarQuadraticFunction{T}},
+                               func::MOI.AbstractScalarFunction,
                                set::MOI.AbstractScalarSet;
                                allow_modify_function::Bool=false) where T
+    return MOI.add_constraint(
+        model, normalize_constant(
+            func, set; allow_modify_function=allow_modify_function)...)
+end
+
+function normalize_constant(func::MOI.AbstractFunction, set::MOI.AbstractSet;
+                            allow_modify_function::Bool=false)
+    return func, set
+end
+function normalize_constant(
+    func::Union{MOI.ScalarAffineFunction{T}, MOI.ScalarQuadraticFunction{T}},
+    set::MOI.AbstractScalarSet; allow_modify_function::Bool=false) where T
     set = shift_constant(set, -func.constant)
     if !allow_modify_function
         func = copy(func)
     end
     func.constant = zero(T)
-    return MOI.add_constraint(model, func, set)
+    return func, set
 end
