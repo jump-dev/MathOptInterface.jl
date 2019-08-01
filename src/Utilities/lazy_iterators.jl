8b30e6e06bb4e386fe96df9196aa50b5418eca8c
Benoît Legat <benoit.legat@gmail.com>
2019-08-01 16:28:36 +0200

    Address comments

--- src/Utilities/lazy_iterators.jl
+++ src/Utilities/lazy_iterators.jl
@@ -4,6 +4,16 @@ Base.isempty(::EmptyVector) = true
 Base.eltype(::EmptyVector{T}) where {T} = T
 Base.iterate(::EmptyVector) = nothing
 
+"""
+    struct LazyMap{T, VT}
+        f::Function
+        data::VT
+    end
+
+Iterator over the elements of `data` mapped by `f`. This is similar to
+`Base.Generator(f, data)` except that the `eltype` of a `LazyMap` is given at
+construction while the `eltype` of `Base.Generator(f, data)` is `Any`.
+"""
 struct LazyMap{T, VT}
     f::Function
     data::VT
@@ -22,44 +32,3 @@ function Base.iterate(it::LazyMap, args...)
 end
 Base.IteratorSize(it::LazyMap) = Base.IteratorSize(it.data)
 Base.eltype(::LazyMap{T}) where {T} = T
-
-struct LazyFilter{T, VT}
-    f::Function
-    data::VT
-end
-LazyFilter(f::Function, data) = LazyFilter{eltype(data), typeof(data)}(f, data)
-function Base.iterate(it::LazyFilter, args...)
-    elem_state = iterate(it.data, args...)
-    while elem_state !== nothing && !it.f(elem_state[1])
-        elem_state = iterate(it.data, elem_state[2])
-    end
-    if elem_state === nothing
-        return nothing
-    else
-        return elem_state
-    end
-end
-Base.IteratorSize(::LazyFilter) = Base.SizeUnknown()
-Base.eltype(::LazyFilter{T}) where {T} = T
-
-struct LazyCat{VT}
-    data::VT
-end
-function _iterate(it::LazyCat, i, state)
-    while i < length(it.data) && state === nothing
-        i += 1
-        state = iterate(it.data[i])
-    end
-    if state === nothing
-        return nothing
-    else
-        return state[1], (i, state[2])
-    end
-end
-Base.iterate(it::LazyCat) = _iterate(it, 0, nothing)
-function Base.iterate(it::LazyCat, state)
-    i = state[1]
-    return _iterate(it, i, iterate(it.data[i], state[2]))
-end
-Base.IteratorSize(::LazyCat) = Base.SizeUnknown()
-Base.IteratorEltype(::LazyCat) = Base.EltypeUnknown()
--- test/Utilities/lazy_iterators.jl
+++ test/Utilities/lazy_iterators.jl
@@ -23,20 +23,3 @@ end
     @test c isa Vector{T}
     @test c == [4, 9]
 end
-
-@testset "LazyFilter{$T}" for T in [Int32, Int64]
-    v = MOIU.LazyFilter(isodd, T[2, 3, 5, 4])
-    @test eltype(v) == T
-    c = collect(v)
-    @test c isa Vector{T}
-    @test c == [3, 5]
-end
-
-@testset "LazyCat{$T}" for T in [Int, Float64]
-    v1 = T[2, 3]
-    v2 = T[5]
-    v = MOIU.LazyCat((v1, v2))
-    c = collect(v)
-    @test c isa Vector{T}
-    @test c == T[2, 3, 5]
-end
