1e7316274cb1987082fa8f109b2a886da66cb3e6
Chris Coey <coey.chris@gmail.com>
2019-08-09 09:59:21 +0200

    disallow VQF; start on duals thru new bridges

--- src/Bridges/Constraint/norm_to_lp.jl
+++ src/Bridges/Constraint/norm_to_lp.jl
@@ -6,7 +6,7 @@ The `NormInfinityCone` is representable with LP constraints, since ``t \\ge \\ma
 struct NormInfinityBridge{T} <: AbstractBridge
     ge_index::CI{MOI.VectorAffineFunction{T}, MOI.Nonnegatives}
 end
-function bridge_constraint(::Type{NormInfinityBridge{T}}, model, f::MOI.AbstractVectorFunction, s::MOI.NormInfinityCone) where T
+function bridge_constraint(::Type{NormInfinityBridge{T}}, model, f::Union{MOI.VectorOfVariables, MOI.VectorAffineFunction{T}}, s::MOI.NormInfinityCone) where T
     f_scalars = MOIU.eachscalar(f)
     t = f_scalars[1]
     if t isa MOI.SingleVariable # TODO currently needed for operate_output_index!
@@ -23,9 +23,9 @@ function bridge_constraint(::Type{NormInfinityBridge{T}}, model, f::MOI.Abstract
     return NormInfinityBridge(ge_index)
 end
 
-MOI.supports_constraint(::Type{NormInfinityBridge{T}}, ::Type{<:MOI.AbstractVectorFunction}, ::Type{MOI.NormInfinityCone}) where T = true
+MOI.supports_constraint(::Type{NormInfinityBridge{T}}, ::Type{<:Union{MOI.VectorOfVariables, MOI.VectorAffineFunction{T}}}, ::Type{MOI.NormInfinityCone}) where T = true
 MOIB.added_constrained_variable_types(::Type{<:NormInfinityBridge}) = Tuple{DataType}[]
-MOIB.added_constraint_types(::Type{NormInfinityBridge{T}}, ::Type{<:MOI.AbstractVectorFunction}, ::Type{MOI.NormInfinityCone}) where T = [(MOI.VectorAffineFunction{T}, MOI.Nonnegatives),]
+MOIB.added_constraint_types(::Type{NormInfinityBridge{T}}, ::Type{<:Union{MOI.VectorOfVariables, MOI.VectorAffineFunction{T}}}, ::Type{MOI.NormInfinityCone}) where T = [(MOI.VectorAffineFunction{T}, MOI.Nonnegatives),]
 
 # Attributes, Bridge acting as a model
 MOI.get(b::NormInfinityBridge{T}, ::MOI.NumberOfConstraints{MOI.VectorAffineFunction{T}, MOI.Nonnegatives}) where T = 1
@@ -44,6 +44,14 @@ function MOI.get(model::MOI.ModelLike, ::MOI.ConstraintPrimal, c::NormInfinityBr
     x = (ge_value[(d + 1):end] - ge_value[1:d]) / 2
     return vcat(t, x)
 end
+function MOI.get(model::MOI.ModelLike, ::MOI.ConstraintDual, c::NormInfinityBridge)
+    ge_dual = MOI.get(model, MOI.ConstraintDual(), c.ge_index)
+    t = sum(abs, ge_dual)
+    d = div(length(ge_dual), 2)
+    x = (ge_dual[(d + 1):end] - ge_dual[1:d])
+    return vcat(t, x)
+end
+
 
 """
     NormOneBridge{T}
@@ -55,7 +63,7 @@ struct NormOneBridge{T} <: AbstractBridge
     eq_index::CI{MOI.ScalarAffineFunction{T}, MOI.EqualTo{T}}
     ge_index::CI{MOI.VectorAffineFunction{T}, MOI.Nonnegatives}
 end
-function bridge_constraint(::Type{NormOneBridge{T}}, model, f::MOI.AbstractVectorFunction, s::MOI.NormOneCone) where T
+function bridge_constraint(::Type{NormOneBridge{T}}, model, f::Union{MOI.VectorOfVariables, MOI.VectorAffineFunction{T}}, s::MOI.NormOneCone) where T
     if f isa MOI.VectorOfVariables # TODO currently needed for test to pass
         f = MOI.VectorAffineFunction{T}(f)
     end
@@ -72,9 +80,9 @@ function bridge_constraint(::Type{NormOneBridge{T}}, model, f::MOI.AbstractVecto
     return NormOneBridge(y, eq_index, ge_index)
 end
 
-MOI.supports_constraint(::Type{NormOneBridge{T}}, ::Type{<:MOI.AbstractVectorFunction}, ::Type{MOI.NormOneCone}) where T = true
+MOI.supports_constraint(::Type{NormOneBridge{T}}, ::Type{<:Union{MOI.VectorOfVariables, MOI.VectorAffineFunction{T}}}, ::Type{MOI.NormOneCone}) where T = true
 MOIB.added_constrained_variable_types(::Type{<:NormOneBridge}) = Tuple{DataType}[]
-MOIB.added_constraint_types(::Type{NormOneBridge{T}}, ::Type{<:MOI.AbstractVectorFunction}, ::Type{MOI.NormOneCone}) where T = [(MOI.ScalarAffineFunction{T}, MOI.EqualTo{T}), (MOI.VectorAffineFunction{T}, MOI.Nonnegatives)]
+MOIB.added_constraint_types(::Type{NormOneBridge{T}}, ::Type{<:Union{MOI.VectorOfVariables, MOI.VectorAffineFunction{T}}}, ::Type{MOI.NormOneCone}) where T = [(MOI.ScalarAffineFunction{T}, MOI.EqualTo{T}), (MOI.VectorAffineFunction{T}, MOI.Nonnegatives)]
 
 # Attributes, Bridge acting as a model
 MOI.get(b::NormOneBridge, ::MOI.NumberOfVariables) = length(b.y)
@@ -98,5 +106,13 @@ function MOI.get(model::MOI.ModelLike, ::MOI.ConstraintPrimal, c::NormOneBridge)
     t = eq_value + sum(ge_value) / 2
     d = length(c.y)
     x = (ge_value[(d + 1):end] - ge_value[1:d]) / 2
+    return vcat(t, x)
+end
+function MOI.get(model::MOI.ModelLike, ::MOI.ConstraintDual, c::NormOneBridge)
+    eq_dual = MOI.get(model, MOI.ConstraintDual(), c.eq_index)
+    ge_dual = MOI.get(model, MOI.ConstraintDual(), c.ge_index)
+    t = eq_dual
+    d = div(length(ge_dual), 2)
+    x = (ge_dual[(d + 1):end] - ge_dual[1:d])
     return vcat(t, x)
 end
--- test/Bridges/Constraint/norm_to_lp.jl
+++ test/Bridges/Constraint/norm_to_lp.jl
@@ -9,24 +9,133 @@ const MOIB = MathOptInterface.Bridges
 include("../utilities.jl")
 
 mock = MOIU.MockOptimizer(MOIU.Model{Float64}())
-config = MOIT.TestConfig(duals = false)
+config = MOIT.TestConfig()
 
 @testset "NormInfinity" begin
-    mock.optimize! = (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock, [1.0, 0.5, 1.0])
     bridged_mock = MOIB.Constraint.NormInfinity{Float64}(mock)
+    mock.optimize! = (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock, [1.0, 0.5, 1.0],
+        (MOI.VectorAffineFunction{Float64}, MOI.Nonnegatives) => [[0.0, 1.0, 0.0, 0.0]])
     MOIT.norminf1vtest(bridged_mock, config)
     MOIT.norminf1ftest(bridged_mock, config)
-    # Dual is not yet implemented for NormInfinity bridge
     ci = first(MOI.get(bridged_mock, MOI.ListOfConstraintIndices{MOI.VectorAffineFunction{Float64}, MOI.NormInfinityCone}()))
     test_delete_bridge(bridged_mock, ci, 3, ((MOI.VectorAffineFunction{Float64}, MOI.Nonnegatives, 0),))
 end
 
 @testset "NormOne" begin
-    mock.optimize! = (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock, [1.0, 0.5, 0.5, 0.5, 0.5])
     bridged_mock = MOIB.Constraint.NormOne{Float64}(mock)
+    mock.optimize! = (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock, [1.0, 0.5, 0.5, 0.5, 0.5],
+        (MOI.VectorAffineFunction{Float64}, MOI.Nonnegatives) => [[1.0, 1.0, 0.0, 0.0]],
+        (MOI.ScalarAffineFunction{Float64}, MOI.EqualTo{Float64}) => [[1.0]],
+        )
     MOIT.normone1vtest(bridged_mock, config)
     MOIT.normone1ftest(bridged_mock, config)
-    # Dual is not yet implemented for NormOne bridge
     ci = first(MOI.get(bridged_mock, MOI.ListOfConstraintIndices{MOI.VectorAffineFunction{Float64}, MOI.NormOneCone}()))
     test_delete_bridge(bridged_mock, ci, 3, ((MOI.ScalarAffineFunction{Float64}, MOI.EqualTo{Float64}, 0), (MOI.VectorAffineFunction{Float64}, MOI.Nonnegatives, 0)))
 end
+
+# TODO adapt for these cones
+#
+# using Test
+#
+# using MathOptInterface
+# const MOI = MathOptInterface
+# const MOIT = MathOptInterface.Test
+# const MOIU = MathOptInterface.Utilities
+# const MOIB = MathOptInterface.Bridges
+#
+# include("../utilities.jl")
+#
+# mock = MOIU.MockOptimizer(MOIU.Model{Float64}())
+# config = MOIT.TestConfig()
+#
+# bridged_mock = MOIB.Variable.RSOCtoPSD{Float64}(mock)
+#
+# @testset "RSOC4" begin
+#     mock.optimize! = (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock, [1.0, 1.0, 2.0, 1.0, 0.0, 2.0],
+#         (MOI.ScalarAffineFunction{Float64}, MOI.EqualTo{Float64})  => [0.25],
+#         (MOI.SingleVariable, MOI.EqualTo{Float64})  => [-0.5],
+#         (MOI.ScalarAffineFunction{Float64}, MOI.LessThan{Float64}) => [-1.0],
+#         (MOI.VectorOfVariables, MOI.PositiveSemidefiniteConeTriangle) => [[1.0, -0.5, 0.25, -0.5, 0.25, 0.25]])
+#     mock.eval_variable_constraint_dual = false
+#     MOIT.rotatedsoc4test(bridged_mock, config)
+#     mock.eval_variable_constraint_dual = true
+#
+#     @testset "Test mock model" begin
+#         var_names = ["Q$i$j" for j in 1:3 for i in 1:j]
+#         MOI.set(
+#             mock, MOI.VariableName(),
+#             MOI.get(mock, MOI.ListOfVariableIndices()), var_names)
+#         psd = MOI.get(mock, MOI.ListOfConstraintIndices{
+#             MOI.VectorOfVariables, MOI.PositiveSemidefiniteConeTriangle}())
+#         @test length(psd) == 1
+#         MOI.set(mock, MOI.ConstraintName(), psd[1], "psd")
+#         off_diag = MOI.get(mock, MOI.ListOfConstraintIndices{
+#             MOI.SingleVariable, MOI.EqualTo{Float64}}())
+#         @test length(off_diag) == 1
+#         MOI.set(mock, MOI.ConstraintName(), off_diag[1], "off_diag23")
+#         diag = MOI.get(mock, MOI.ListOfConstraintIndices{
+#             MOI.ScalarAffineFunction{Float64}, MOI.EqualTo{Float64}}())
+#         @test length(diag) == 1
+#         MOI.set(mock, MOI.ConstraintName(), diag[1], "diag33")
+#         c = MOI.get(mock, MOI.ListOfConstraintIndices{
+#             MOI.ScalarAffineFunction{Float64}, MOI.LessThan{Float64}}())
+#         @test length(c) == 1
+#         MOI.set(mock, MOI.ConstraintName(), c[1], "c")
+#
+#         s = """
+#         variables: Q11, Q12, Q13, Q22, Q23, Q33
+#         psd: [Q11, Q12, Q22, Q13, Q23, Q33] in MathOptInterface.PositiveSemidefiniteConeTriangle(3)
+#         off_diag23: Q23 == 0.0
+#         diag33: Q22 + -1.0Q33 == 0.0
+#         c: Q11 + 0.5Q22 <= 2.0
+#         maxobjective: Q12 + Q13
+#         """
+#         model = MOIU.Model{Float64}()
+#         MOIU.loadfromstring!(model, s)
+#         MOIU.test_models_equal(mock, model, var_names, ["psd", "off_diag23", "diag33", "c"])
+#     end
+#
+#     @testset "Test bridged model" begin
+#         var_names = ["t", "u", "x", "y"]
+#         MOI.set(
+#             bridged_mock, MOI.VariableName(),
+#             MOI.get(bridged_mock, MOI.ListOfVariableIndices()), var_names)
+#         rsoc = MOI.get(bridged_mock, MOI.ListOfConstraintIndices{
+#             MOI.VectorOfVariables, MOI.RotatedSecondOrderCone}())
+#         @test length(rsoc) == 1
+#         MOI.set(bridged_mock, MOI.ConstraintName(), rsoc[1], "rsoc")
+#         c = MOI.get(bridged_mock, MOI.ListOfConstraintIndices{
+#             MOI.ScalarAffineFunction{Float64}, MOI.LessThan{Float64}}())
+#         @test length(c) == 1
+#         MOI.set(bridged_mock, MOI.ConstraintName(), c[1], "c")
+#
+#         s = """
+#         variables: t, u, x, y
+#         rsoc: [t, u, x, y] in MathOptInterface.RotatedSecondOrderCone(4)
+#         c: t + u <= 2.0
+#         maxobjective: x + y
+#         """
+#         model = MOIU.Model{Float64}()
+#         MOIU.loadfromstring!(model, s)
+#         MOIU.test_models_equal(bridged_mock, model, var_names, ["rsoc", "c"])
+#     end
+#
+#
+#     @testset "Delete" begin
+#         v = MOI.get(bridged_mock, MOI.ListOfVariableIndices())
+#         @test length(v) == 4
+#
+#         message = string("Cannot delete variable as it is constrained with other",
+#                          " variables in a `MOI.VectorOfVariables`.")
+#         for i in 1:4
+#             err = MOI.DeleteNotAllowed(v[i], message)
+#             @test_throws err MOI.delete(bridged_mock, v[i])
+#         end
+#
+#         test_delete_bridged_variables(bridged_mock, v, MOI.RotatedSecondOrderCone, 4, (
+#             (MOI.VectorOfVariables, MOI.PositiveSemidefiniteConeTriangle, 0),
+#             (MOI.SingleVariable, MOI.EqualTo{Float64}, 0),
+#             (MOI.ScalarAffineFunction{Float64}, MOI.EqualTo{Float64}, 0),
+#         ))
+#     end
+# end
