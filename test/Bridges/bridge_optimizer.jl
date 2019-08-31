3414ade5b4baf7eff4ea11bf65e999222f224644
Eric <5846501+ericphanson@users.noreply.github.com>
2019-08-31 20:42:57 +0100

    Add test

--- test/Bridges/bridge_optimizer.jl
+++ test/Bridges/bridge_optimizer.jl
@@ -180,3 +180,17 @@ end
     exclude = ["partial_start"] # VariablePrimalStart not supported.
     MOIT.contlineartest(bridged_mock, MOIT.TestConfig(solve=false), exclude)
 end
+
+@testset "Show" begin
+    io = IOBuffer()
+    output = raw"""
+    MOIB.Constraint.SingleBridgeOptimizer{MOIB.Constraint.LessToGreaterBridge{Float64,F,G} where G<:MOI.AbstractScalarFunction where F<:MOI.AbstractScalarFunction,MOIB.Constraint.SingleBridgeOptimizer{MOIB.Constraint.SplitIntervalBridge{Float64,F} where F<:MOI.AbstractScalarFunction,MOIU.MockOptimizer{NoIntervalModel{Float64}}}}
+    with 1 constraint bridge
+    with inner model MOIB.Constraint.SingleBridgeOptimizer{MOIB.Constraint.SplitIntervalBridge{Float64,F} where F<:MOI.AbstractScalarFunction,MOIU.MockOptimizer{NoIntervalModel{Float64}}}
+      with 0 constraint bridges
+      with inner model MOIU.MockOptimizer{NoIntervalModel{Float64}}"""
+    show(io, bridged_mock)
+    str = String(take!(io))
+    @test str == output
+
+end
