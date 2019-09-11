7506209b24600fdc386568f832f9e782e0fe1f6f
Mathieu Besançon <mathieu.besancon@gmail.com>
2019-09-11 15:30:47 +0200

    indicator to SOS bridge

--- src/Bridges/Constraint/indicator.jl
+++ src/Bridges/Constraint/indicator.jl
@@ -51,3 +51,89 @@ function concrete_bridge_type(::Type{<:IndicatorActiveOnFalseBridge},
                               ::Type{MOI.IndicatorSet{MOI.ACTIVATE_ON_ZERO, S}}) where {F<:MOI.VectorAffineFunction, S<:MOI.AbstractScalarSet}
     return IndicatorActiveOnFalseBridge{Float64, F, S}
 end
+
+## Indicator constraint by SOS1 bridge
+
+"""
+    IndicatorSOS1Bridge{T, BC <: Union{MOI.LessThan{T}, MOI.GreaterThan{T}, MOI.EqualTo{T}}}
+
+The `IndicatorSOS1Bridge` replaces an indicator constraint of the following form:
+``z \\in \\mathbb{B}, z == 1 \\implies f(x) \\leq b`` with a SOS1 constraint:
+``z \\in \\mathbb{B}, w \\leq 0, f(x) + w \\leq b, SOS1(w, z)``.
+`GreaterThan` constraints are handled in a symmetric way:
+``z \\in \\mathbb{B}, z == 1 \\implies f(x) \\geq b`` is reformulated as:
+``z \\in \\mathbb{B}, w \\geq 0, f(x) + w \\geq b, SOS1(w, z)``.
+`EqualTo` constraints are handled without a bound constraint:
+``z \\in \\mathbb{B}, z == 1 \\implies f(x) == b`` is reformulated as:
+``z \\in \\mathbb{B}, w \\text{ free}, f(x) + w == b, SOS1(w, z)``.
+
+If `BC <: EqualTo`, `bound_constraint_index` is `nothing`.
+"""
+struct IndicatorSOS1Bridge{T, BC <: Union{MOI.LessThan{T}, MOI.GreaterThan{T}, MOI.EqualTo{T}}, MaybeBC <: Union{MOI.ConstraintIndex{MOI.SingleVariable, BC}, Nothing}} <: AbstractBridge
+    w_variable_index::MOI.VariableIndex
+    bound_constraint_index::MaybeBC
+    sos_constraint_index::MOI.ConstraintIndex{MOI.VectorOfVariables, MOI.SOS1{T}}
+    linear_constraint_index::MOI.ConstraintIndex{MOI.ScalarAffineFunction{T}, BC}
+end
+
+function bridge_constraint(::Type{IndicatorSOS1Bridge{T,BC,MaybeBC}}, model::MOI.ModelLike, f::MOI.VectorAffineFunction{T}, s::MOI.IndicatorSet{MOI.ACTIVATE_ON_ONE, BC}) where {T <: Real, BC, MaybeBC}
+    f_scalars = MOIU.eachscalar(f)
+    (w, bound_constraint) = _add_bound_constraint!(model, BC)
+    z = f_scalars[1].terms[1].variable_index
+    sos_vector = MOI.VectorOfVariables([w, z])
+    sos_constraint = MOI.add_constraint(model, sos_vector, MOI.SOS1{T}([0.4, 0.6]))
+    affine_func = copy(f_scalars[2])
+    push!(
+        affine_func.terms,
+        MOI.ScalarAffineTerm{T}(one(T), w)
+    )
+    linear_constraint = MOI.add_constraint(model, affine_func, s.set)
+    return IndicatorSOS1Bridge{T,BC,MaybeBC}(w, bound_constraint, sos_constraint, linear_constraint)
+end
+
+function _add_bound_constraint!(model::MOI.ModelLike, ::Type{BC}) where {T <: Real, BC <: Union{MOI.LessThan{T}, MOI.GreaterThan{T}}}
+    return MOI.add_constrained_variable(model, BC(zero(T)))
+end
+
+function _add_bound_constraint!(model::MOI.ModelLike, ::Type{<:MOI.EqualTo})
+    return (MOI.add_variable(model), nothing)
+end
+
+function MOI.supports_constraint(::Type{<:IndicatorSOS1Bridge},
+                                 ::Type{<:MOI.VectorAffineFunction},
+                                 ::Type{<:MOI.IndicatorSet{MOI.ACTIVATE_ON_ONE, <: Union{MOI.LessThan, MOI.GreaterThan, MOI.EqualTo}}})
+    return true
+end
+
+function MOIB.added_constrained_variable_types(::Type{<:IndicatorSOS1Bridge{T, BC}}) where {T, BC <: Union{MOI.LessThan{T}, MOI.GreaterThan{T}}}
+    return [(MOI.ZeroOne,), (BC,)]
+end
+
+function MOIB.added_constrained_variable_types(::Type{<:IndicatorSOS1Bridge{T, <:MOI.EqualTo}}) where {T}
+    return [(MOI.ZeroOne,)]
+end
+
+function MOIB.added_constraint_types(::Type{<:IndicatorSOS1Bridge{T, BC}}) where {T, BC <: <: Union{MOI.LessThan{T}, MOI.GreaterThan{T}}}
+    return [(MOI.SingleVariable, BC),
+            (MOI.VectorOfVariables, MOI.SOS1{T}),
+            (MOI.ScalarAffineFunction{T}, BC),
+            ]
+end
+
+function MOIB.added_constraint_types(::Type{<:IndicatorSOS1Bridge{T, <:EqualTo}}) where {T}
+    return [(MOI.VectorOfVariables, MOI.SOS1{T}),
+            (MOI.ScalarAffineFunction{T}, EqualTo{T}),
+            ]
+end
+
+function concrete_bridge_type(::Type{<:IndicatorSOS1Bridge},
+                              ::Type{<:MOI.VectorAffineFunction},
+                              ::Type{MOI.IndicatorSet{MOI.ACTIVATE_ON_ONE, S}}) where {T, S<:Union{MOI.LessThan{T}, MOI.GreaterThan{T}}}
+    return IndicatorSOS1Bridge{T, S, MOI.ConstraintIndex{MOI.SingleVariable, S}}
+end
+
+function concrete_bridge_type(::Type{<:IndicatorSOS1Bridge},
+                              ::Type{<:MOI.VectorAffineFunction},
+                              ::Type{MOI.IndicatorSet{MOI.ACTIVATE_ON_ONE, MOI.EqualTo{T}}}) where {T}
+    return IndicatorSOS1Bridge{T, MOI.EqualTo{T}, Nothing}
+end
