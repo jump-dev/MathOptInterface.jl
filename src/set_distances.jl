90d36314437742b3ce6d761c789b6a1f41f26976
Mathieu Besançon <mathieu.besancon@gmail.com>
2020-02-13 12:30:53 +0100

    vectorized versions

--- src/set_distances.jl
+++ src/set_distances.jl
@@ -52,47 +52,35 @@ end
 function unsigned_distance(v::AbstractVector{<:Real}, ::SecondOrderCone)
     t = v[1]
     xs = v[2:end]
-    return max(-t, dot(xs, xs) - t^2) # avoids sqrt
+    return [-t, dot(xs, xs) - t^2] # avoids sqrt
 end
 
 function unsigned_distance(v::AbstractVector{<:Real}, ::RotatedSecondOrderCone)
     t = v[1]
     u = v[2]
     xs = v[3:end]
-    return max(
-        -t, -u,
-        dot(xs, xs) - 2 * t * u
-    )
+    return [-t, -u, dot(xs, xs) - 2 * t * u]
 end
 
 function unsigned_distance(v::AbstractVector{<:Real}, s::GeometricMeanCone)
     t = v[1]
     xs = v[2:end]
     n = dimension(s) - 1
-    return max(
-        -minimum(xs),
-        t^n - prod(xs),
-    )
+    return [-minimum(xs), t^n - prod(xs)]
 end
 
 function unsigned_distance(v::AbstractVector{<:Real}, ::ExponentialCone)
     x = v[1]
     y = v[2]
     z = v[3]
-    return max(
-        y,
-        y * exp(x/y) - z,
-    )
+    return [y, y * exp(x/y) - z]
 end
 
 function unsigned_distance(v::AbstractVector{<:Real}, ::DualExponentialCone)
     u = v[1]
     v = v[2]
     w = v[3]
-    return max(
-        -u,
-        -u*exp(v/u) - ℯ * w
-    )
+    return [-u, -u*exp(v/u) - ℯ * w]
 end
 
 function unsigned_distance(v::AbstractVector{<:Real}, s::PowerCone)
@@ -100,11 +88,7 @@ function unsigned_distance(v::AbstractVector{<:Real}, s::PowerCone)
     y = v[2]
     z = v[3]
     e = s.exponent
-    return max(
-        x,
-        y,
-        abs(z) - x^e * y^(1-e)
-    )
+    return [-x, -y, abs(z) - x^e * y^(1-e)]
 end
 
 function unsigned_distance(v::AbstractVector{<:Real}, s::DualPowerCone)
@@ -113,11 +97,7 @@ function unsigned_distance(v::AbstractVector{<:Real}, s::DualPowerCone)
     w = v[3]
     e = s.exponent
     ce = 1-e
-    return max(
-        u,
-        v,
-        abs(w) - (u/e)^e * (v/ce)^ce
-    )
+    return [u, v, abs(w) - (u/e)^e * (v/ce)^ce]
 end
 
 function unsigned_distance(v::AbstractVector{<:Real}, set::RelativeEntropyCone)
@@ -127,10 +107,7 @@ function unsigned_distance(v::AbstractVector{<:Real}, set::RelativeEntropyCone)
     v = v[2:(n+1)]
     w = v[(n+2):end]
     s = sum(w[i] * log(w[i]/v[i]) for i in eachindex(w))
-    return max(
-        -minimum(v[2:end]),
-        s - u,
-    )
+    return [-minimum(v[2:end]), s - u]
 end
 
 
@@ -158,34 +135,20 @@ function unsigned_distance(v::Real, ::Integer)
     return min(abs(v - floor(v)), abs(v - ceil(v)))
 end
 
-# return the second largest absolute value (=0 if SOS1 respected)
-function unsigned_distance(v::AbstractVector{T}, s::SOS1) where {T <: Real}
-    first_non_zero = second_non_zero = -1
-    v1 = zero(T)
-    v2 = zero(T)
-    for (i, x) in enumerate(v)
-        if !isapprox(x, 0)
-            if first_non_zero < 0
-                first_non_zero = i
-                v1 = x
-            elseif second_non_zero < 0
-                second_non_zero = i
-                v2 = x
-            else # replace one of the two
-                xa = abs(x)
-                if xa > v1
-                    second_non_zero = first_non_zero
-                    v2 = v1
-                    first_non_zero = i
-                    v1 = xa
-                elseif v[second_non_zero] <= x < v[first_non_zero]
-                    second_non_zero = i
-                    v2 = xa
-                end                                    
-            end
+# return the element-wise distance to zero, with the greatest element to 0
+function unsigned_distance(v::AbstractVector{T}, ::SOS1) where {T <: Real}
+    d = unsigned_distance(v, Zeros(length(v)))
+    m = maximum(d)
+    if m ≈ zero(T)
+        return d
+    end
+    # removing greatest distance
+    for i in eachindex(d)
+        @inbounds if d[i] == m
+            d[i] = zero(T)
+            return d
         end
     end
-    return v2
 end
 
 # TODO SOS2
