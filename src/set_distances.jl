b3678b587791287c44e0683db5caf3dc372a3a20
Mathieu Besançon <mathieu.besancon@gmail.com>
2020-02-20 10:24:12 +0100

    check all max locally

--- src/set_distances.jl
+++ src/set_distances.jl
@@ -1,12 +1,3 @@
-
-function _correct_distance_to_set(d::Real)
-    return ifelse(d < 0, zero(d), d)
-end
-
-function _correct_distance_to_set(d::AbstractVector{T}) where {T <: Real}
-    return [_correct_distance_to_set(di) for di in d]
-end
-
 """
     distance_to_set(v, s)
 
@@ -18,128 +9,154 @@ Each set `S` implements `distance_to_set(v::T, s::S)` with `T` of appropriate ty
 """
 function distance_to_set end
 
-distance_to_set(v::Real, s::LessThan) = _correct_distance_to_set(v - s.upper)
-distance_to_set(v::Real, s::GreaterThan) = _correct_distance_to_set(s.lower - v)
+distance_to_set(v::Real, s::LessThan) = max(v - s.upper, zero(v))
+distance_to_set(v::Real, s::GreaterThan) = max(s.lower - v, zero(v))
 distance_to_set(v::Real, s::EqualTo) = abs(v - s.value)
 
 distance_to_set(v::T, s::Interval) where {T <: Real} = max(s.lower - v, v - s.upper, zero(T))
 
-function distance_to_set(v::AbstractVector{<:Real}, s::Reals)
+function _check_dimension(v::AbstractVector, s)
     length(v) != dimension(s) && throw(DimensionMismatch("Mismatch between value and set"))
+    return nothing
+end
+
+function distance_to_set(v::AbstractVector{<:Real}, s::Reals)
+    _check_dimension(v, s)
     return zeros(length(v))
 end
 
 function distance_to_set(v::AbstractVector{<:Real}, s::Zeros)
-    length(v) != dimension(s) && throw(DimensionMismatch("Mismatch between value and set"))
+    _check_dimension(v, s)
     return abs.(v)
 end
 
 function distance_to_set(v::AbstractVector{<:Real}, s::Nonnegatives)
-    length(v) != dimension(s) && throw(DimensionMismatch("Mismatch between value and set"))
-    return _correct_distance_to_set(-v)
+    _check_dimension(v, s)
+    return [ifelse(vi < 0, -vi, zero(vi)) for vi in v]
 end
 function distance_to_set(v::AbstractVector{<:Real}, s::Nonpositives)
-    length(v) != dimension(s) && throw(DimensionMismatch("Mismatch between value and set"))
-    return _correct_distance_to_set(v)
+    _check_dimension(v, s)
+    return [ifelse(vi > 0, vi, zero(vi)) for vi in v]
 end
 
 # Norm cones
 
 function distance_to_set(v::AbstractVector{<:Real}, s::NormInfinityCone)
-    length(v) != dimension(s) && throw(DimensionMismatch("Mismatch between value and set"))
+    _check_dimension(v, s)
     t = first(v)
     xs = v[2:end]
     return [max(t - abs(x), zero(t)) for x in xs]
 end
 
 function distance_to_set(v::AbstractVector{<:Real}, s::NormOneCone)
-    length(v) != dimension(s) && throw(DimensionMismatch("Mismatch between value and set"))
+    _check_dimension(v, s)
     t = v[1]
     xs = v[2:end]
-    return _correct_distance_to_set(t - sum(abs, xs))
+    result = t - sum(abs, xs)
+    return max(result, zero(result))
 end
 
 function distance_to_set(v::AbstractVector{<:Real}, s::SecondOrderCone)
-    length(v) != dimension(s) && throw(DimensionMismatch("Mismatch between value and set"))
+    _check_dimension(v, s)
     t = v[1]
     xs = v[2:end]
-    return _correct_distance_to_set([-t, dot(xs, xs) - t^2]) # avoids sqrt
+    result = LinearAlgebra.norm(xs) - t
+    return max(result, zero(result)) # avoids sqrt
 end
 
 function distance_to_set(v::AbstractVector{<:Real}, s::RotatedSecondOrderCone)
-    length(v) != dimension(s) && throw(DimensionMismatch("Mismatch between value and set"))
+    _check_dimension(v, s)
     t = v[1]
     u = v[2]
     xs = v[3:end]
-    return _correct_distance_to_set([-t, -u, dot(xs, xs) - 2 * t * u])
+    return [
+        max(-t, zero(t)),
+        max(-u, zero(u)),
+        max(dot(xs,xs) - 2 * t * u),
+    ]
 end
 
 function distance_to_set(v::AbstractVector{<:Real}, s::GeometricMeanCone)
-    length(v) != dimension(s) && throw(DimensionMismatch("Mismatch between value and set"))
+    _check_dimension(v, s)
     t = v[1]
     xs = v[2:end]
     n = dimension(s) - 1
-    return _correct_distance_to_set([-minimum(xs), t^n - prod(xs)])
+    result = t^n - prod(xs)
+    return push!(
+        max.(xs, zero(result)), # x >= 0
+        max(result, zero(result)),
+    )
 end
 
 function distance_to_set(v::AbstractVector{<:Real}, s::ExponentialCone)
-    length(v) != dimension(s) && throw(DimensionMismatch("Mismatch between value and set"))
+    _check_dimension(v, s)
     x = v[1]
     y = v[2]
     z = v[3]
-    return _correct_distance_to_set([y, y * exp(x/y) - z])
+    result = y * exp(x/y) - z
+    return [max(y, zero(result)), max(result, zero(result))]
 end
 
 function distance_to_set(v::AbstractVector{<:Real}, s::DualExponentialCone)
-    length(v) != dimension(s) && throw(DimensionMismatch("Mismatch between value and set"))
+    _check_dimension(v, s)
     u = v[1]
     v = v[2]
     w = v[3]
-    return _correct_distance_to_set([-u, -u*exp(v/u) - ℯ * w])
+    result = -u*exp(v/u) - ℯ * w
+    return [max(-u, zero(result)), max(result, zero(result))]
 end
 
 function distance_to_set(v::AbstractVector{<:Real}, s::PowerCone)
-    length(v) != dimension(s) && throw(DimensionMismatch("Mismatch between value and set"))
+    _check_dimension(v, s)
     x = v[1]
     y = v[2]
     z = v[3]
     e = s.exponent
-    return _correct_distance_to_set([-x, -y, abs(z) - x^e * y^(1-e)])
+    result = abs(z) - x^e * y^(1-e)
+    return [max(-x, zero(result)), max(-y, zero(result)), max(result, zero(result))]
 end
 
 function distance_to_set(v::AbstractVector{<:Real}, s::DualPowerCone)
-    length(v) != dimension(s) && throw(DimensionMismatch("Mismatch between value and set"))
+    _check_dimension(v, s)
     u = v[1]
     v = v[2]
     w = v[3]
     e = s.exponent
     ce = 1-e
-    return _correct_distance_to_set([u, v, abs(w) - (u/e)^e * (v/ce)^ce])
+    result = abs(w) - (u/e)^e * (v/ce)^ce
+    return [max(-u, zero(result)), max(-v, zero(result)), max(result, zero(result))]
 end
 
 function distance_to_set(v::AbstractVector{<:Real}, set::RelativeEntropyCone)
-    length(v) != dimension(set) && throw(DimensionMismatch("Mismatch between value and set"))
-    all(>=(0), v[2:end]) || return false
+    _check_dimension(v, s)
     n = (dimension(set)-1) ÷ 2
     u = v[1]
     v = v[2:(n+1)]
     w = v[(n+2):end]
     s = sum(w[i] * log(w[i]/v[i]) for i in eachindex(w))
-    return _correct_distance_to_set([-minimum(v[2:end]), s - u])
+    result = s - u
+    return push!(
+        max.(v[2:end], zero(result)),
+        max(result, zero(result)),
+    )
 end
 
 function distance_to_set(v::AbstractVector{<:Real}, s::NormSpectralCone)
+    _check_dimension(v, s)
     t = v[1]
     m = reshape(v[2:end], (s.row_dim, s.column_dim))
     s1 = LinearAlgebra.svd(m).S[1]
-    return _correct_distance_to_set(s1 - t)
+    result = s1 - t
+    return max(result, zero(result))
 end
 
 function distance_to_set(v::AbstractVector{<:Real}, s::NormNuclearCone)
+    _check_dimension(v, s)
     t = v[1]
     m = reshape(v[2:end], (s.row_dim, s.column_dim))
     s1 = sum(LinearAlgebra.svd(m).S)
-    return _correct_distance_to_set(s1 - t)
+    result = s1 - t
+    return max(result, zero(result))
 end
 
 ## Integer sets
@@ -154,6 +171,7 @@ end
 
 # return the element-wise distance to zero, with the greatest element to 0
 function distance_to_set(v::AbstractVector{T}, ::SOS1) where {T <: Real}
+    _check_dimension(v, s)
     d = distance_to_set(v, Zeros(length(v)))
     m = maximum(d)
     if m ≈ zero(T)
@@ -172,6 +190,7 @@ end
 
 # takes in input [z, f(x)]
 function distance_to_set(v::AbstractVector{T}, s::IndicatorSet{A}) where {A, T <: Real}
+    _check_dimension(v, s)
     z = v[1]
     # inactive constraint
     if A === ACTIVATE_ON_ONE && isapprox(z, 0) || A === ACTIVATE_ON_ZERO && isapprox(z, 1)
