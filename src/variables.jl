d76af20726fd824c90ef27fd69cab2c94203c724
Thibaut Cuvelier <dourouc05@gmail.com>
2020-04-12 03:22:59 +0200

    Typo in doc.

--- src/variables.jl
+++ src/variables.jl
@@ -147,7 +147,7 @@ supports_add_constrained_variables(::ModelLike, ::Type{Reals}) = true
 """
     add_constrained_variables(
         model::ModelLike,
-        sets:AbstractVector{<:AbstractScalarSet}
+        sets::AbstractVector{<:AbstractScalarSet}
     )::Tuple{Vector{MOI.VariableIndex},
              Vector{MOI.ConstraintIndex{MOI.SingleVariable, eltype(sets)}}}
 
