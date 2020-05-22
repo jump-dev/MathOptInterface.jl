83a9a9864ac24e498bc404fdc7cc02eaa55c081f
Thibaut Cuvelier <dourouc05@gmail.com>
2020-05-22 20:06:48 +0200

    Update attributes.jl

--- src/attributes.jl
+++ src/attributes.jl
@@ -920,14 +920,17 @@ most recent call to [`compute_conflict!`](@ref).
 Possible values are:
 * `COMPUTE_CONFLICT_NOT_CALLED`: the function [`compute_conflict!`](@ref) has
   not yet been called
+* `NO_CONFLICT_EXISTS`: there is no conflict available, because the solver could 
+  prove that the problem is feasible (i.e. there are no conflicts)
 * `NO_CONFLICT_FOUND`: there is no conflict available. There may be several
   reasons for this: the function [`compute_conflict!`](@ref) has finished
   and decided that the problem is feasible, the function has failed to 
-  completely execute
+  completely execute (e.g., because it met a time limit)
 * `CONFLICT_FOUND`: at least one conflict could be found
 """
 @enum ConflictStatusCode begin
     COMPUTE_CONFLICT_NOT_CALLED
+    NO_CONFLICT_EXISTS
     NO_CONFLICT_FOUND
     CONFLICT_FOUND
 end
