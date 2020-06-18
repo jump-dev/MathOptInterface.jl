954a23c014116bfc28a4a7dbff65006ee12ef955
Ryan Walker <rtwalker@psu.edu>
2020-06-18 17:24:46 -0400

    change less than and greater than into interval

--- src/Bridges/Constraint/zero_one.jl
+++ src/Bridges/Constraint/zero_one.jl
@@ -1,27 +1,25 @@
 """
     ZeroOneBridge{T}
 
-The `ZeroOneBridge` splits a `F`-in-`MOI.ZeroOne` constraint into a `F`-in-`MOI.Integer` constraint,
-a `F`-in-`MOI.LessThan(1)` constraint, and a `F`-in-`MOI.GreaterThan(0)` constraint.
+The `ZeroOneBridge` splits a `MOI.SingleVariable`-in-`MOI.ZeroOne` constraint
+into a `MOI.SingleVariable`-in-`MOI.Integer` constraint
+and a `MOI.SingleVariable`-in-`MOI.Interval(0,1)` constraint.
 """
 struct ZeroOneBridge{T} <: AbstractBridge
     variable_index::MOI.VariableIndex
-    zero_index::MOI.ConstraintIndex{MOI.SingleVariable, MOI.GreaterThan{T}}
-    one_index::MOI.ConstraintIndex{MOI.SingleVariable, MOI.LessThan{T}}
+    interval_index::MOI.ConstraintIndex{MOI.SingleVariable, MOI.Interval{T}}
     integer_index::MOI.ConstraintIndex{MOI.SingleVariable, MOI.Integer}
 end
 
 function bridge_constraint(::Type{ZeroOneBridge{T}}, model::MOI.ModelLike, f::MOI.SingleVariable, ::MOI.ZeroOne) where {T <: Real}
-    zero_index = MOI.add_constraint(model, f, MOI.GreaterThan{T}(zero(T)))
-    one_index = MOI.add_constraint(model, f, MOI.LessThan{T}(one(T)))
+    interval_index = MOI.add_constraint(model, f, MOI.Interval{T}(zero(T), one(T)))
     integer_index = MOI.add_constraint(model, f, MOI.Integer())
-    return ZeroOneBridge{T}(f.variable, zero_index, one_index, integer_index)
+    return ZeroOneBridge{T}(f.variable, interval_index, integer_index)
 end
 
 function MOIB.added_constraint_types(::Type{<:ZeroOneBridge{T}}) where {T}
     return [
-            (MOI.SingleVariable, MOI.LessThan{T}),
-            (MOI.SingleVariable, MOI.GreaterThan{T}),
+            (MOI.SingleVariable, MOI.Interval{T}),
             (MOI.SingleVariable, MOI.Integer),
             ]
 end
@@ -49,8 +47,7 @@ function MOI.get(model::MOI.ModelLike, attr::MOI.ConstraintFunction, bridge::Zer
 end
 
 function MOI.delete(model::MOI.ModelLike, bridge::ZeroOneBridge)
-    MOI.delete(model, bridge.zero_index)
-    MOI.delete(model, bridge.one_index)
+    MOI.delete(model, bridge.interval_index)
     MOI.delete(model, bridge.integer_index)
 end
 
@@ -72,19 +69,13 @@ end
 function MOI.set(model::MOI.ModelLike, attr::MOI.ConstraintPrimalStart,
                  bridge::ZeroOneBridge{T}, value) where {T}
     MOI.set(model, MOI.VariablePrimalStart(), bridge.variable_index, value)
-    MOI.set(model, MOI.ConstraintPrimalStart(), bridge.zero_index, value)
-    MOI.set(model, MOI.ConstraintPrimalStart(), bridge.one_index, value)
+    MOI.set(model, MOI.ConstraintPrimalStart(), bridge.interval_index, value)
     return
 end
 
 # Attributes, Bridge acting as a model
 function MOI.get(bridge::ZeroOneBridge{T},
-    ::MOI.NumberOfConstraints{MOI.SingleVariable, MOI.GreaterThan{T}}) where {T}
-    return 1
-end
-
-function MOI.get(bridge::ZeroOneBridge{T},
-    ::MOI.NumberOfConstraints{MOI.SingleVariable, MOI.LessThan{T}}) where {T}
+    ::MOI.NumberOfConstraints{MOI.SingleVariable, MOI.Interval{T}}) where {T}
     return 1
 end
 
@@ -94,13 +85,8 @@ function MOI.get(bridge::ZeroOneBridge{T},
 end
 
 function MOI.get(bridge::ZeroOneBridge{T},
-    ::MOI.ListOfConstraintIndices{MOI.SingleVariable, MOI.GreaterThan{T}}) where {T}
-    return [bridge.zero_index]
-end
-
-function MOI.get(bridge::ZeroOneBridge{T},
-    ::MOI.ListOfConstraintIndices{MOI.SingleVariable, MOI.LessThan{T}}) where {T}
-    return [bridge.one_index]
+    ::MOI.ListOfConstraintIndices{MOI.SingleVariable, MOI.Interval{T}}) where {T}
+    return [bridge.interval_index]
 end
 
 function MOI.get(bridge::ZeroOneBridge{T},
