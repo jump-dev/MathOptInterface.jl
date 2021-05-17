e517520f705adc60c8417b02a154cfa0f63d8780
Oscar Dowson <odow@users.noreply.github.com>
2021-05-17 15:18:13 +1200

    Refactor FileFormats into functional tests (#1355)

--- test/FileFormats/FileFormats.jl
+++ test/FileFormats/FileFormats.jl
@@ -1,100 +1,135 @@
+module TestFileFormats
+
 using MathOptInterface
 using Test
 
 const MOI = MathOptInterface
 
-@testset "MOI.FileFormats tests" begin
-    @testset "$(file)" for file in ["CBF", "LP", "MOF", "MPS", "NL", "SDPA"]
-        include(joinpath(@__DIR__, file, "$(file).jl"))
-    end
+function test_CBF()
+    return include(joinpath(@__DIR__, "CBF", "CBF.jl"))
+end
 
-    @testset "Copying options" begin
-        models = [
-            MOI.FileFormats.CBF.Model,
-            MOI.FileFormats.LP.Model,
-            MOI.FileFormats.MOF.Model,
-            MOI.FileFormats.MPS.Model,
-            MOI.FileFormats.SDPA.Model,
-        ]
-        for src in models
-            model_src = src()
-            for dest in models
-                model_dest = dest()
-                MOI.copy_to(model_dest, model_src)
-                @test !isempty(sprint(write, model_dest))
-            end
-            model_dest =
-                MOI.Utilities.MockOptimizer(MOI.Utilities.Model{Float64}())
+function test_LP()
+    return include(joinpath(@__DIR__, "LP", "LP.jl"))
+end
+
+function test_MOF()
+    return include(joinpath(@__DIR__, "MOF", "MOF.jl"))
+end
+
+function test_MPS()
+    return include(joinpath(@__DIR__, "MPS", "MPS.jl"))
+end
+
+function test_NL()
+    return include(joinpath(@__DIR__, "NL", "NL.jl"))
+end
+
+function test_SDPA()
+    return include(joinpath(@__DIR__, "SDPA", "SDPA.jl"))
+end
+
+function test_copying()
+    models = [
+        MOI.FileFormats.CBF.Model,
+        MOI.FileFormats.LP.Model,
+        MOI.FileFormats.MOF.Model,
+        MOI.FileFormats.MPS.Model,
+        MOI.FileFormats.SDPA.Model,
+    ]
+    for src in models
+        model_src = src()
+        for dest in models
+            model_dest = dest()
             MOI.copy_to(model_dest, model_src)
+            @test !isempty(sprint(write, model_dest))
         end
-    end
-
-    @testset "Calling MOF.compressed_open" begin
-        for cs in [MOI.FileFormats.Bzip2(), MOI.FileFormats.Gzip()]
-            for open_type in ["a", "r+", "w+", "a+"]
-                @test_throws ArgumentError MOI.FileFormats.compressed_open(
-                    (x) -> nothing,
-                    "dummy.gz",
-                    open_type,
-                    cs,
-                )
-            end
-        end
-    end
-
-    @testset "Provided compression schemes" begin
-        filename = joinpath(@__DIR__, "MPS", "free_integer.mps")
-        model = MOI.FileFormats.Model(filename = filename)
-        MOI.read_from_file(model, filename)
-        filename = joinpath(@__DIR__, "free_integer.mps")
-        MOI.write_to_file(model, filename * ".garbage")
-        for ext in ["", ".bz2", ".gz"]
-            MOI.write_to_file(model, filename * ext)
-            model2 = MOI.FileFormats.Model(filename = filename * ext)
-            MOI.read_from_file(model2, filename)
-        end
-
-        sleep(1.0)  # Allow time for unlink to happen.
-        for ext in ["", ".garbage", ".bz2", ".gz"]
-            rm(filename * ext, force = true)
-        end
-    end
-
-    @testset "Model" begin
-        for (format, model) in [
-            (MOI.FileFormats.FORMAT_CBF, MOI.FileFormats.CBF.Model()),
-            (MOI.FileFormats.FORMAT_LP, MOI.FileFormats.LP.Model()),
-            (MOI.FileFormats.FORMAT_MOF, MOI.FileFormats.MOF.Model()),
-            (MOI.FileFormats.FORMAT_MPS, MOI.FileFormats.MPS.Model()),
-            (MOI.FileFormats.FORMAT_NL, MOI.FileFormats.NL.Model()),
-            (MOI.FileFormats.FORMAT_SDPA, MOI.FileFormats.SDPA.Model()),
-        ]
-            @test typeof(
-                MOI.FileFormats.Model(format = format, filename = "foo.bar"),
-            ) == typeof(model)
-        end
-        @test_throws(
-            ErrorException(
-                "When `format==FORMAT_AUTOMATIC` you must pass a `filename`.",
-            ),
-            MOI.FileFormats.Model(format = MOI.FileFormats.FORMAT_AUTOMATIC)
-        )
-        for (ext, model) in [
-            (".cbf", MOI.FileFormats.CBF.Model()),
-            (".lp", MOI.FileFormats.LP.Model()),
-            (".mof.json", MOI.FileFormats.MOF.Model()),
-            (".mps", MOI.FileFormats.MPS.Model()),
-            (".nl", MOI.FileFormats.NL.Model()),
-            (".sdpa", MOI.FileFormats.SDPA.Model()),
-        ]
-            @test typeof(MOI.FileFormats.Model(filename = "a$(ext)")) ==
-                  typeof(model)
-            @test typeof(MOI.FileFormats.Model(filename = "a$(ext).gz")) ==
-                  typeof(model)
-        end
-        @test_throws(
-            ErrorException("Unable to automatically detect format of a.b."),
-            MOI.FileFormats.Model(filename = "a.b")
-        )
+        model_dest = MOI.Utilities.MockOptimizer(MOI.Utilities.Model{Float64}())
+        MOI.copy_to(model_dest, model_src)
     end
 end
+
+function test_compressed_open()
+    for cs in [MOI.FileFormats.Bzip2(), MOI.FileFormats.Gzip()]
+        for open_type in ["a", "r+", "w+", "a+"]
+            @test_throws ArgumentError MOI.FileFormats.compressed_open(
+                (x) -> nothing,
+                "dummy.gz",
+                open_type,
+                cs,
+            )
+        end
+    end
+end
+
+function test_compression()
+    filename = joinpath(@__DIR__, "MPS", "free_integer.mps")
+    model = MOI.FileFormats.Model(filename = filename)
+    MOI.read_from_file(model, filename)
+    filename = joinpath(@__DIR__, "free_integer.mps")
+    MOI.write_to_file(model, filename * ".garbage")
+    for ext in ["", ".bz2", ".gz"]
+        MOI.write_to_file(model, filename * ext)
+        model2 = MOI.FileFormats.Model(filename = filename * ext)
+        MOI.read_from_file(model2, filename)
+    end
+
+    sleep(1.0)  # Allow time for unlink to happen.
+    for ext in ["", ".garbage", ".bz2", ".gz"]
+        rm(filename * ext, force = true)
+    end
+    return
+end
+
+function test_Model()
+    for (format, model) in [
+        (MOI.FileFormats.FORMAT_CBF, MOI.FileFormats.CBF.Model()),
+        (MOI.FileFormats.FORMAT_LP, MOI.FileFormats.LP.Model()),
+        (MOI.FileFormats.FORMAT_MOF, MOI.FileFormats.MOF.Model()),
+        (MOI.FileFormats.FORMAT_MPS, MOI.FileFormats.MPS.Model()),
+        (MOI.FileFormats.FORMAT_NL, MOI.FileFormats.NL.Model()),
+        (MOI.FileFormats.FORMAT_SDPA, MOI.FileFormats.SDPA.Model()),
+    ]
+        @test typeof(
+            MOI.FileFormats.Model(format = format, filename = "foo.bar"),
+        ) == typeof(model)
+    end
+    @test_throws(
+        ErrorException(
+            "When `format==FORMAT_AUTOMATIC` you must pass a `filename`.",
+        ),
+        MOI.FileFormats.Model(format = MOI.FileFormats.FORMAT_AUTOMATIC)
+    )
+    for (ext, model) in [
+        (".cbf", MOI.FileFormats.CBF.Model()),
+        (".lp", MOI.FileFormats.LP.Model()),
+        (".mof.json", MOI.FileFormats.MOF.Model()),
+        (".mps", MOI.FileFormats.MPS.Model()),
+        (".nl", MOI.FileFormats.NL.Model()),
+        (".sdpa", MOI.FileFormats.SDPA.Model()),
+    ]
+        @test typeof(MOI.FileFormats.Model(filename = "a$(ext)")) ==
+              typeof(model)
+        @test typeof(MOI.FileFormats.Model(filename = "a$(ext).gz")) ==
+              typeof(model)
+    end
+    @test_throws(
+        ErrorException("Unable to automatically detect format of a.b."),
+        MOI.FileFormats.Model(filename = "a.b")
+    )
+end
+
+function runtests()
+    for name in names(@__MODULE__, all = true)
+        if startswith("$(name)", "test_")
+            @testset "$name" begin
+                getfield(@__MODULE__, name)()
+            end
+        end
+    end
+    return
+end
+
+end
+
+TestFileFormats.runtests()
--- test/FileFormats/LP/LP.jl
+++ test/FileFormats/LP/LP.jl
@@ -1,3 +1,5 @@
+module TestLP
+
 import MathOptInterface
 using Test
 
@@ -6,14 +8,15 @@ const MOIU = MOI.Utilities
 const LP = MOI.FileFormats.LP
 const LP_TEST_FILE = "test.lp"
 
-@test sprint(show, LP.Model()) == "A .LP-file model"
+function test_show()
+    @test sprint(show, LP.Model()) == "A .LP-file model"
+end
 
-@testset "write_to_file" begin
-    @testset "comprehensive write" begin
-        model = LP.Model()
-        MOIU.loadfromstring!(
-            model,
-            """
+function test_comprehensive_write()
+    model = LP.Model()
+    MOIU.loadfromstring!(
+        model,
+        """
 variables: a, x, y, z
 minobjective: x
 c1: x >= -1.0
@@ -27,220 +30,233 @@ c8: 1.7a + 1.8 in Interval(0.3, 0.4)
 c9: x in ZeroOne()
 c10: y in Integer()
 """,
+    )
+    MOI.write_to_file(model, LP_TEST_FILE)
+    @test read(LP_TEST_FILE, String) ==
+          "minimize\n" *
+          "obj: x\n" *
+          "subject to\n" *
+          "c5: 1.1 x <= 5.1\n" *
+          "c6: -1.4 + 1.3 x >= -0.1\n" *
+          "c7: 1.6 + 1.5 a = 0.2\n" *
+          "c8: 0.3 <= 1.8 + 1.7 a <= 0.4\n" *
+          "Bounds\n" *
+          "x <= 2\n" *
+          "x >= -1\n" *
+          "y = 3\n" *
+          "4 <= z <= 5\n" *
+          "a free\n" *
+          "General\n" *
+          "y\n" *
+          "Binary\n" *
+          "x\n" *
+          "End\n"
+
+    @test !MOI.is_empty(model)
+    MOI.empty!(model)
+    @test MOI.is_empty(model)
+end
+
+function test_name_sanitization_start()
+    for starting_letter in [".", "0", "E", "e"]
+        model = LP.Model()
+        MOI.Utilities.loadfromstring!(
+            model,
+            """
+variables: x
+minobjective: x
+c1: 1.0 * x >= -1.0
+""",
         )
+        x = MOI.get(model, MOI.VariableIndex, "x")
+        MOI.set(model, MOI.VariableName(), x, starting_letter * "x")
+        c = MOI.get(model, MOI.ConstraintIndex, "c1")
+        MOI.set(model, MOI.ConstraintName(), c, starting_letter * "c1")
         MOI.write_to_file(model, LP_TEST_FILE)
         @test read(LP_TEST_FILE, String) ==
               "minimize\n" *
-              "obj: x\n" *
+              "obj: _$(starting_letter)x\n" *
               "subject to\n" *
-              "c5: 1.1 x <= 5.1\n" *
-              "c6: -1.4 + 1.3 x >= -0.1\n" *
-              "c7: 1.6 + 1.5 a = 0.2\n" *
-              "c8: 0.3 <= 1.8 + 1.7 a <= 0.4\n" *
+              "_$(starting_letter)c1: 1 _$(starting_letter)x >= -1\n" *
               "Bounds\n" *
-              "x <= 2\n" *
-              "x >= -1\n" *
-              "y = 3\n" *
-              "4 <= z <= 5\n" *
-              "a free\n" *
-              "General\n" *
-              "y\n" *
-              "Binary\n" *
-              "x\n" *
+              "_$(starting_letter)x free\n" *
               "End\n"
-
-        @test !MOI.is_empty(model)
-        MOI.empty!(model)
-        @test MOI.is_empty(model)
     end
-    @testset "Name sanitization" begin
-        @testset "Start name sanitisation" begin
-            for starting_letter in [".", "0", "E", "e"]
-                model = LP.Model()
-                MOI.Utilities.loadfromstring!(
-                    model,
-                    """
+end
+
+function test_name_sanitization_illegal()
+    for illegal_letter in ["[", "]", "*", "^"]
+        model = LP.Model()
+        MOI.Utilities.loadfromstring!(
+            model,
+            """
 variables: x
 minobjective: x
 c1: 1.0 * x >= -1.0
 """,
-                )
-                x = MOI.get(model, MOI.VariableIndex, "x")
-                MOI.set(model, MOI.VariableName(), x, starting_letter * "x")
-                c = MOI.get(model, MOI.ConstraintIndex, "c1")
-                MOI.set(model, MOI.ConstraintName(), c, starting_letter * "c1")
-                MOI.write_to_file(model, LP_TEST_FILE)
-                @test read(LP_TEST_FILE, String) ==
-                      "minimize\n" *
-                      "obj: _$(starting_letter)x\n" *
-                      "subject to\n" *
-                      "_$(starting_letter)c1: 1 _$(starting_letter)x >= -1\n" *
-                      "Bounds\n" *
-                      "_$(starting_letter)x free\n" *
-                      "End\n"
-            end
-        end
-        @testset "Illegal character sanitisation" begin
-            for illegal_letter in ["[", "]", "*", "^"]
-                model = LP.Model()
-                MOI.Utilities.loadfromstring!(
-                    model,
-                    """
-variables: x
-minobjective: x
-c1: 1.0 * x >= -1.0
-""",
-                )
-                x = MOI.get(model, MOI.VariableIndex, "x")
-                MOI.set(model, MOI.VariableName(), x, "x$(illegal_letter)y")
-                c = MOI.get(model, MOI.ConstraintIndex, "c1")
-                MOI.set(model, MOI.ConstraintName(), c, "c$(illegal_letter)d")
-                MOI.write_to_file(model, LP_TEST_FILE)
-                @test read(LP_TEST_FILE, String) ==
-                      "minimize\n" *
-                      "obj: x_y\n" *
-                      "subject to\n" *
-                      "c_d: 1 x_y >= -1\n" *
-                      "Bounds\n" *
-                      "x_y free\n" *
-                      "End\n"
-            end
-        end
-        @testset "Duplicate names after sanitization" begin
-            model = LP.Model()
-            MOI.Utilities.loadfromstring!(
-                model,
-                """
+        )
+        x = MOI.get(model, MOI.VariableIndex, "x")
+        MOI.set(model, MOI.VariableName(), x, "x$(illegal_letter)y")
+        c = MOI.get(model, MOI.ConstraintIndex, "c1")
+        MOI.set(model, MOI.ConstraintName(), c, "c$(illegal_letter)d")
+        MOI.write_to_file(model, LP_TEST_FILE)
+        @test read(LP_TEST_FILE, String) ==
+              "minimize\n" *
+              "obj: x_y\n" *
+              "subject to\n" *
+              "c_d: 1 x_y >= -1\n" *
+              "Bounds\n" *
+              "x_y free\n" *
+              "End\n"
+    end
+end
+
+function test_name_sanitization_duplicate()
+    model = LP.Model()
+    MOI.Utilities.loadfromstring!(
+        model,
+        """
 variables: a, b, c, d
 minobjective: a + b + c + d
 c1: a + b + c + d >= -1.0
 """,
-            )
-            variables = MOI.get(model, MOI.ListOfVariableIndices())
-            MOI.set.(
-                model,
-                MOI.VariableName(),
-                variables,
-                ["a[", "a]", "a*", "a^"],
-            )
-            MOI.write_to_file(model, LP_TEST_FILE)
-            @test read(LP_TEST_FILE, String) ==
-                  "minimize\n" *
-                  "obj: 1 a_ + 1 a__1 + 1 a__2 + 1 a__3\n" *
-                  "subject to\n" *
-                  "c1: 1 a_ + 1 a__1 + 1 a__2 + 1 a__3 >= -1\n" *
-                  "Bounds\n" *
-                  "a_ free\n" *
-                  "a__1 free\n" *
-                  "a__2 free\n" *
-                  "a__3 free\n" *
-                  "End\n"
-        end
-        @testset "Length sanitisation" begin
-            model = LP.Model(maximum_length = 3)
-            x = MOI.add_variable(model)
-            MOI.set(model, MOI.VariableName(), x, "abcdefg")
-            MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE)
-            MOI.set(
-                model,
-                MOI.ObjectiveFunction{MOI.SingleVariable}(),
-                MOI.SingleVariable(x),
-            )
-            MOI.write_to_file(model, LP_TEST_FILE)
-            @test read(LP_TEST_FILE, String) ==
-                  "minimize\n" *
-                  "obj: abc\n" *
-                  "subject to\n" *
-                  "Bounds\n" *
-                  "abc free\n" *
-                  "End\n"
-        end
-        @testset "Too long duplicate names after sanitization" begin
-            model = LP.Model(maximum_length = 3)
-            MOI.Utilities.loadfromstring!(
-                model,
-                """
+    )
+    variables = MOI.get(model, MOI.ListOfVariableIndices())
+    MOI.set.(model, MOI.VariableName(), variables, ["a[", "a]", "a*", "a^"])
+    MOI.write_to_file(model, LP_TEST_FILE)
+    @test read(LP_TEST_FILE, String) ==
+          "minimize\n" *
+          "obj: 1 a_ + 1 a__1 + 1 a__2 + 1 a__3\n" *
+          "subject to\n" *
+          "c1: 1 a_ + 1 a__1 + 1 a__2 + 1 a__3 >= -1\n" *
+          "Bounds\n" *
+          "a_ free\n" *
+          "a__1 free\n" *
+          "a__2 free\n" *
+          "a__3 free\n" *
+          "End\n"
+end
+
+function test_name_sanitization_length()
+    model = LP.Model(maximum_length = 3)
+    x = MOI.add_variable(model)
+    MOI.set(model, MOI.VariableName(), x, "abcdefg")
+    MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE)
+    MOI.set(
+        model,
+        MOI.ObjectiveFunction{MOI.SingleVariable}(),
+        MOI.SingleVariable(x),
+    )
+    MOI.write_to_file(model, LP_TEST_FILE)
+    @test read(LP_TEST_FILE, String) ==
+          "minimize\n" *
+          "obj: abc\n" *
+          "subject to\n" *
+          "Bounds\n" *
+          "abc free\n" *
+          "End\n"
+end
+
+function test_name_sanitization_too_long()
+    model = LP.Model(maximum_length = 3)
+    MOI.Utilities.loadfromstring!(
+        model,
+        """
 variables: abcd, abce
 minobjective: abcd + abce
 c1: abcd + abce >= -1.0
 """,
-            )
-            MOI.write_to_file(model, LP_TEST_FILE)
-            @test read(LP_TEST_FILE, String) ==
-                  "minimize\n" *
-                  "obj: 1 abc + 1 abc_1\n" *
-                  "subject to\n" *
-                  "c1: 1 abc + 1 abc_1 >= -1\n" *
-                  "Bounds\n" *
-                  "abc free\n" *
-                  "abc_1 free\n" *
-                  "End\n"
-        end
-    end
-    @testset "other features" begin
-        model = LP.Model()
-        MOIU.loadfromstring!(
-            model,
-            """
+    )
+    MOI.write_to_file(model, LP_TEST_FILE)
+    @test read(LP_TEST_FILE, String) ==
+          "minimize\n" *
+          "obj: 1 abc + 1 abc_1\n" *
+          "subject to\n" *
+          "c1: 1 abc + 1 abc_1 >= -1\n" *
+          "Bounds\n" *
+          "abc free\n" *
+          "abc_1 free\n" *
+          "End\n"
+end
+
+function test_name_sanitization_other()
+    model = LP.Model()
+    MOIU.loadfromstring!(
+        model,
+        """
 variables: x
 maxobjective: 2.0 * x + -1.0
 """,
-        )
-        MOI.write_to_file(model, LP_TEST_FILE)
-        @test read(LP_TEST_FILE, String) ==
-              "maximize\n" *
-              "obj: -1 + 2 x\n" *
-              "subject to\n" *
-              "Bounds\n" *
-              "x free\n" *
-              "End\n"
-    end
-    @testset "free variables" begin
-        model = LP.Model()
-        MOIU.loadfromstring!(
-            model,
-            """
+    )
+    MOI.write_to_file(model, LP_TEST_FILE)
+    @test read(LP_TEST_FILE, String) ==
+          "maximize\n" *
+          "obj: -1 + 2 x\n" *
+          "subject to\n" *
+          "Bounds\n" *
+          "x free\n" *
+          "End\n"
+end
+
+function test_free_variables()
+    model = LP.Model()
+    MOIU.loadfromstring!(
+        model,
+        """
 variables: x, y, z
 maxobjective: x
 c1: x in ZeroOne()
 c2: y in Integer()
 """,
-        )
-        MOI.write_to_file(model, LP_TEST_FILE)
-        @test read(LP_TEST_FILE, String) ==
-              "maximize\n" *
-              "obj: x\n" *
-              "subject to\n" *
-              "Bounds\n" *
-              "y free\n" *
-              "z free\n" *
-              "General\n" *
-              "y\n" *
-              "Binary\n" *
-              "x\n" *
-              "End\n"
-    end
-    @testset "Quadratic Objective" begin
-        model = LP.Model()
-        @test_throws(
-            MOI.UnsupportedAttribute,
-            MOIU.loadfromstring!(
-                model,
-                """
-    variables: x
-    minobjective: 1.0*x*x
-    """,
-            )
-        )
-    end
+    )
+    MOI.write_to_file(model, LP_TEST_FILE)
+    @test read(LP_TEST_FILE, String) ==
+          "maximize\n" *
+          "obj: x\n" *
+          "subject to\n" *
+          "Bounds\n" *
+          "y free\n" *
+          "z free\n" *
+          "General\n" *
+          "y\n" *
+          "Binary\n" *
+          "x\n" *
+          "End\n"
 end
 
-@testset "read_from_file" begin
+function test_quadratic_objective()
+    model = LP.Model()
+    @test_throws(
+        MOI.UnsupportedAttribute,
+        MOIU.loadfromstring!(
+            model,
+            """
+variables: x
+minobjective: 1.0*x*x
+""",
+        )
+    )
+end
+
+function test_read()
     model = LP.Model()
     exception = ErrorException("read! is not implemented for LP files.")
     @test_throws exception MOI.read_from_file(model, LP_TEST_FILE)
 end
 
-# Clean up
-sleep(1.0)  # Allow time for unlink to happen.
-rm(LP_TEST_FILE, force = true)
+function runtests()
+    for name in names(@__MODULE__, all = true)
+        if startswith("$(name)", "test_")
+            @testset "$name" begin
+                getfield(@__MODULE__, name)()
+            end
+        end
+    end
+    sleep(1.0)  # Allow time for unlink to happen.
+    rm(LP_TEST_FILE, force = true)
+    return
+end
+
+end
+
+TestLP.runtests()
