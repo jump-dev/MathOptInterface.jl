0273127e7ffcbb10f883735dbd340ae5c764e13c
Oscar Dowson <odow@users.noreply.github.com>
2021-07-14 10:02:55 +1200

    Migrate tests of sets.jl (#1452)

--- src/Utilities/sets.jl
+++ src/Utilities/sets.jl
@@ -41,6 +41,7 @@ function shift_constant(
 ) where {T}
     return typeof(set)(MOI.constant(set) + offset)
 end
+
 supports_shift_constant(::Type{<:MOI.LessThan}) = true
 supports_shift_constant(::Type{<:MOI.GreaterThan}) = true
 supports_shift_constant(::Type{<:MOI.EqualTo}) = true
@@ -50,14 +51,52 @@ function shift_constant(set::MOI.Interval, offset)
 end
 supports_shift_constant(::Type{<:MOI.Interval}) = true
 
+"""
+    ScalarLinearSet{T}
+
+The union of scalar-valued linear sets with element type `T`.
+
+This is used in the vectorize and scalarize bridges.
+
+See also: [`VectorLinearSet`](@ref).
+"""
 const ScalarLinearSet{T} =
     Union{MOI.EqualTo{T},MOI.LessThan{T},MOI.GreaterThan{T}}
+
+"""
+    VectorLinearSet
+
+The union of vector-valued linear cones.
+
+This is used in the vectorize and scalarize bridges.
+
+See also: [`ScalarLinearSet`](@ref).
+"""
 const VectorLinearSet = Union{MOI.Zeros,MOI.Nonnegatives,MOI.Nonpositives}
 
+"""
+    vector_set_type(::Type{S}) where {S}
+
+A utility function to map scalar sets `S` to their vector equivalents.
+
+This is used in the vectorize and scalarize bridges.
+
+See also: [`scalar_set_type`](@ref).
+"""
 vector_set_type(::Type{<:MOI.EqualTo}) = MOI.Zeros
 vector_set_type(::Type{<:MOI.LessThan}) = MOI.Nonpositives
 vector_set_type(::Type{<:MOI.GreaterThan}) = MOI.Nonnegatives
 
+"""
+    scalar_set_type(::Type{S}, ::Type{T}) where {S,T}
+
+A utility function to map vector sets `S` to their scalar equivalents with
+element type `T`.
+
+This is used in the vectorize and scalarize bridges.
+
+See also: [`vector_set_type`](@ref).
+"""
 scalar_set_type(::Type{<:MOI.Zeros}, T::Type) = MOI.EqualTo{T}
 scalar_set_type(::Type{<:MOI.Nonpositives}, T::Type) = MOI.LessThan{T}
 scalar_set_type(::Type{<:MOI.Nonnegatives}, T::Type) = MOI.GreaterThan{T}
