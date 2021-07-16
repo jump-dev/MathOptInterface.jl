ae09018f4499ece99a0c384f9cef6e5a826429c3
odow <o.dowson@gmail.com>
2021-07-16 14:53:06 +1200

    Refactor tests

--- test/Utilities/matrix_of_constraints.jl
+++ test/Utilities/matrix_of_constraints.jl
@@ -17,14 +17,249 @@ function runtests()
 end
 
 MOI.Utilities.@product_of_sets(
-    ProductOfSetsLP,
+    ScalarSets,
     MOI.EqualTo{T},
     MOI.GreaterThan{T},
     MOI.LessThan{T},
     MOI.Interval{T},
 )
 
-function test_ProductLP()
+function _new_ScalarSets()
+    return MOI.Utilities.GenericOptimizer{
+        Float64,
+        MOI.Utilities.MatrixOfConstraints{
+            Float64,
+            MOI.Utilities.MutableSparseMatrixCSC{
+                Float64,
+                Int,
+                MOI.Utilities.ZeroBasedIndexing,
+            },
+            MOI.Utilities.Box{Float64},
+            ScalarSets{Float64},
+        },
+    }()
+end
+
+MOI.Utilities.@product_of_sets(
+    VectorSets,
+    MOI.Nonnegatives,
+    MOI.Nonpositives,
+    MOI.SecondOrderCone,
+)
+
+function _new_VectorSets()
+    return MOI.Utilities.GenericOptimizer{
+        Int,
+        MOI.Utilities.MatrixOfConstraints{
+            Int,
+            MOI.Utilities.MutableSparseMatrixCSC{
+                Int,
+                Int,
+                MOI.Utilities.OneBasedIndexing,
+            },
+            Vector{Int},
+            VectorSets{Int},
+        },
+    }()
+end
+
+
+function test_ScalarSets_basic()
+    model = _new_ScalarSets()
+    @test MOI.is_empty(model)
+    src = MOI.Utilities.Model{Float64}()
+    MOI.Utilities.loadfromstring!(src, """
+    variables: x, y
+    minobjective: x + y
+    x >= 1.0
+    y == 2.1
+    c: x + 2.0 * y <= 3.0
+    """)
+    index_map = MOI.copy_to(model, src)
+    @test MOI.is_empty(model) == false
+    for (k, v) in index_map
+        @test MOI.is_valid(src, k)
+        @test MOI.is_valid(model, v)
+    end
+    @test length(MOI.get(src, MOI.ListOfConstraintTypesPresent())) == 3
+    MOI.empty!(model)
+    @test MOI.is_empty(model)
+    return
+end
+
+function test_VectorSets_basic()
+    model = _new_VectorSets()
+    @test MOI.is_empty(model)
+    src = MOI.Utilities.Model{Int}()
+    x = MOI.add_variables(src, 2)
+    c = MOI.add_constraint(
+        src,
+        MOI.VectorAffineFunction{Int}(
+            MOI.VectorAffineTerm.(1, MOI.ScalarAffineTerm.(1, x)),
+            [1, 3],
+        ),
+        MOI.SecondOrderCone(2),
+    )
+    index_map = MOI.copy_to(model, src)
+    @test MOI.is_empty(model) == false
+    for (k, v) in index_map
+        @test MOI.is_valid(src, k)
+        @test MOI.is_valid(model, v)
+    end
+    @test length(MOI.get(src, MOI.ListOfConstraintTypesPresent())) == 1
+    MOI.empty!(model)
+    @test MOI.is_empty(model)
+    return
+end
+
+function test_ScalarSets_supports_constraint()
+    model = _new_ScalarSets()
+    @test MOI.supports_constraint(
+        model,
+        MOI.ScalarAffineFunction{Float64},
+        MOI.LessThan{Float64},
+    )
+    @test !MOI.supports_constraint(
+        model,
+        MOI.VectorAffineFunction{Float64},
+        MOI.Nonnegatives,
+    )
+    return
+end
+
+function test_VectorSets_supports_constraint()
+    model = _new_VectorSets()
+    @test !MOI.supports_constraint(
+        model,
+        MOI.ScalarAffineFunction{Float64},
+        MOI.LessThan{Float64},
+    )
+    @test MOI.supports_constraint(
+        model,
+        MOI.VectorAffineFunction{Int},
+        MOI.Nonnegatives,
+    )
+    return
+end
+
+function test_delete()
+    model = _new_ScalarSets()
+    x = MOI.add_variable(model)
+    @test_throws MOI.DeleteNotAllowed MOI.delete(model, x)
+    @test_throws MOI.DeleteNotAllowed MOI.delete(model, [x])
+    return
+end
+
+function test_ScalarSets_get_ConstraintFunction()
+    model = _new_ScalarSets()
+    src = MOI.Utilities.Model{Float64}()
+    MOI.Utilities.loadfromstring!(src, """
+    variables: x, y
+    minobjective: x + y
+    c: x + 2.0 * y <= 3.0
+    """)
+    index_map = MOI.copy_to(model, src)
+    c = MOI.get(model, MOI.ConstraintIndex, "c")
+    c_f = MOI.get(model, MOI.ConstraintFunction(), c)
+    @test MOI.Utilities.map_indices(c_f) do x
+        return index_map[x]
+    end ≈ MOI.get(src, MOI.ConstraintFunction(), index_map[c])
+    return
+end
+
+function test_VectorSets_get_ConstraintFunction()
+    model = _new_VectorSets()
+    src = MOI.Utilities.Model{Int}()
+    x = MOI.add_variables(src, 2)
+    c = MOI.add_constraint(
+        src,
+        MOI.VectorAffineFunction{Int}(
+            MOI.VectorAffineTerm.(1, MOI.ScalarAffineTerm.(1, x)),
+            [1, 3],
+        ),
+        MOI.SecondOrderCone(2),
+    )
+    index_map = MOI.copy_to(model, src)
+    c_f = MOI.get(model, MOI.ConstraintFunction(), c)
+    @test MOI.Utilities.map_indices(c_f) do x
+        return index_map[x]
+    end ≈ MOI.get(src, MOI.ConstraintFunction(), index_map[c])
+    return
+end
+
+function test_ScalarSets_get_ConstraintSet()
+    model = _new_ScalarSets()
+    src = MOI.Utilities.Model{Float64}()
+    MOI.Utilities.loadfromstring!(src, """
+    variables: x, y
+    minobjective: x + y
+    c: 2.0 * x + y >= 4.0
+    """)
+    index_map = MOI.copy_to(model, src)
+    c = MOI.get(model, MOI.ConstraintIndex, "c")
+    c_set = MOI.get(model, MOI.ConstraintSet(), c)
+    @test c_set == MOI.get(src, MOI.ConstraintSet(), index_map[c])
+    return
+end
+
+
+function test_VectorSets_get_ConstraintSet()
+    model = _new_VectorSets()
+    src = MOI.Utilities.Model{Int}()
+    x = MOI.add_variables(src, 2)
+    c = MOI.add_constraint(
+        src,
+        MOI.VectorAffineFunction{Int}(
+            MOI.VectorAffineTerm.(1, MOI.ScalarAffineTerm.(1, x)),
+            [1, 3],
+        ),
+        MOI.SecondOrderCone(2),
+    )
+    index_map = MOI.copy_to(model, src)
+    c_set = MOI.get(model, MOI.ConstraintSet(), c)
+    @test c_set == MOI.get(src, MOI.ConstraintSet(), index_map[c])
+    return
+end
+
+function test_add_after_final_touch()
+    model = _new_ScalarSets()
+    @test MOI.is_empty(model)
+    src = MOI.Utilities.Model{Float64}()
+    MOI.Utilities.loadfromstring!(src, """
+    variables: x, y
+    minobjective: x + y
+    x >= 1.0
+    y == 2.1
+    c: x + 2.0 * y <= 3.0
+    """)
+    index_map = MOI.copy_to(model, src)
+    x = MOI.get(src, MOI.VariableIndex, "x")
+    @test_throws(
+        MOI.AddConstraintNotAllowed,
+        MOI.add_constraint(
+            model,
+            MOI.ScalarAffineFunction(
+                [MOI.ScalarAffineTerm(1.0, index_map[x])],
+                0.0,
+            ),
+            MOI.LessThan(2.0),
+        ),
+    )
+    return
+end
+
+function test_UnsupportedConstraint()
+    model = _new_ScalarSets()
+    src = MOI.Utilities.Model{Float64}()
+    MOI.Utilities.loadfromstring!(src, """
+    variables: x, y
+    c: [x, y] in Nonnegatives(2)
+    """)
+    @test_throws MOI.UnsupportedConstraint MOI.copy_to(model, src)
+    return
+end
+
+function test_ScalarSets()
     optimizer = MOI.Utilities.GenericOptimizer{
         Float64,
         MOI.Utilities.MatrixOfConstraints{
@@ -35,65 +270,15 @@ function test_ProductLP()
                 MOI.Utilities.ZeroBasedIndexing,
             },
             MOI.Utilities.Box{Float64},
-            ProductOfSetsLP{Float64},
+            ScalarSets{Float64},
         },
     }()
     MOI.Test.runtests(
         optimizer,
         MOI.Test.Config(exclude = Any[MOI.optimize!]),
+        include = ["test_model_", "test_objective_", "test_variable_"],
         exclude = [
-            # These tests require `get` before `final_touch` is called.
-            "test_basic_",
-            "test_linear_integration",
-            "test_linear_modify_GreaterThan_and_LessThan_constraints",
-            "test_linear_transform",
-            "test_quadratic_duplicate_terms",
-            "test_quadratic_integration",
-            # Can safely ignore this one.
             "test_model_ScalarFunctionConstantNotZero",
-            # Unsupported Attributes
-            "test_attribute_SolverName",
-            "test_model_default_DualStatus",
-            "test_model_default_PrimalStatus",
-            "test_model_default_TerminationStatus",
-        ],
-    )
-    return
-end
-
-MOI.Utilities.@product_of_sets(
-    ProductOfSetsConic,
-    MOI.Nonnegatives,
-    MOI.Nonpositives,
-    MOI.SecondOrderCone,
-)
-
-function test_ProductOfSetsConic()
-    optimizer = MOI.Utilities.GenericOptimizer{
-        Float64,
-        MOI.Utilities.MatrixOfConstraints{
-            Float64,
-            MOI.Utilities.MutableSparseMatrixCSC{
-                Float64,
-                Int,
-                MOI.Utilities.OneBasedIndexing,
-            },
-            MOI.Utilities.Box{Float64},
-            ProductOfSetsConic{Float64},
-        },
-    }()
-    MOI.Test.runtests(
-        optimizer,
-        MOI.Test.Config(exclude = Any[MOI.optimize!]),
-        exclude = [
-            # These tests require `get` before `final_touch` is called.
-            "test_basic_",
-            # Can safely ignore this one.
-            "test_model_ScalarFunctionConstantNotZero",
-            # Unsupported Attributes
-            "test_attribute_SolverName",
-            "test_constraint_ConstraintDualStart",
-            "test_constraint_ConstraintPrimalStart",
             "test_model_default_DualStatus",
             "test_model_default_PrimalStatus",
             "test_model_default_TerminationStatus",
@@ -113,7 +298,7 @@ function test_modify()
                 MOI.Utilities.OneBasedIndexing,
             },
             MOI.Utilities.Box{Int},
-            ProductOfSetsLP{Int},
+            ScalarSets{Int},
         },
     }()
     x = MOI.add_variable(model)
