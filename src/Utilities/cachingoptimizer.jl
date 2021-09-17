e2d4f05ed39129ba052cca9e0118949bbf4bc15f
Oscar Dowson <odow@users.noreply.github.com>
2021-09-18 11:58:01 +1200

    Update cachingoptimizer.jl

--- src/Utilities/cachingoptimizer.jl
+++ src/Utilities/cachingoptimizer.jl
@@ -277,7 +277,7 @@ function MOI.optimize!(m::CachingOptimizer)
         # to check if we have an `AbstractCallback` set and attach the optimizer
         # before recalling `MOI.optimize!`.
         for attr in MOI.get(m, MOI.ListOfModelAttributesSet())
-            if typeof(attr) <: MOI.AbstractCallback
+            if attr isa MOI.AbstractCallback
                 attach_optimizer(m)
                 return MOI.optimize!(m)
             end
