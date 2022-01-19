d40db72096f4ff2540fb343b26f15972c348f5ea
Benoît Legat <benoit.legat@gmail.com>
2022-01-19 12:58:11 -0500

    Expand docstring for free variables

--- src/Utilities/free_variables.jl
+++ src/Utilities/free_variables.jl
@@ -5,7 +5,61 @@
     end
 
 A struct for storing free variables that can be used as the `variables` field
-of [`GenericModel`](@ref) or [`GenericModel`](@ref).
+of [`GenericModel`](@ref) or [`GenericModel`](@ref). It represents a model
+that does not support any constraint nor objective function.
+
+## Example
+
+The following model type represents a conic model in geometric form. As opposed
+to [`VariablesContainer`](@ref), `FreeVariables` does not support constraint
+bounds so they are bridged into an affine constraint in the
+[`MathOptInterface.Nonnegatives`](@ref) cone as expected for the geometric
+conic form.
+```jldocstest
+julia> MOI.Utilities.@product_of_sets(
+    Cones,
+    MOI.Zeros,
+    MOI.Nonnegatives,
+    MOI.SecondOrderCone,
+    MOI.PositiveSemidefiniteConeTriangle,
+);
+
+julia> const ConicModel{T} = MOI.Utilities.GenericOptimizer{
+    T,
+    MOI.Utilities.ObjectiveContainer{T},
+    MOI.Utilities.FreeVariables,
+    MOI.Utilities.MatrixOfConstraints{
+        T,
+        MOI.Utilities.MutableSparseMatrixCSC{
+            T,
+            Int,
+            MOI.Utilities.OneBasedIndexing,
+        },
+        Vector{T},
+        Cones{T},
+    },
+};
+
+julia> model = MOI.instantiate(ConicModel{Float64}, with_bridge_type=Float64);
+
+julia> x = MOI.add_variable(model)
+MathOptInterface.VariableIndex(1)
+
+julia> c = MOI.add_constraint(model, x, MOI.GreaterThan(1.0))
+MathOptInterface.ConstraintIndex{MathOptInterface.VariableIndex, MathOptInterface.GreaterThan{Float64}}(1)
+
+julia> MOI.Bridges.is_bridged(model, c)
+true
+
+julia> bridge = MOI.Bridges.bridge(model, c)
+MathOptInterface.Bridges.Constraint.VectorizeBridge{Float64, MathOptInterface.VectorAffineFunction{Float64}, MathOptInterface.Nonnegatives, MathOptInterface.VariableIndex}(MathOptInterface.ConstraintIndex{MathOptInterface.VectorAffineFunction{Float64}, MathOptInterface.Nonnegatives}(1), 1.0)
+
+julia> bridge.vector_constraint
+MathOptInterface.ConstraintIndex{MathOptInterface.VectorAffineFunction{Float64}, MathOptInterface.Nonnegatives}(1)
+
+julia> MOI.Bridges.is_bridged(model, bridge.vector_constraint)
+false
+```
 """
 mutable struct FreeVariables <: MOI.ModelLike
     n::Int64
