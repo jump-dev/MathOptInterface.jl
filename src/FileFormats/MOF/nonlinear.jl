4d0f52e7eeebabc8ea87cc81ea3386ff8f6fa329
Oscar Dowson <odow@users.noreply.github.com>
2022-04-23 09:06:45 +1200

    Add license header to all files (#1813)

--- src/FileFormats/MOF/nonlinear.jl
+++ src/FileFormats/MOF/nonlinear.jl
@@ -1,3 +1,9 @@
+# Copyright (c) 2017: Miles Lubin and contributors
+# Copyright (c) 2017: Google Inc.
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 # Overload for writing.
 function moi_to_object(foo::Nonlinear, name_map::Dict{MOI.VariableIndex,String})
     node_list = OrderedObject[]
--- src/Utilities/CleverDicts.jl
+++ src/Utilities/CleverDicts.jl
@@ -1,3 +1,9 @@
+# Copyright (c) 2017: Miles Lubin and contributors
+# Copyright (c) 2017: Google Inc.
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 module CleverDicts
 
 # The following two functions are overloaded for `MOI.VariableIndex` here
