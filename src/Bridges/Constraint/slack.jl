e4dd4d7c28160d1fdddc913342ded514a9315eee
odow <o.dowson@gmail.com>
2022-04-22 17:01:16 +1200

    Add license header to all files

--- src/Bridges/Constraint/slack.jl
+++ src/Bridges/Constraint/slack.jl
@@ -1,3 +1,9 @@
+# Copyright (c) 2017: Miles Lubin and contributors
+# Copyright (c) 2017: Google Inc.
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 abstract type AbstractSlackBridge{T,VF,ZS,F,S} <: AbstractBridge end
 
 function MOI.Bridges.added_constrained_variable_types(
--- src/Test/test_variable.jl
+++ src/Test/test_variable.jl
@@ -1,3 +1,9 @@
+# Copyright (c) 2017: Miles Lubin and contributors
+# Copyright (c) 2017: Google Inc.
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 """
     test_variable_add_variable(model::MOI.ModelLike, config::Config)
 
