969129c272a7740772e71c36b42da59fabaa8bb1
odow <o.dowson@gmail.com>
2022-04-25 10:40:06 +1200

    Add warning to module docstring

--- src/Nonlinear/Nonlinear.jl
+++ src/Nonlinear/Nonlinear.jl
@@ -4,6 +4,14 @@
 # Use of this source code is governed by an MIT-style license that can be found
 # in the LICENSE.md file or at https://opensource.org/licenses/MIT.
 
+"""
+    Nonlinear
+
+!!! warning
+    The Nonlinear submodule is experimental. Until this message is removed,
+    breaking changes may be introduced in any minor or patch release of
+    MathOptInterface.
+"""
 module Nonlinear
 
 import Base.Meta: isexpr
