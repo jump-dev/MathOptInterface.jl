aa3d326db2c160f94cb237f0b3c07d846917ac9d
odow <o.dowson@gmail.com>
2022-04-25 12:17:26 +1200

    Add license header

--- src/Nonlinear/ReverseAD/mathoptinterface_api.jl
+++ src/Nonlinear/ReverseAD/mathoptinterface_api.jl
@@ -1,3 +1,9 @@
+# Copyright (c) 2017: Miles Lubin and contributors
+# Copyright (c) 2017: Google Inc.
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 function MOI.features_available(d::NLPEvaluator)
     # Check if we have any user-defined multivariate operators, in which case we
     # need to disable hessians. The result of features_available depends on this.
