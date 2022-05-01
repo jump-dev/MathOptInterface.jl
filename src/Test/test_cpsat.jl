73b9338dfe7cd7a765fbbcf442f4e89a57b123ed
odow <o.dowson@gmail.com>
2022-05-02 08:24:48 +1200

    Add AllDifferent set

---
+++ src/Test/test_cpsat.jl
@@ -0,0 +1,48 @@
+# Copyright (c) 2017: Miles Lubin and contributors
+# Copyright (c) 2017: Google Inc.
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
+"""
+    test_cpsat_AllDifferent(model::MOI.ModelLike, config::Config)
+
+Add a VectorOfVariables-in-AllDifferent constraint.
+"""
+function test_cpsat_AllDifferent(
+    model::MOI.ModelLike,
+    config::Config{T},
+) where {T}
+    @requires MOI.supports_constraint(
+        model,
+        MOI.VectorOfVariables,
+        MOI.AllDifferent,
+    )
+    @requires MOI.supports_add_constrained_variable(model, MOI.Integer)
+    @requires _supports(config, MOI.optimize!)
+    y = [MOI.add_constrained_variable(model, MOI.Integer()) for _ in 1:3]
+    x = first.(y)
+    MOI.add_constraint(model, MOI.VectorOfVariables(x), MOI.AllDifferent(3))
+    MOI.optimize!(model)
+    x_val = MOI.get.(model, MOI.VariablePrimal(), x)
+    @test abs(x_val[1] - x_val[2]) > 0.5
+    @test abs(x_val[1] - x_val[3]) > 0.5
+    @test abs(x_val[2] - x_val[3]) > 0.5
+    return
+end
+
+function setup_test(
+    ::typeof(test_cpsat_AllDifferent),
+    model::MOIU.MockOptimizer,
+    ::Config{T},
+) where {T}
+    MOIU.set_mock_optimize!(
+        model,
+        (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(
+            mock,
+            MOI.OPTIMAL,
+            (MOI.FEASIBLE_POINT, T[0, 1, 2]),
+        ),
+    )
+    return
+end
