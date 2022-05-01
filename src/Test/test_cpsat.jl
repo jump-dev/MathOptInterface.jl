06302e67cfbfa906f7ee9e631096425446882346
odow <o.dowson@gmail.com>
2022-05-02 09:40:48 +1200

    Add Among set

--- src/Test/test_cpsat.jl
+++ src/Test/test_cpsat.jl
@@ -87,3 +87,38 @@ function setup_test(
     )
     return
 end
+
+"""
+    test_cpsat_Among(model::MOI.ModelLike, config::Config)
+
+Add a VectorOfVariables-in-Among constraint.
+"""
+function test_cpsat_Among(model::MOI.ModelLike, config::Config{T}) where {T}
+    @requires MOI.supports_constraint(model, MOI.VectorOfVariables, MOI.Among)
+    @requires MOI.supports_add_constrained_variable(model, MOI.Integer)
+    @requires _supports(config, MOI.optimize!)
+    y = [MOI.add_constrained_variable(model, MOI.Integer()) for _ in 1:4]
+    x = first.(y)
+    set = Set([3, 4])
+    MOI.add_constraint(model, MOI.VectorOfVariables(x), MOI.Among(4, set))
+    MOI.optimize!(model)
+    x_val = round.(Int, MOI.get.(model, MOI.VariablePrimal(), x))
+    @test x_val[1] == sum(x_val[i] in set for i in 2:length(x))
+    return
+end
+
+function setup_test(
+    ::typeof(test_cpsat_Among),
+    model::MOIU.MockOptimizer,
+    ::Config{T},
+) where {T}
+    MOIU.set_mock_optimize!(
+        model,
+        (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(
+            mock,
+            MOI.OPTIMAL,
+            (MOI.FEASIBLE_POINT, T[2, 3, 4, 0]),
+        ),
+    )
+    return
+end
