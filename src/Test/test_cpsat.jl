7c753f2d4125585fa5646d42897769fc7d29c147
Oscar Dowson <odow@users.noreply.github.com>
2022-05-02 13:41:35 +1200

    Update src/Test/test_cpsat.jl

--- src/Test/test_cpsat.jl
+++ src/Test/test_cpsat.jl
@@ -142,7 +142,11 @@ function test_cpsat_CountAtLeast(
     y = [MOI.add_constrained_variable(model, MOI.Integer()) for _ in 1:3]
     x = first.(y)
     set = Set([3, 4])
-    MOI.add_constraint(model, MOI.VectorOfVariables(x), MOI.Among(3, 2, set))
+    MOI.add_constraint(
+        model,
+        MOI.VectorOfVariables(x),
+        MOI.CountAtLeast(3, 2, set),
+)
     MOI.optimize!(model)
     x_val = round.(Int, MOI.get.(model, MOI.VariablePrimal(), x))
     @test sum(x_val[i] in set for i in 1:length(x)) >= 2
