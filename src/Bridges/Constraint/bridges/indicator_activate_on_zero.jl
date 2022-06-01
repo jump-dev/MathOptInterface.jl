70d3a2c4f96747408243dbc258bcf250bd9e33e6
Oscar Dowson <odow@users.noreply.github.com>
2022-06-01 16:06:24 +1200

    [Bridges] tidy various Constraint bridges (#1877)

--- src/Bridges/Constraint/bridges/indicator_activate_on_zero.jl
+++ src/Bridges/Constraint/bridges/indicator_activate_on_zero.jl
@@ -5,14 +5,29 @@
 # in the LICENSE.md file or at https://opensource.org/licenses/MIT.
 
 """
-    IndicatorActiveOnFalseBridge{T}
+    IndicatorActiveOnFalseBridge{T,F,S} <: Bridges.Constraint.AbstractBridge
 
-The `IndicatorActiveOnFalseBridge` replaces an indicator constraint activated
-on 0 with a variable ``z_0`` with the constraint activated on 1, with a variable ``z_1``.
-It stores the added `variable` and added constraints:
-- ``z_1 \\in \\mathbb{B}`` in `zero_one_cons`
-- ``z_0 + z_1 == 1`` in `` in `disjunction_cons`
-- The added `ACTIVATE_ON_ONE` indicator constraint in `indicator_cons_index`.
+`IndicatorActiveOnFalseBridge` implements the following reformulation:
+
+  * ``!z \\implies {f(x) \\in S}`` into ``y \\implies {f(x) \\in S}``,
+    ``z + y == 1``, and ``y \\in \\{0, \\}``
+
+## Source node
+
+`IndicatorActiveOnFalseBridge` supports:
+
+  * [`MOI.VectorAffineFunction{T}`](@ref) in
+    [`MOI.Indicator{MOI.ACTIVATE_ON_ZERO,S}`](@ref)
+
+## Target nodes
+
+`IndicatorActiveOnFalseBridge` creates:
+
+  * [`MOI.VectorAffineFunction{T}`](@ref) in
+    [`MOI.Indicator{MOI.ACTIVATE_ON_ONE,S}`](@ref)
+  * [`MOI.ScalarAffineFunction{T}`](@ref) in [`MOI.EqualTo`](@ref)
+  * [`MOI.VariableIndex`](@ref) in [`MOI.ZeroOne`](@ref)
+
 """
 struct IndicatorActiveOnFalseBridge{
     T,
@@ -45,12 +60,15 @@ function bridge_constraint(
     F,
     IS<:MOI.Indicator{MOI.ACTIVATE_ON_ZERO,S},
 }
-    f_scalars = MOIU.eachscalar(f)
+    f_scalars = MOI.Utilities.eachscalar(f)
     z2, zo_cons = MOI.add_constrained_variable(model, MOI.ZeroOne())
     # z1 + z2 == 1
-    z1_z2 = MOIU.operate(+, T, f_scalars[1], z2)
-    dcons = MOIU.normalize_and_add_constraint(model, z1_z2, MOI.EqualTo(one(T)))
-    f2 = MOIU.operate(vcat, T, z2, f_scalars[2])
+    dcons = MOI.Utilities.normalize_and_add_constraint(
+        model,
+        MOI.Utilities.operate(+, T, f_scalars[1], z2),
+        MOI.EqualTo(one(T)),
+    )
+    f2 = MOI.Utilities.operate(vcat, T, z2, f_scalars[2])
     ci =
         MOI.add_constraint(model, f2, MOI.Indicator{MOI.ACTIVATE_ON_ONE}(s.set))
     return IndicatorActiveOnFalseBridge{T,F,S}(z2, zo_cons, dcons, ci)
@@ -64,13 +82,13 @@ function MOI.supports_constraint(
     return true
 end
 
-function MOIB.added_constrained_variable_types(
+function MOI.Bridges.added_constrained_variable_types(
     ::Type{<:IndicatorActiveOnFalseBridge},
 )
     return Tuple{Type}[(MOI.ZeroOne,)]
 end
 
-function MOIB.added_constraint_types(
+function MOI.Bridges.added_constraint_types(
     ::Type{IndicatorActiveOnFalseBridge{T,F,S}},
 ) where {T,F,S}
     return Tuple{Type,Type}[
