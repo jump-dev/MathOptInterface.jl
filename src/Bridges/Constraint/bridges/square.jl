6656267f201694dda29080def01918e06d1d4ae3
odow <o.dowson@gmail.com>
2022-06-15 14:41:31 +1200

    [docs] Document square PSD duality convention

--- src/Bridges/Constraint/bridges/square.jl
+++ src/Bridges/Constraint/bridges/square.jl
@@ -4,42 +4,6 @@
 # Use of this source code is governed by an MIT-style license that can be found
 # in the LICENSE.md file or at https://opensource.org/licenses/MIT.
 
-# Let S₊ be the cone of symmetric semidefinite matrices in
-# the n*(n+1)/2 dimensional space of symmetric R^{nxn} matrices.
-# It is well known that S₊ is a self-dual proper cone.
-# Let P₊ be the cone of symmetric semidefinite matrices in
-# the n^2 dimensional space of R^{nxn} matrices and
-# let D₊ be the cone of matrices A such that A+Aᵀ ∈ P₊.
-# P₊ is not proper since it is not solid (as it is not n^2 dimensional) so it is not ensured that (P₊)** = P₊
-# However this is the case since, as we will see, (P₊)* = D₊ and (D₊)* = P₊.
-# * Let us first see why (P₊)* = D₊.
-#   If B is symmetric, then ⟨A,B⟩ = ⟨Aᵀ,Bᵀ⟩ = ⟨Aᵀ,B⟩ so 2⟨A,B⟩ = ⟨A,B⟩ + ⟨Aᵀ,B⟩ = ⟨A+Aᵀ,B⟩
-#   Therefore, ⟨A,B⟩ ⩾ 0 for all B ∈ P₊ if and only if ⟨A+Aᵀ,B⟩ ⩾ 0 for all B ∈ P₊
-#   Since A+Aᵀ is symmetric and we know that S₊ is self-dual, we have shown that (P₊)*
-#   is the set of matrices A such that A+Aᵀ is PSD
-# * Let us now see why (D₊)* = P₊.
-#   Since A ∈ D₊ implies that Aᵀ ∈ D₊, B ∈ (D₊)* means that ⟨A+Aᵀ,B⟩ ⩾ 0 for any A ∈ D₊ hence B is positive semi-definite.
-#   To see why it should be symmetric, simply notice that if B[i,j] < B[j,i] then ⟨A,B⟩ can be made arbitrarily small by setting
-#   A[i,j] += s
-#   A[j,i] -= s
-#   with s arbitrarilly large, and A stays in D₊ as A+Aᵀ does not change.
-#
-# Typically, SDP primal/dual are presented as
-# min ⟨C, X⟩                                                                max ∑ b_ky_k
-# ⟨A_k, X⟩ = b_k ∀k                                                         C - ∑ A_ky_k ∈ S₊
-#        X ∈ S₊                                                                      y_k free ∀k
-# Here, as we allow A_i to be non-symmetric, we should rather use
-# min ⟨C, X⟩                                                                max ∑ b_ky_k
-# ⟨A_k, X⟩ = b_k ∀k                                                         C - ∑ A_ky_k ∈ P₊
-#        X ∈ D₊                                                                      y_k free ∀k
-# which is implemented as
-# min ⟨C, Z⟩ + (C[i,j]-C[j-i])s[i,j]                                        max ∑ b_ky_k
-# ⟨A_k, Z⟩ + (A_k[i,j]-A_k[j,i])s[i,j] = b_k ∀k                   C+Cᵀ - ∑ (A_k+A_kᵀ)y_k ∈ S₊
-#       s[i,j] free  1 ⩽ i,j ⩽ n with i > j     C[i,j]-C[j-i] - ∑ (A_k[i,j]-A_k[j,i])y_k = 0  1 ⩽ i,j ⩽ n with i > j
-#        Z ∈ S₊                                                                      y_k free ∀k
-# where "∈ S₊" only look at the diagonal and upper diagonal part.
-# In the last primal program, we have the variables Z = X + Xᵀ and a upper triangular matrix S such that X = Z + S - Sᵀ
-
 """
     SquareBridge{T,F,G,TT,ST} <: Bridges.Constraint.AbstractBridge
 
