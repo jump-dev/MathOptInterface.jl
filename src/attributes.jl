0638b6bd495bbd911d8c9cca9955b344461caaa6
mtanneau <9593025+mtanneau@users.noreply.github.com>
2022-07-12 16:53:47 -0400

    Fix typo
    Co-authored-by: Oscar Dowson <odow@users.noreply.github.com>

--- src/attributes.jl
+++ src/attributes.jl
@@ -855,7 +855,7 @@ attribute_value_type(::RelativeGapTolerance) = Union{Nothing,Float64}
 An optimizer attribute for setting the absolute gap tolerance for an optimization.
 This is an _optimizer_ attribute, and should be set before calling [`optimize!`](@ref).
 
-To set a _relative_ gap tolerance, see [`RelativeGapTolerance``](@ref).
+To set a _relative_ gap tolerance, see [`RelativeGapTolerance`](@ref).
 
 !!! warning
     The mathematical definition of "absolute gap", and its treatment during the optimization,
