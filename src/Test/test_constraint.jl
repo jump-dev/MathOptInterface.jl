3c37f1d0695e84a5fcb1ea28f48856835e2e0ec9
Mathieu Besançon <mathieu.besancon@gmail.com>
2022-10-27 00:14:43 +0200

    [Test] add requires for test_constraint_get_ConstraintIndex (#2024)

--- src/Test/test_constraint.jl
+++ src/Test/test_constraint.jl
@@ -17,9 +17,12 @@ function test_constraint_get_ConstraintIndex(
     ::Config{T},
 ) where {T}
     x = MOI.add_variable(model)
-    c1 = MOI.add_constraint(model, T(1) * x, MOI.GreaterThan(T(1)))
+    f = T(1) * x
+    CI = MOI.ConstraintIndex{typeof(f),MOI.GreaterThan{T}}
+    @requires MOI.supports(model, MOI.ConstraintName(), CI)
+    c1 = MOI.add_constraint(model, f, MOI.GreaterThan(T(1)))
     MOI.set(model, MOI.ConstraintName(), c1, "c1")
-    c2 = MOI.add_constraint(model, T(1) * x, MOI.LessThan(T(2)))
+    c2 = MOI.add_constraint(model, f, MOI.LessThan(T(2)))
     MOI.set(model, MOI.ConstraintName(), c2, "c2")
     F = MOI.ScalarAffineFunction{T}
     @test MOI.get(model, MOI.ConstraintIndex, "c3") === nothing
