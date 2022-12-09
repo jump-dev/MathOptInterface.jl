0ed33d0fd173e1c7ece10ca5c1b713c8d89f5643
odow <o.dowson@gmail.com>
2022-12-09 16:43:11 +1300

    [Utilities] add distance_to_set for SecondOrderCone and ExponentialCone

--- test/Utilities/distance_to_set.jl
+++ test/Utilities/distance_to_set.jl
@@ -8,6 +8,7 @@ module TestFeasibilityChecker
 
 using Test
 
+import LinearAlgebra
 import MathOptInterface
 
 const MOI = MathOptInterface
@@ -124,6 +125,33 @@ function test_zeros()
         MOI.Utilities.distance_to_set([-1.0, 1.0], MOI.Zeros(1))
     )
     @test MOI.Utilities.distance_to_set([-1.0, 1.0], MOI.Zeros(2)) ≈ sqrt(2)
+    return
+end
+
+function test_secondordercone()
+    @test_throws(
+        DimensionMismatch,
+        MOI.Utilities.distance_to_set([-1.0, 1.0], MOI.SecondOrderCone(3))
+    )
+    set = MOI.SecondOrderCone(3)
+    @test MOI.Utilities.distance_to_set([sqrt(2), 1, 1], set) ≈ 0.0
+    @test MOI.Utilities.distance_to_set([-sqrt(2), 1, 1], set) ≈ 2
+    @test MOI.Utilities.distance_to_set([-2, 1, 1], set) ≈ sqrt(6)
+    # According to Boyd, (t, x) = (1, [1, 1]), projects to:
+    d = ((1/2) * (1 + 1 / √2) * [√2, 1, 1]) .- [1, 1, 1]
+    @test MOI.Utilities.distance_to_set([1, 1, 1], set) ≈ LinearAlgebra.norm(d)
+    return
+end
+
+function test_exponentialcone()
+    @test_throws(
+        DimensionMismatch,
+        MOI.Utilities.distance_to_set([-1.0, 1.0], MOI.ExponentialCone())
+    )
+    set = MOI.ExponentialCone()
+    @test MOI.Utilities.distance_to_set([1, 1, exp(1)], set) ≈ 0.0
+    @test MOI.Utilities.distance_to_set([2, 3, 3], set) ≈ 3 * exp(2 / 3) - 3
+    @test MOI.Utilities.distance_to_set([2, -1, 3], set) ≈ sqrt(2^2 + (exp(2) - 3)^2)
     return
 end
 
