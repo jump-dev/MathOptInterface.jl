9fbb579ab1b20830a142e4927ce727273c2eeec6
odow <o.dowson@gmail.com>
2023-02-20 13:39:07 +1300

    Fix formatting

--- test/Bridges/Constraint/fix_parametric_variables.jl
+++ test/Bridges/Constraint/fix_parametric_variables.jl
@@ -8,8 +8,7 @@ module TestConstraintFixParametricVariables
 
 using Test
 
-using MathOptInterface
-const MOI = MathOptInterface
+import MathOptInterface as MOI
 
 function runtests()
     for name in names(@__MODULE__; all = true)
