f91c82569c3d0763f142aab8d9118ba608bd9fa9
odow <o.dowson@gmail.com>
2023-04-12 15:13:05 +1200

    Add more tests

--- test/Bridges/Objective/vector_functionize.jl
+++ test/Bridges/Objective/vector_functionize.jl
@@ -47,6 +47,45 @@ function test_runtests()
     return
 end
 
+function test_objective_function_value()
+    inner = MOI.Utilities.MockOptimizer(
+        MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}()),
+    )
+    model = MOI.Bridges.Objective.VectorFunctionize{Float64}(inner)
+    MOI.Utilities.loadfromstring!(
+        model,
+        """
+        variables: x, y
+        minobjective: [x, y]
+        """,
+    )
+    MOI.Utilities.set_mock_optimize!(
+        inner,
+        mock -> MOI.Utilities.mock_optimize!(mock, [3.0, 5.6]),
+    )
+    MOI.optimize!(model)
+    @test MOI.get(model, MOI.ObjectiveValue()) ≈ [3.0, 5.6]
+    return
+end
+
+function test_set_objective_sense()
+    inner = MOI.Utilities.MockOptimizer(
+        MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}()),
+    )
+    model = MOI.Bridges.Objective.VectorFunctionize{Float64}(inner)
+    MOI.Utilities.loadfromstring!(
+        model,
+        """
+        variables: x, y
+        minobjective: [x, y]
+        """,
+    )
+    @test MOI.get(model, MOI.ObjectiveSense()) == MOI.MIN_SENSE
+    MOI.set(model, MOI.ObjectiveSense(), MOI.MAX_SENSE)
+    @test MOI.get(model, MOI.ObjectiveSense()) == MOI.MAX_SENSE
+    return
+end
+
 end  # module
 
 TestObjectiveVectorFunctionize.runtests()
