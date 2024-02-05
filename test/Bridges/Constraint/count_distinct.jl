f5d8efa1f1ec756858a99ee06ef0a94fe0e5ab4e
Oscar Dowson <odow@users.noreply.github.com>
2024-02-06 09:55:49 +1300

    [Bridge] implement special case for x != y in CountDistinctToMILPBridge (#2416)

--- test/Bridges/Constraint/count_distinct.jl
+++ test/Bridges/Constraint/count_distinct.jl
@@ -61,21 +61,46 @@ function test_runtests_VectorOfVariables()
     return
 end
 
+function test_runtests_VectorOfVariables_NotEqualTo()
+    MOI.Bridges.runtests(
+        MOI.Bridges.Constraint.CountDistinctToMILPBridge,
+        """
+        variables: n, x, y
+        [n, x, y] in CountDistinct(3)
+        x in Interval(1.0, 4.0)
+        y >= 2.0
+        y <= 5.0
+        n == 2.0
+        """,
+        """
+        variables: n, x, y, z
+        1.0 * x + -1.0 * y + -3.0 * z <= -1.0
+        1.0 * y + -1.0 * x + 5.0 * z <= 4.0
+        x in Interval(1.0, 4.0)
+        y >= 2.0
+        y <= 5.0
+        n == 2.0
+        z in ZeroOne()
+        """,
+    )
+    return
+end
+
 function test_runtests_VectorAffineFunction()
     MOI.Bridges.runtests(
         MOI.Bridges.Constraint.CountDistinctToMILPBridge,
         """
-        variables: x, y
-        [2.0, 2.0 * x + -1.0, y] in CountDistinct(3)
+        variables: d, x, y
+        [d, 2.0 * x + -1.0, y] in CountDistinct(3)
         x in Interval(1.0, 2.0)
         y >= 2.0
         y <= 3.0
         """,
         """
-        variables: x, y, z_x1, z_x2, z_x3, z_y2, z_y3, a_1, a_2, a_3
+        variables: d, x, y, z_x1, z_x2, z_x3, z_y2, z_y3, a_1, a_2, a_3
         2.0 * x + -1.0 * z_x1 + -2.0 * z_x2 + -3.0 * z_x3 == 1.0
         1.0 * y + -2.0 * z_y2 + -3.0 * z_y3 == 0.0
-        a_1 + a_2 + a_3 == 2.0
+        a_1 + a_2 + a_3 + -1.0 * d == 0.0
         z_x1 + z_x2 + z_x3 == 1.0
         z_y2 + z_y3 == 1.0
         z_x1 + -1.0 * a_1 <= 0.0
@@ -143,6 +168,22 @@ function test_runtests_error_affine()
         ),
         MOI.Bridges.final_touch(model),
     )
+    return
+end
+
+function test_resolve_with_modified_not_equal_to()
+    inner = MOI.Utilities.Model{Int}()
+    model = MOI.Bridges.Constraint.CountDistinctToMILP{Int}(inner)
+    x = MOI.add_variables(model, 3)
+    c = MOI.add_constraint.(model, x[2:3], MOI.Interval(0, 2))
+    MOI.add_constraint(model, x[1], MOI.EqualTo(2))
+    MOI.add_constraint(model, MOI.VectorOfVariables(x), MOI.CountDistinct(3))
+    @test MOI.get(inner, MOI.NumberOfVariables()) == 3
+    MOI.Bridges.final_touch(model)
+    @test MOI.get(inner, MOI.NumberOfVariables()) == 4
+    MOI.set(model, MOI.ConstraintSet(), c[2], MOI.Interval(0, 1))
+    MOI.Bridges.final_touch(model)
+    @test MOI.get(inner, MOI.NumberOfVariables()) == 4
     return
 end
 
