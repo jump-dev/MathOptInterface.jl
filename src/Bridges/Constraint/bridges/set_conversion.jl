77a5de046d1faa01a848ef91bff5ece0b2753111
Oscar Dowson <odow@users.noreply.github.com>
2024-11-01 10:03:21 +1300

    Update src/Bridges/Constraint/bridges/set_conversion.jl

--- src/Bridges/Constraint/bridges/set_conversion.jl
+++ src/Bridges/Constraint/bridges/set_conversion.jl
@@ -1,3 +1,9 @@
+# Copyright (c) 2017: Miles Lubin and contributors
+# Copyright (c) 2017: Google Inc.
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 """
     SetConversionBridge{T,S2,S1,F} <:
         MOI.Bridges.Constraint.SetMapBridge{T,S2,S1,F,F}
