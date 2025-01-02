5feb1c0977f41ec24f39616ef4c250a96cfaca34
Oscar Dowson <odow@users.noreply.github.com>
2025-01-03 11:14:54 +1300

    [Bridges.Variable] add bridges in alphabetical order and change weight (#2599)

--- src/Bridges/Variable/bridges/RSOCtoPSDBridge.jl
+++ src/Bridges/Variable/bridges/RSOCtoPSDBridge.jl
@@ -56,6 +56,11 @@ end
 const RSOCtoPSD{T,OT<:MOI.ModelLike} =
     SingleBridgeOptimizer{RSOCtoPSDBridge{T},OT}
 
+# This bridge destorys a lot of structure and adding PSD variables is almost
+# always undesirable. We give this bridge an arbitrarily hight cost so that it
+# is used only if necessary.
+MOI.Bridges.bridging_cost(::Type{<:RSOCtoPSDBridge}) = 10.0
+
 function bridge_constrained_variable(
     ::Type{RSOCtoPSDBridge{T}},
     model::MOI.ModelLike,
