19c9b76a6cd00cd8e0629ac26e5b68190d95b2df
Mathieu Besançon <mathieu.besancon@gmail.com>
2025-02-17 17:57:44 +0100

    Update test/functions.jl
    Co-authored-by: Joaquim <joaquimdgarcia@gmail.com>

--- test/functions.jl
+++ test/functions.jl
@@ -133,8 +133,8 @@ end
 
 function test_VectorQuadraticFunction_constructor()
     x = MOI.VariableIndex.(1:2)
-    expr1 = dot(1.0 * x, x) + dot([2.0, 3.0], x) + 4.2
-    expr2 = dot(3.0 * x, x) + dot([1.0, 3.0], x) + 1.2
+    expr1 = LinearAlgebra.dot(1.0 * x, x) + LinearAlgebra.dot([2.0, 3.0], x) + 4.2
+    expr2 = LinearAlgebra.dot(3.0 * x, x) + LinearAlgebra.dot([1.0, 3.0], x) + 1.2
     f = MOI.VectorQuadraticFunction([expr1, expr2])
     f_vec = MOI.Utilities.vectorize([expr1, expr2])
     @test f ≈ f_vec
