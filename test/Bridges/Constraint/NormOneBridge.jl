6c493ccb7109bba25e6552ae593cb94621419b16
odow <o.dowson@gmail.com>
2025-03-06 11:41:23 +1300

    [Bridges] refactor NormOneBridge.jl from NormInfinityBridge.jl

--- test/Bridges/Constraint/NormOneBridge.jl
+++ test/Bridges/Constraint/NormOneBridge.jl
@@ -4,7 +4,7 @@
 # Use of this source code is governed by an MIT-style license that can be found
 # in the LICENSE.md file or at https://opensource.org/licenses/MIT.
 
-module TestConstraintNormInfinity
+module TestConstraintNormOne
 
 using Test
 
@@ -23,49 +23,47 @@ end
 
 include("../utilities.jl")
 
-function test_NormInfinity()
+function test_NormOne()
     mock = MOI.Utilities.MockOptimizer(
         MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}()),
     )
     config = MOI.Test.Config()
-    bridged_mock = MOI.Bridges.Constraint.NormInfinity{Float64}(mock)
+    bridged_mock = MOI.Bridges.Constraint.NormOne{Float64}(mock)
     MOI.Test.runtests(
         bridged_mock,
         config,
         include = [
-            "test_basic_VectorOfVariables_NormInfinityCone",
-            "test_basic_VectorAffineFunction_NormInfinityCone",
-            "test_basic_VectorQuadraticFunction_NormInfinityCone",
+            "test_basic_VectorOfVariables_NormOneCone",
+            "test_basic_VectorAffineFunction_NormOneCone",
+            "test_basic_VectorQuadraticFunction_NormOneCone",
+            # "test_basic_VectorNonlinearFunction_NormOneCone",
         ],
     )
     return
 end
 
-function test_NormInfinity_1()
+function test_conic_NormOneCone_VectorOfVariables()
     mock = MOI.Utilities.MockOptimizer(
         MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}()),
     )
     config = MOI.Test.Config()
-    bridged_mock = MOI.Bridges.Constraint.NormInfinity{Float64}(mock)
+    bridged_mock = MOI.Bridges.Constraint.NormOne{Float64}(mock)
     mock.optimize! =
         (mock::MOI.Utilities.MockOptimizer) -> MOI.Utilities.mock_optimize!(
             mock,
-            [1.0, 0.5, 1.0],
+            [1.0, 0.5, 0.5, 0.5, 0.5],
             (MOI.VectorAffineFunction{Float64}, MOI.Nonnegatives) =>
-                [[0.0, 1.0, 0.0, 0.0]],
-            (MOI.VectorAffineFunction{Float64}, MOI.Zeros) => [[-1], [-1]],
+                [[1.0, 1.0, 1.0, 0.0, 0.0]],
+            (MOI.VectorAffineFunction{Float64}, MOI.Zeros) => [[-1], [0]],
         )
-    MOI.Test.test_conic_NormInfinityCone_VectorOfVariables(bridged_mock, config)
+    MOI.Test.test_conic_NormOneCone_VectorOfVariables(bridged_mock, config)
     MOI.empty!(bridged_mock)
-    MOI.Test.test_conic_NormInfinityCone_VectorAffineFunction(
-        bridged_mock,
-        config,
-    )
+    MOI.Test.test_conic_NormOneCone_VectorAffineFunction(bridged_mock, config)
     var_names = ["x", "y", "z"]
     MOI.set(
-        mock,
+        bridged_mock,
         MOI.VariableName(),
-        MOI.get(mock, MOI.ListOfVariableIndices()),
+        MOI.get(bridged_mock, MOI.ListOfVariableIndices()),
         var_names,
     )
     nonneg = MOI.get(
@@ -75,6 +73,9 @@ function test_NormInfinity_1()
             MOI.Nonnegatives,
         }(),
     )
+    u, v = MOI.get(mock, MOI.ListOfVariableIndices())[4:5]
+    MOI.set(mock, MOI.VariableName(), u, "u")
+    MOI.set(mock, MOI.VariableName(), v, "v")
     @test length(nonneg) == 1
     MOI.set(mock, MOI.ConstraintName(), nonneg[1], "nonneg")
     zeros = MOI.get(
@@ -89,8 +90,8 @@ function test_NormInfinity_1()
     MOI.set(mock, MOI.ConstraintName(), zeros[2], "y_eq")
 
     s = """
-    variables: x, y, z
-    nonneg: [x + -1.0y, x + -1.0z, x + y, x + z] in Nonnegatives(4)
+    variables: x, y, z, u, v
+    nonneg: [x + -1.0u + -1.0v, u + -1.0y, v + -1.0z, u + y, v + z] in Nonnegatives(5)
     x_eq: [-1.0 + x] in Zeros(1)
     y_eq: [-0.5 + y] in Zeros(1)
     maxobjective: y + z
@@ -100,24 +101,18 @@ function test_NormInfinity_1()
     MOI.Test.util_test_models_equal(
         mock,
         model,
-        var_names,
+        [var_names; "u"; "v"],
         ["nonneg", "x_eq", "y_eq"],
     )
-    MOI.set(
-        bridged_mock,
-        MOI.VariableName(),
-        MOI.get(bridged_mock, MOI.ListOfVariableIndices()),
-        var_names,
-    )
-    norminf = MOI.get(
+    normone = MOI.get(
         bridged_mock,
         MOI.ListOfConstraintIndices{
             MOI.VectorAffineFunction{Float64},
-            MOI.NormInfinityCone,
+            MOI.NormOneCone,
         }(),
     )
-    @test length(norminf) == 1
-    MOI.set(bridged_mock, MOI.ConstraintName(), norminf[1], "norminf")
+    @test length(normone) == 1
+    MOI.set(bridged_mock, MOI.ConstraintName(), normone[1], "normone")
     zeros = MOI.get(
         bridged_mock,
         MOI.ListOfConstraintIndices{
@@ -131,7 +126,7 @@ function test_NormInfinity_1()
 
     s = """
     variables: x, y, z
-    norminf: [1.0x, y, z] in NormInfinityCone(3)
+    normone: [1.0x, y, z] in NormOneCone(3)
     x_eq: [-1.0 + x] in Zeros(1)
     y_eq: [-0.5 + y] in Zeros(1)
     maxobjective: y + z
@@ -142,23 +137,16 @@ function test_NormInfinity_1()
         bridged_mock,
         model,
         var_names,
-        ["norminf", "x_eq", "y_eq"],
+        ["normone", "x_eq", "y_eq"],
     )
     ci = first(
         MOI.get(
             bridged_mock,
             MOI.ListOfConstraintIndices{
                 MOI.VectorAffineFunction{Float64},
-                MOI.NormInfinityCone,
+                MOI.NormOneCone,
             }(),
         ),
-    )
-    nonneg = MOI.get(
-        mock,
-        MOI.ListOfConstraintIndices{
-            MOI.VectorAffineFunction{Float64},
-            MOI.Nonnegatives,
-        }(),
     )
     for attr in [MOI.ConstraintPrimalStart(), MOI.ConstraintDualStart()]
         @test MOI.supports(bridged_mock, attr, typeof(ci))
@@ -166,11 +154,12 @@ function test_NormInfinity_1()
         MOI.set(bridged_mock, attr, ci, value)
         @test MOI.get(bridged_mock, attr, ci) ≈ value
         if attr isa MOI.ConstraintPrimalStart
-            nonneg_value = Float64[3, 6, 5, 2]
+            @test MOI.get(mock, MOI.VariablePrimalStart(), u) == 1
+            @test MOI.get(mock, MOI.VariablePrimalStart(), v) == 2
+            @test MOI.get(mock, attr, nonneg[1]) == Float64[1, 0, 4, 2, 0]
         else
-            nonneg_value = [0.25, 2.25, 1.25, 0.25]
+            @test MOI.get(mock, attr, nonneg[1]) == Float64[4, 0, 2, 1, 0]
         end
-        @test MOI.get(mock, attr, nonneg[1]) ≈ nonneg_value
     end
     _test_delete_bridge(
         bridged_mock,
@@ -181,26 +170,27 @@ function test_NormInfinity_1()
     return
 end
 
-function test_conic_NormInfinityCone_3()
+function test_conic_NormOneCone()
     mock = MOI.Utilities.MockOptimizer(
         MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}()),
     )
     config = MOI.Test.Config()
-    bridged_mock = MOI.Bridges.Constraint.NormInfinity{Float64}(mock)
+    bridged_mock = MOI.Bridges.Constraint.NormOne{Float64}(mock)
     mock.optimize! =
         (mock::MOI.Utilities.MockOptimizer) -> MOI.Utilities.mock_optimize!(
             mock,
-            [2, -1, -1, -1],
+            [4, -1, -1, -1, 1, 1, 1],
             (MOI.VectorAffineFunction{Float64}, MOI.Nonnegatives) =>
-                [vcat(fill(inv(3), 3), zeros(3)), fill(inv(3), 3)],
+                [vcat(ones(4), zeros(3)), ones(3)],
         )
-    MOI.Test.test_conic_NormInfinityCone_3(bridged_mock, config)
+    MOI.Test.test_conic_NormOneCone(bridged_mock, config)
     var_names = ["x", "y1", "y2", "y3"]
+    var_names_all = vcat(var_names, "z1", "z2", "z3")
     MOI.set(
         mock,
         MOI.VariableName(),
         MOI.get(mock, MOI.ListOfVariableIndices()),
-        var_names,
+        var_names_all,
     )
     nonneg = MOI.get(
         mock,
@@ -212,10 +202,9 @@ function test_conic_NormInfinityCone_3()
     @test length(nonneg) == 2
     MOI.set(mock, MOI.ConstraintName(), nonneg[1], "nonneg1")
     MOI.set(mock, MOI.ConstraintName(), nonneg[2], "nonneg2")
-
     s = """
-    variables: x, y1, y2, y3
-    nonneg1: [x + -1.0y1 + -3.0, x + -1.0y2 + -3.0, x + -1.0y3 + -3.0, x + y1 + 1.0, x + y2 + 1.0, x + y3 + 1.0] in Nonnegatives(6)
+    variables: x, y1, y2, y3, z1, z2, z3
+    nonneg1: [x + -1.0 + -1.0z1 + -1.0z2 + -1.0z3, z1 + -1.0y1 + -2.0, z2 + -1.0y2 + -2.0, z3 + -1.0y3 + -2.0, z1 + y1 + 2.0, z2 + y2 + 2.0, z3 + y3 + 2.0] in Nonnegatives(7)
     nonneg2: [y1 + 1.0, y2 + 1.0, y3 + 1.0] in Nonnegatives(3)
     minobjective: x
     """
@@ -224,7 +213,7 @@ function test_conic_NormInfinityCone_3()
     MOI.Test.util_test_models_equal(
         mock,
         model,
-        var_names,
+        var_names_all,
         ["nonneg1", "nonneg2"],
     )
     MOI.set(
@@ -233,15 +222,15 @@ function test_conic_NormInfinityCone_3()
         MOI.get(bridged_mock, MOI.ListOfVariableIndices()),
         var_names,
     )
-    norminf = MOI.get(
+    normone = MOI.get(
         bridged_mock,
         MOI.ListOfConstraintIndices{
             MOI.VectorAffineFunction{Float64},
-            MOI.NormInfinityCone,
+            MOI.NormOneCone,
         }(),
     )
-    @test length(norminf) == 1
-    MOI.set(bridged_mock, MOI.ConstraintName(), norminf[1], "norminf")
+    @test length(normone) == 1
+    MOI.set(bridged_mock, MOI.ConstraintName(), normone[1], "normone")
     nonneg = MOI.get(
         bridged_mock,
         MOI.ListOfConstraintIndices{
@@ -251,10 +240,9 @@ function test_conic_NormInfinityCone_3()
     )
     @test length(nonneg) == 1
     MOI.set(bridged_mock, MOI.ConstraintName(), nonneg[1], "nonneg")
-
     s = """
     variables: x, y1, y2, y3
-    norminf: [x + -1.0, y1 + 2.0, y2 + 2.0, y3 + 2.0] in NormInfinityCone(4)
+    normone: [x + -1.0, y1 + 2.0, y2 + 2.0, y3 + 2.0] in NormOneCone(4)
     nonneg: [y1 + 1.0, y2 + 1.0, y3 + 1.0] in Nonnegatives(3)
     minobjective: x
     """
@@ -264,14 +252,14 @@ function test_conic_NormInfinityCone_3()
         bridged_mock,
         model,
         var_names,
-        ["norminf", "nonneg"],
+        ["normone", "nonneg"],
     )
     ci = first(
         MOI.get(
             bridged_mock,
             MOI.ListOfConstraintIndices{
                 MOI.VectorAffineFunction{Float64},
-                MOI.NormInfinityCone,
+                MOI.NormOneCone,
             }(),
         ),
     )
@@ -284,14 +272,16 @@ function test_conic_NormInfinityCone_3()
     )
     for attr in [MOI.ConstraintPrimalStart(), MOI.ConstraintDualStart()]
         @test MOI.supports(bridged_mock, attr, typeof(ci))
-        value = Float64[5, 1, -2, 3]
+        value = (
+            attr isa MOI.ConstraintPrimalStart ? vcat(3, ones(3)) :
+            vcat(1, fill(-1, 3))
+        )
         MOI.set(bridged_mock, attr, ci, value)
         @test MOI.get(bridged_mock, attr, ci) ≈ value
-        if attr isa MOI.ConstraintPrimalStart
-            nonneg_value = Float64[4, 7, 2, 6, 3, 8]
-        else
-            nonneg_value = [-1, 11, -1, 5, -1, 17] / 6
-        end
+        nonneg_value = (
+            attr isa MOI.ConstraintPrimalStart ?
+            vcat(zeros(4), fill(2.0, 3)) : vcat(ones(4), zeros(3))
+        )
         @test MOI.get(mock, attr, nonneg[1]) ≈ nonneg_value
     end
     _test_delete_bridge(
@@ -305,14 +295,14 @@ end
 
 function test_runtests_vector_of_variables()
     MOI.Bridges.runtests(
-        MOI.Bridges.Constraint.NormInfinityBridge,
+        MOI.Bridges.Constraint.NormOneBridge,
         """
         variables: t, x
-        [t, x] in NormInfinityCone(2)
+        [t, x] in NormOneCone(2)
         """,
         """
-        variables: t, x
-        [t + -1.0 * x, t + x] in Nonnegatives(2)
+        variables: t, x, y
+        [t + -1.0 * y, y + -1.0 * x, y + x] in Nonnegatives(3)
         """,
     )
     return
@@ -320,64 +310,53 @@ end
 
 function test_runtests_vector_affine_function()
     MOI.Bridges.runtests(
-        MOI.Bridges.Constraint.NormInfinityBridge,
+        MOI.Bridges.Constraint.NormOneBridge,
         """
         variables: t, x
-        [t, 2.0 * x + 1.0] in NormInfinityCone(2)
+        [t, 2.0 * x + 1.0] in NormOneCone(2)
         """,
         """
-        variables: t, x
-        [t + -2.0 * x + -1.0, t + 2.0 * x + 1.0] in Nonnegatives(2)
+        variables: t, x, y
+        [t + -1.0 * y, y + -2.0 * x + -1.0, y + 2.0 * x + 1.0] in Nonnegatives(3)
         """,
     )
     return
 end
 
-function test_NormInfinity_VectorNonlinearFunction()
+function test_NormOne_VectorNonlinearFunction()
     # We can't use the standard runtests because ScalarNonlinearFunction does
     # not preserve f(x) ≈ (f(x) - g(x)) + g(x)
     inner = MOI.Utilities.Model{Float64}()
-    model = MOI.Bridges.Constraint.NormInfinity{Float64}(inner)
+    model = MOI.Bridges.Constraint.NormOne{Float64}(inner)
     t = MOI.add_variable(model)
     x = MOI.add_variable(model)
     f = MOI.VectorNonlinearFunction([
         MOI.ScalarNonlinearFunction(:+, Any[t]),
         MOI.ScalarNonlinearFunction(:sin, Any[x]),
     ])
-    c = MOI.add_constraint(model, f, MOI.NormInfinityCone(2))
+    c = MOI.add_constraint(model, f, MOI.NormOneCone(2))
     F, S = MOI.VectorNonlinearFunction, MOI.Nonnegatives
     indices = MOI.get(inner, MOI.ListOfConstraintIndices{F,S}())
     @test length(indices) == 1
     inner_variables = MOI.get(inner, MOI.ListOfVariableIndices())
-    @test length(inner_variables) == 2
-    u, v = inner_variables
-    u_p = MOI.ScalarNonlinearFunction(:+, Any[u])
+    @test length(inner_variables) == 3
+    u, v, w = inner_variables
     v_sin = MOI.ScalarNonlinearFunction(:sin, Any[v])
     g = MOI.VectorNonlinearFunction([
         MOI.ScalarNonlinearFunction(
-            :+,
-            Any[MOI.ScalarNonlinearFunction(:-, Any[v_sin]), u_p],
+            :-,
+            Any[MOI.ScalarNonlinearFunction(:+, Any[u]), 0.0+1.0*w],
         ),
-        MOI.ScalarNonlinearFunction(:+, Any[v_sin, u_p]),
-    ])
-    @test ≈(MOI.get(inner, MOI.ConstraintFunction(), indices[1]), g)
-    h = MOI.VectorNonlinearFunction([
-        MOI.ScalarNonlinearFunction(:+, Any[t]),
-        MOI.ScalarNonlinearFunction(:cos, Any[x]),
-    ])
-    MOI.set(model, MOI.ConstraintFunction(), c, h)
-    v_cos = MOI.ScalarNonlinearFunction(:cos, Any[v])
-    g_2 = MOI.VectorNonlinearFunction([
         MOI.ScalarNonlinearFunction(
             :+,
-            Any[MOI.ScalarNonlinearFunction(:-, Any[v_cos]), u_p],
+            Any[MOI.ScalarNonlinearFunction(:-, Any[v_sin]), w],
         ),
-        MOI.ScalarNonlinearFunction(:+, Any[v_cos, u_p]),
+        MOI.ScalarNonlinearFunction(:+, Any[v_sin, w]),
     ])
-    @test ≈(MOI.get(inner, MOI.ConstraintFunction(), indices[1]), g_2)
+    @test ≈(MOI.get(inner, MOI.ConstraintFunction(), indices[1]), g)
     return
 end
 
 end  # module
 
-TestConstraintNormInfinity.runtests()
+TestConstraintNormOne.runtests()
