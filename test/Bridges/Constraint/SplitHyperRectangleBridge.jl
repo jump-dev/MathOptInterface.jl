6e9fcac503c36b03c27b4c37ec6cb69886d1a92f
Oscar Dowson <odow@users.noreply.github.com>
2025-03-06 14:06:26 +1300

    [Bridges] improve function getter of SplitHyperRectangleBridge (#2681)

--- test/Bridges/Constraint/SplitHyperRectangleBridge.jl
+++ test/Bridges/Constraint/SplitHyperRectangleBridge.jl
@@ -99,6 +99,45 @@ function test_runtests_free_row()
     return
 end
 
+function test_basic_HyperRectangle()
+    model = MOI.Bridges.Constraint.SplitHyperRectangle{Float64}(
+        MOI.Utilities.Model{Float64}(),
+    )
+    config = MOI.Test.Config()
+    MOI.empty!(model)
+    MOI.Test.test_basic_VectorOfVariables_HyperRectangle(model, config)
+    MOI.empty!(model)
+    MOI.Test.test_basic_VectorAffineFunction_HyperRectangle(model, config)
+    MOI.empty!(model)
+    MOI.Test.test_basic_VectorQuadraticFunction_HyperRectangle(model, config)
+    MOI.empty!(model)
+    MOI.Test.test_basic_VectorNonlinearFunction_HyperRectangle(model, config)
+    return
+end
+
+function test_runtests_VectorOfVariables_mix_of_signs()
+    #     0 <= a <= 1       |        a >= 0
+    #  -Inf <= b <= Inf     |        c >= 0
+    #     0 <= c <= Inf     |   -1 + d >= 0
+    #     1 <= d <= Inf     |    1 - a >= 0
+    #  -Inf <= e <= 0       |      - e >= 0
+    #  -Inf <= f <= Inf     |    1 - g >= 0
+    #  -Inf <= g <= 1       |
+    MOI.Bridges.runtests(
+        MOI.Bridges.Constraint.SplitHyperRectangleBridge,
+        """
+        variables: a, b, c, d, e, f, g
+        [a, b, c, d, e, f, g] in HyperRectangle([0.0, -Inf, 0.0, 1.0, -Inf, -Inf, -Inf], [1.0, Inf, Inf, Inf, 0.0, Inf, 1.0])
+        """,
+        """
+        variables: a, b, c, d, e, f, g
+        [1.0 * a, 1.0 * c, 1.0 * d + -1.0, 1.0 + -1.0 * a, -1.0 * e, 1.0 + -1.0 * g] in Nonnegatives(6)
+        """;
+        constraint_start = [1.1, 0.0, 1.2, 1.3, -1.1, 0.0, -1.2],
+    )
+    return
+end
+
 end  # module
 
 TestConstraintHyperRectangle.runtests()
