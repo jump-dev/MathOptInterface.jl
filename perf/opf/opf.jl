ce486313b8cb1bfbaef62bc188c2058ef118ef68
Benoît Legat <benoit.legat@gmail.com>
2025-04-27 21:54:32 +0200

    Add bench

--- perf/opf/opf.jl
+++ perf/opf/opf.jl
@@ -1,19 +1,21 @@
+import MathOptInterface as MOI
 ROSETTA_OPF_DIR = "/home/blegat/git/rosetta-opf"
 include(joinpath(ROSETTA_OPF_DIR, "jump.jl"))
 model = opf_model(parse_data(joinpath(ROSETTA_OPF_DIR, "data/opf_warmup.m")))
-import MathOptInterface as MOI
+JuMP.set_attribute(model, "max_iter", 1)
 JuMP.optimize!(model)
 ipopt = JuMP.unsafe_backend(model)
 ipopt.nlp_model
 ad = ipopt.nlp_data.evaluator
 x = ipopt.inner.x
 g = zeros(length(ipopt.nlp_data.constraint_bounds))
-bench0(ad, x, g)
-bench1(ad, x, g)
-bench2(ad, x, g)
 
 function bench0(ad, x, y)
+    if !isnothing(ad.backend.objective)
+        @time MOI.eval_objective(ad, x)
+    end
     @time MOI.eval_constraint(ad, g, x)
+    return
 end
 
 function bench1(ad, x, y)
@@ -28,3 +30,15 @@ function bench2(ad, x, y)
     σ = 1.0
     @time MOI.eval_hessian_lagrangian(ad, V, x, σ, y)
 end
+
+function bench_opt(model)
+    @time JuMP.optimize!(model)
+end
+
+JuMP.set_attribute(model, "max_iter", 1)
+@profview_allocs JuMP.optimize!(model)
+
+bench_opt(model)
+bench0(ad, x, g)
+bench1(ad, x, g)
+bench2(ad, x, g)
