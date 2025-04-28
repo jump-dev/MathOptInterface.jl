ae37715398e9c27dd6b6256fc4e0ba5bf55e0ff2
Benoît Legat <benoit.legat@gmail.com>
2025-04-28 10:21:58 +0200

    Add odow's bench

---
+++ perf/opf/opf2.jl
@@ -0,0 +1,32 @@
+using Revise
+import Ipopt
+import JuMP
+import MathOptInterface as MOI
+import PGLib
+import PowerModels
+
+model = JuMP.direct_model(Ipopt.Optimizer())
+pm = PowerModels.instantiate_model(
+    PGLib.pglib("pglib_opf_case10000_goc"),
+    PowerModels.ACPPowerModel,
+    PowerModels.build_opf;
+    jump_model = model,
+);
+
+ipopt = JuMP.backend(model)
+x = MOI.get(ipopt, MOI.ListOfVariableIndices())
+m, n = length(ipopt.nlp_model.constraints), length(x)
+
+evaluator = MOI.Nonlinear.Evaluator(
+    ipopt.nlp_model,
+    MOI.Nonlinear.SparseReverseMode(),
+    x,
+)
+MOI.initialize(evaluator, [:Grad, :Jac, :Hess])
+
+H_struct = MOI.hessian_lagrangian_structure(evaluator)
+H = zeros(length(H_struct))
+mu = rand(m)
+sigma = 0.0
+x_v = rand(n)
+@time MOI.eval_hessian_lagrangian(evaluator, H, x_v, sigma, mu)
