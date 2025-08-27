4a2641b0ad155c3d3d45648f33aa75cae7206d56
Oscar Dowson <odow@users.noreply.github.com>
2025-08-28 08:24:55 +1200

    [Utilities] fix dual objective value with open intervals (#2823)

--- test/Utilities/results.jl
+++ test/Utilities/results.jl
@@ -51,6 +51,104 @@ function _test_hyperrectangle(T)
     return
 end
 
+function test_dual_objective_value_open_interval_Interval()
+    inner = MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}())
+    model = MOI.Utilities.MockOptimizer(inner)
+    # -Inf <= x[1] - 1.1 <= Inf
+    # -Inf <= x[2] - 1.2 <= 2.1
+    # -2.2 <= x[3] + 1.3 <= Inf
+    # -2.3 <= x[4] + 1.4 <= 2.4
+    x = MOI.add_variables(model, 4)
+    f = x .+ [-1.1, -1.2, 1.3, 1.4]
+    set = MOI.Interval.([-Inf, -Inf, -2.2, -2.3], [Inf, 2.1, Inf, 2.4])
+    c = MOI.add_constraint.(model, f, set)
+    for (dual, obj) in [
+        [0.0, 0.0, 0.0, 0.0] => 0.0,
+        # d[1]: -(-1.1) = 1.1
+        [-2.0, 0.0, 0.0, 0.0] => -2.2,
+        [-1.0, 0.0, 0.0, 0.0] => -1.1,
+        [1.0, 0.0, 0.0, 0.0] => 1.1,
+        [2.0, 0.0, 0.0, 0.0] => 2.2,
+        # d[2]: -(-1.2 - 2.1) = 3.3
+        [0.0, -2.0, 0.0, 0.0] => -6.6,
+        [0.0, -1.0, 0.0, 0.0] => -3.3,
+        [0.0, 1.0, 0.0, 0.0] => 3.3,
+        [0.0, 2.0, 0.0, 0.0] => 6.6,
+        # d[3]: -(1.3 - -2.2) = -3.5
+        [0.0, 0.0, -2.0, 0.0] => 7.0,
+        [0.0, 0.0, -1.0, 0.0] => 3.5,
+        [0.0, 0.0, 1.0, 0.0] => -3.5,
+        [0.0, 0.0, 2.0, 0.0] => -7.0,
+        # d[4]: -(1.4 - -2.3) = -3.7
+        # d[4]: -(1.4 - 2.4) = 1.0
+        [0.0, 0.0, 0.0, -2.0] => -2.0,
+        [0.0, 0.0, 0.0, -1.0] => -1.0,
+        [0.0, 0.0, 0.0, 1.0] => -3.7,
+        [0.0, 0.0, 0.0, 2.0] => -7.4,
+        #
+        [1.0, 1.0, 1.0, 1.0] => -2.8,
+        [-1.0, -1.0, -1.0, -1.0] => -1.9,
+    ]
+        MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE)
+        MOI.set.(model, MOI.ConstraintDual(), c, dual)
+        d = MOI.Utilities.get_fallback(model, MOI.DualObjectiveValue(), Float64)
+        @test isapprox(d, obj)
+        MOI.set.(model, MOI.ObjectiveSense(), MOI.MAX_SENSE)
+        d = MOI.Utilities.get_fallback(model, MOI.DualObjectiveValue(), Float64)
+        @test isapprox(d, -obj)
+    end
+    return
+end
+
+function test_dual_objective_value_open_interval_Hyperrectangle()
+    inner = MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}())
+    model = MOI.Utilities.MockOptimizer(inner)
+    # -Inf <= x[1] - 1.1 <= Inf
+    # -Inf <= x[2] - 1.2 <= 2.1
+    # -2.2 <= x[3] + 1.3 <= Inf
+    # -2.3 <= x[4] + 1.4 <= 2.4
+    x = MOI.add_variables(model, 4)
+    f = MOI.Utilities.vectorize(x .+ [-1.1, -1.2, 1.3, 1.4])
+    set = MOI.HyperRectangle([-Inf, -Inf, -2.2, -2.3], [Inf, 2.1, Inf, 2.4])
+    c = MOI.add_constraint(model, f, set)
+    for (dual, obj) in [
+        [0.0, 0.0, 0.0, 0.0] => 0.0,
+        # d[1]: -(-1.1) = 1.1
+        [-2.0, 0.0, 0.0, 0.0] => -2.2,
+        [-1.0, 0.0, 0.0, 0.0] => -1.1,
+        [1.0, 0.0, 0.0, 0.0] => 1.1,
+        [2.0, 0.0, 0.0, 0.0] => 2.2,
+        # d[2]: -(-1.2 - 2.1) = 3.3
+        [0.0, -2.0, 0.0, 0.0] => -6.6,
+        [0.0, -1.0, 0.0, 0.0] => -3.3,
+        [0.0, 1.0, 0.0, 0.0] => 3.3,
+        [0.0, 2.0, 0.0, 0.0] => 6.6,
+        # d[3]: -(1.3 - -2.2) = -3.5
+        [0.0, 0.0, -2.0, 0.0] => 7.0,
+        [0.0, 0.0, -1.0, 0.0] => 3.5,
+        [0.0, 0.0, 1.0, 0.0] => -3.5,
+        [0.0, 0.0, 2.0, 0.0] => -7.0,
+        # d[4]: -(1.4 - -2.3) = -3.7
+        # d[4]: -(1.4 - 2.4) = 1.0
+        [0.0, 0.0, 0.0, -2.0] => -2.0,
+        [0.0, 0.0, 0.0, -1.0] => -1.0,
+        [0.0, 0.0, 0.0, 1.0] => -3.7,
+        [0.0, 0.0, 0.0, 2.0] => -7.4,
+        #
+        [1.0, 1.0, 1.0, 1.0] => -2.8,
+        [-1.0, -1.0, -1.0, -1.0] => -1.9,
+    ]
+        MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE)
+        MOI.set(model, MOI.ConstraintDual(), c, dual)
+        d = MOI.Utilities.get_fallback(model, MOI.DualObjectiveValue(), Float64)
+        @test isapprox(d, obj)
+        MOI.set(model, MOI.ObjectiveSense(), MOI.MAX_SENSE)
+        d = MOI.Utilities.get_fallback(model, MOI.DualObjectiveValue(), Float64)
+        @test isapprox(d, -obj)
+    end
+    return
+end
+
 end  # module TestResults
 
 TestResults.runtests()
