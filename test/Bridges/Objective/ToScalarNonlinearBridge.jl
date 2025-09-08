14d5af56916087c0f9ee42b0942144030305b08b
Oscar Dowson <odow@users.noreply.github.com>
2025-09-08 15:33:01 +1200

    [Bridges] restrict when ToScalarNonlinearBridge applies and simplify tests (#2835)

--- test/Bridges/Objective/ToScalarNonlinearBridge.jl
+++ test/Bridges/Objective/ToScalarNonlinearBridge.jl
@@ -21,137 +21,54 @@ function runtests()
     return
 end
 
-include("../utilities.jl")
-
-function test_solve_singlevariable_obj()
-    mock = MOI.Utilities.MockOptimizer(MOI.Utilities.Model{Float64}())
-    model = MOI.Bridges.Objective.ToScalarNonlinear{Float64}(mock)
-    MOI.Utilities.set_mock_optimize!(
-        mock,
-        (mock::MOI.Utilities.MockOptimizer) ->
-            MOI.Utilities.mock_optimize!(mock, [1.0], MOI.FEASIBLE_POINT),
-    )
-    MOI.Test.test_objective_ObjectiveFunction_duplicate_terms(
-        model,
-        MOI.Test.Config(;
-            exclude = Any[MOI.DualObjectiveValue, MOI.ConstraintDual],
-        ),
-    )
-    @test MOI.get(mock, MOI.ObjectiveFunctionType()) ==
-          MOI.ScalarNonlinearFunction
-    @test MOI.get(model, MOI.ObjectiveFunctionType()) ==
-          MOI.ScalarAffineFunction{Float64}
-    @test MOI.get(mock, MOI.ObjectiveSense()) == MOI.MIN_SENSE
-    @test MOI.get(model, MOI.ObjectiveSense()) == MOI.MIN_SENSE
-    vis = MOI.get(model, MOI.ListOfVariableIndices())
-    func = 3.0 * vis[1] + 0.0
-
-    @test MOI.get(
-        model,
-        MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(),
-    ) ≈ func
-    MOI.set(model, MOI.ObjectiveSense(), MOI.MAX_SENSE)
-    @test MOI.get(mock, MOI.ObjectiveSense()) == MOI.MAX_SENSE
-    @test MOI.get(model, MOI.ObjectiveSense()) == MOI.MAX_SENSE
-
-    _test_delete_objective(model, 1, tuple())
-    return
-end
-
-function test_solve_result_index()
-    mock = MOI.Utilities.MockOptimizer(MOI.Utilities.Model{Float64}())
-    model = MOI.Bridges.Objective.ToScalarNonlinear{Float64}(mock)
-    MOI.Utilities.set_mock_optimize!(
-        mock,
-        (mock::MOI.Utilities.MockOptimizer) -> MOI.Utilities.mock_optimize!(
-            mock,
-            MOI.OPTIMAL,
-            (MOI.FEASIBLE_POINT, [1.0]),
-            MOI.FEASIBLE_POINT,
-            (MOI.VariableIndex, MOI.GreaterThan{Float64}) => [1.0],
-        ),
-    )
-    MOI.Test.test_solve_result_index(
-        model,
-        MOI.Test.Config(;
-            exclude = Any[MOI.DualObjectiveValue, MOI.ConstraintDual],
-        ),
-    )
-
-    return
-end
-
 function test_runtests()
     MOI.Bridges.runtests(
         MOI.Bridges.Objective.ToScalarNonlinearBridge,
-        model -> begin
-            x = MOI.add_variable(model)
-            aff = MOI.ScalarAffineFunction(
-                MOI.ScalarAffineTerm.([2.0], [x]),
-                1.0,
-            )
-            MOI.set(
-                model,
-                MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(),
-                aff,
-            )
-            MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE)
-        end,
-        model -> begin
-            x = MOI.add_variable(model)
-            exp = MOI.ScalarNonlinearFunction(
-                :+,
-                [
-                    MOI.ScalarNonlinearFunction(
-                        :*,
-                        [2.0, MOI.VariableIndex(1)],
-                    ),
-                    1.0,
-                ],
-            )
-            MOI.set(
-                model,
-                MOI.ObjectiveFunction{MOI.ScalarNonlinearFunction}(),
-                exp,
-            )
-            MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE)
-        end,
+        """
+        variables: x
+        minobjective: 2.0 * x + 1.0
+        """,
+        """
+        variables: x
+        minobjective: ScalarNonlinearFunction(2.0 * x + 1.0)
+        """,
     )
     MOI.Bridges.runtests(
         MOI.Bridges.Objective.ToScalarNonlinearBridge,
-        model -> begin
-            x = MOI.add_variable(model)
-            aff = MOI.ScalarAffineFunction(
-                MOI.ScalarAffineTerm.([2.0], [x]),
-                1.0,
-            )
-            MOI.set(
-                model,
-                MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(),
-                aff,
-            )
-            MOI.set(model, MOI.ObjectiveSense(), MOI.MAX_SENSE)
-        end,
-        model -> begin
-            x = MOI.add_variable(model)
-            exp = MOI.ScalarNonlinearFunction(
-                :+,
-                [
-                    MOI.ScalarNonlinearFunction(
-                        :*,
-                        [2.0, MOI.VariableIndex(1)],
-                    ),
-                    1.0,
-                ],
-            )
-            MOI.set(
-                model,
-                MOI.ObjectiveFunction{MOI.ScalarNonlinearFunction}(),
-                exp,
-            )
-            MOI.set(model, MOI.ObjectiveSense(), MOI.MAX_SENSE)
-        end,
+        """
+        variables: x
+        maxobjective: 2.0 * x + 1.0
+        """,
+        """
+        variables: x
+        maxobjective: ScalarNonlinearFunction(2.0 * x + 1.0)
+        """,
     )
+    MOI.Bridges.runtests(
+        MOI.Bridges.Objective.ToScalarNonlinearBridge,
+        """
+        variables: x, y
+        minobjective: 1.0 * x * x + 2.0 * x * y + 3.0 * y + 4.0
+        """,
+        """
+        variables: x, y
+        minobjective: ScalarNonlinearFunction(1.0 * x * x + 2.0 * x * y + 3.0 * y + 4.0)
+        """,
+    )
+    return
+end
+
+function test_supports()
+    for T in (Int, Float64)
+        model = MOI.instantiate(MOI.Utilities.Model{T}; with_bridge_type = T)
+        for (F, flag) in [
+            MOI.ScalarNonlinearFunction => true,
+            MOI.ScalarAffineFunction{Float64} => (T == Float64),
+            MOI.ScalarAffineFunction{Int} => (T == Int),
+        ]
+            @test MOI.supports(model, MOI.ObjectiveFunction{F}()) == flag
+        end
+    end
     return
 end
 
