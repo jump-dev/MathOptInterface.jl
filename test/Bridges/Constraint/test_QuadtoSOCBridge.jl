19acb0a63795392c5418f4614ee09265e4fd8791
Oscar Dowson <odow@users.noreply.github.com>
2026-03-04 11:18:47 +1300

    Apply suggestion from @odow

--- test/Bridges/Constraint/test_QuadtoSOCBridge.jl
+++ test/Bridges/Constraint/test_QuadtoSOCBridge.jl
@@ -389,7 +389,6 @@ function test_compute_sparse_sqrt_edge_cases()
         BigFloat[1.0 0.0; 0.0 2.0],
         BigFloat[1.0 1.0; 1.0 1.0],
     ]
-        @show A
         B = SparseArrays.sparse(A)
         f = zero(MOI.ScalarQuadraticFunction{eltype(A)})
         s = MOI.GreaterThan(zero(eltype(A)))
